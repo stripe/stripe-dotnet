dd73c3a6801d56b922098e2c05fac9bcc3d8a5a0
Chris Williams <sirana@me.com>
2012-04-02 11:44:16 -0700

    Added new StripeTransfer, StripeTransferSummary entities. Also, implemented parsing for Stripe web hooks so that referenced data will appear as a native Stripe type (StripeCharge, StripeCustomer, etc.)

---
+++ src/Stripe/Entities/StripeTransfer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Stripe.Infrastructure;
+
+namespace Stripe
+{
+    public class StripeTransfer
+    {
+        [JsonProperty("date")]
+        [JsonConverter(typeof(StripeDateTimeConverter))]
+        public DateTime Date { get; set; }
+
+        [JsonProperty("amount")]
+        public int? AmountInCents { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("other_transfers")]
+        public List<string> OtherTransfers { get; set; }
+
+        [JsonProperty("summary")]
+        public StripeTransferSummary Summary { get; set; }
+
+
+    }
+}
