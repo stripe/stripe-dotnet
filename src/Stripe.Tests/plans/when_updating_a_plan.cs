0bb8117bb42599c0fbc8c446c3b2698a6ab60b8f
Faris Zacina <faris.zacina@upandrunningsoftware.com>
2013-01-21 10:45:23 +0100

    Added support for Plan Update API operation

---
+++ src/Stripe.Tests/plans/when_updating_a_plan.cs
@@ -0,0 +1,33 @@
+﻿using Machine.Specifications;
+
+namespace Stripe.Tests
+{
+    public class when_updating_a_plan
+    {
+        protected static StripePlanCreateOptions StripePlanCreateOptions;
+        protected static StripePlanUpdateOptions StripePlanUpdateOptions;
+        protected static StripePlan CreatedPlan;
+        protected static StripePlan StripePlan;
+
+        private static StripePlanService _stripePlanService;
+
+        Establish context = () =>
+        {
+            _stripePlanService = new StripePlanService();
+            StripePlanCreateOptions = test_data.stripe_plan_create_options.Valid();
+            StripePlanUpdateOptions = test_data.stripe_plan_update_options.Valid();
+            StripePlanUpdateOptions.Id = StripePlanCreateOptions.Id;
+            
+        };
+
+        private Because of = () =>
+                                 {
+                                     CreatedPlan = _stripePlanService.Create(StripePlanCreateOptions);
+
+                                     StripePlan = _stripePlanService.Update(StripePlanUpdateOptions);
+                                 };
+
+
+        Behaves_like<updated_plan_behaviors> behaviors;
+    }
+}
---
+++ src/Stripe/Services/Plans/StripePlanUpdateOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Stripe
+{
+    public class StripePlanUpdateOptions
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+}
