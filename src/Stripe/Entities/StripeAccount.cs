a4f3583f6d46ab5f2e36bec408ee4ba9530eb2b0
Daniel Crenna <daniel.crenna@gmail.com>
2013-02-11 21:15:41 -0500

    Added support for account endpoint

---
+++ src/Stripe/Entities/StripeAccount.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace Stripe
+{
+    public class StripeAccount
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("statement_descriptor")]
+        public string StatementDescriptor { get; set; }
+
+        [JsonProperty("details_submitted")]
+        public bool DetailsSubmitted { get; set; }
+
+        [JsonProperty("charge_enabled")]
+        public bool ChargeEnabled { get; set; }
+
+        [JsonProperty("currencies_supported")]
+        public string[] CurrenciesSupported { get; set; }
+
+        [JsonProperty("object")]
+        public string Object { get; set; }
+    }
+}
---
+++ src/Stripe/Services/Account/StripeAccountService.cs
@@ -0,0 +1,19 @@
+﻿namespace Stripe
+{
+	public class StripeAccountService
+	{
+		private string ApiKey { get; set; }
+
+        public StripeAccountService(string apiKey = null)
+		{
+			ApiKey = apiKey;
+		}
+
+		public virtual StripeAccount Get()
+		{
+			var response = Requestor.GetString(Urls.Account, ApiKey);
+
+			return Mapper<StripeAccount>.MapFromJson(response);
+		}
+	}
+}
