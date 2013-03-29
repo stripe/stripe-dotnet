e7d77df65a13a2abc2f92406a5bbe2a1bbc606ed
Sean Brogan <spbrogan@live.com>
2013-03-28 23:38:40 -0700

    Add support for creating invoice. This allows the user to charge the customer their outstanding balance immediately or sometime prior to the scheduled recurring payment date.  Useful when upgrading a subscription.

---
+++ src/Stripe/Services/Invoices/StripeInvoiceCreateOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Stripe
+{
+    public class StripeInvoiceCreateOptions
+    {
+        [JsonProperty("customer")]
+        public string CustomerId { get; set; }
+    }
+}
