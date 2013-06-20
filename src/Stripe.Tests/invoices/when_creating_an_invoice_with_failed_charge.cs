5fb43aae11270be87afa83d0d34a21f05bf720ed
Kevin Jett <kevjett@gmail.com>
2013-06-20 12:53:15 -0500

    added failure code to charges

---
+++ src/Stripe.Tests/invoices/when_creating_an_invoice_with_failed_charge.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+
+namespace Stripe.Tests
+{
+	public class when_creating_an_invoice_with_failed_charge
+	{
+        private static StripeInvoice _stripeCreatedInvoice;
+        private static StripeInvoice _stripeFailedInvoice;
+		private static StripeInvoiceService _stripeInvoiceService;
+        private static StripeCharge _stripeCharge;
+        private static StripeException _stripeInvoiceException;
+
+		Establish context = () =>
+		{
+			var stripeCustomerService = new StripeCustomerService();
+			var stripeCustomerCreateOptions = test_data.stripe_customer_create_options.ValidCardButChargeFails();
+			var stripeCustomer = stripeCustomerService.Create(stripeCustomerCreateOptions);
+
+            var stripeInvoiceItemService = new StripeInvoiceItemService();
+            var StripeInvoiceItemCreateOptions = test_data.stripe_invoiceitem_create_options.Valid(stripeCustomer.Id);
+            stripeInvoiceItemService.Create(StripeInvoiceItemCreateOptions);
+
+			_stripeInvoiceService = new StripeInvoiceService();
+            _stripeCreatedInvoice = _stripeInvoiceService.Create(stripeCustomer.Id);
+		};
+
+	    private Because of = () =>
+	    {
+            try
+            {
+                _stripeInvoiceService.Pay(_stripeCreatedInvoice.Id);
+            }
+            catch (StripeException ex)
+            {
+                _stripeInvoiceException = ex;
+            }
+
+            _stripeFailedInvoice = _stripeInvoiceService.Get(_stripeCreatedInvoice.Id);
+
+	        var stripeChargeService = new StripeChargeService();
+            _stripeCharge = stripeChargeService.Get(_stripeFailedInvoice.ChargeId);
+	    };
+
+		It should_have_the_correct_id = () =>
+            _stripeFailedInvoice.Id.ShouldEqual(_stripeCreatedInvoice.Id);
+
+		It should_have_a_declined_failure_message = () =>
+            _stripeCharge.FailureMessage.ShouldBeEqualIgnoringCase("Your card was declined");
+
+        It should_have_a_declined_failure_code = () =>
+            _stripeCharge.FailureCode.ShouldBeEqualIgnoringCase("card_declined");
+
+        It should_have_a_declined_exception_code = () =>
+            _stripeInvoiceException.StripeError.Code.ShouldBeEqualIgnoringCase("card_declined");
+	}
+}
