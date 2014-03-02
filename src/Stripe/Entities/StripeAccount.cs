86d57d52ad9800af8249845dcf6186013898d642
Ben Cull <bjcull@gmail.com>
2014-03-02 15:21:40 -0700

    Updated StripeAccount to include new fields
    Added all fields that were missing from the latest version of the API,
    with the exception of object, because in C# this is implied by the
    class.
    
    Also replaced tabs with spaces as suggested by ReSharper.

--- src/Stripe/Entities/StripeAccount.cs
+++ src/Stripe/Entities/StripeAccount.cs
@@ -2,24 +2,39 @@
 
 namespace Stripe
 {
-	public class StripeAccount
-	{
-		[JsonProperty("id")]
-		public string Id { get; set; }
+    public class StripeAccount
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
 
-		[JsonProperty("email")]
-		public string Email { get; set; }
+        [JsonProperty("email")]
+        public string Email { get; set; }
 
-		[JsonProperty("statement_descriptor")]
-		public string StatementDescriptor { get; set; }
+        [JsonProperty("statement_descriptor")]
+        public string StatementDescriptor { get; set; }
 
-		[JsonProperty("details_submitted")]
-		public bool DetailsSubmitted { get; set; }
+        [JsonProperty("display_name")]
+        public string DisplayName { get; set; }
 
-		[JsonProperty("charge_enabled")]
-		public bool ChargeEnabled { get; set; }
+        [JsonProperty("timezone")]
+        public string Timezone { get; set; }
 
-		[JsonProperty("currencies_supported")]
-		public string[] CurrenciesSupported { get; set; }
-	}
+        [JsonProperty("details_submitted")]
+        public bool DetailsSubmitted { get; set; }
+
+        [JsonProperty("charge_enabled")]
+        public bool ChargeEnabled { get; set; }
+
+        [JsonProperty("transfer_enabled")]
+        public bool TransferEnabled { get; set; }
+
+        [JsonProperty("currencies_supported")]
+        public string[] CurrenciesSupported { get; set; }
+
+        [JsonProperty("default_currency")]
+        public string DefaultCurrency { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+    }
 }
