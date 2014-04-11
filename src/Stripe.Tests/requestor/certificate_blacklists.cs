6aae546317d9f9726c65c5a0fa5f9deb9dfa3880
Stephen Woodrow <srwoodrow@gmail.com>
2014-04-11 15:06:14 -0700

    Add test to ensure built-in SSL validation still works.

--- src/Stripe.Tests/requestor/certificate_blacklists.cs
+++ src/Stripe.Tests/requestor/certificate_blacklists.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace Stripe.Tests
 {
-	public class certificate_blacklists
+	public class when_requesting_a_blacklisted_certificate
 	{
 		protected static Exception ex;
 
@@ -16,4 +16,21 @@ namespace Stripe.Tests
 			// exception type
 			ex.ShouldBeOfType<System.Net.WebException>();
 	}
+
+	// This is to say a non-blacklisted but name-mismatched certificate.
+	// The goal here is to ensure that SSL policy checks are still working.
+	public class when_requesting_a_mismatched_certificate
+	{
+		protected static Exception ex;
+
+		Because of = () =>
+			ex = Catch.Exception(() => Requestor.GetString("https://mismatched.stripe.com/", ""));
+
+		It should_raise_an_authentication_exception = () =>
+			// ideally this would be more a specific exception type, but I developed
+			// this test on mono and ex.GetBaseException returned a mono-specific
+			// exception type
+			ex.ShouldBeOfType<System.Net.WebException>();
+	}
+
 }
