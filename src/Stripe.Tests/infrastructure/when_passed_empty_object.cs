d2d30dc8184b984bde7733099812eb9872db61b6
Richard Lawley <richard@richardlawley.com>
2014-05-31 10:39:37 +0100

    Add missing infrastructure tests

---
+++ src/Stripe.Tests/infrastructure/when_passed_empty_object.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+
+namespace Stripe.Tests.infrastructure
+{
+	public class when_passed_empty_object
+	{
+		private const string original = "foo";
+		private static string _result;
+
+		Because of = () =>
+			_result = ParameterBuilder.ApplyAllParameters(null, original);
+
+		It should_return_original = () =>
+			_result.ShouldEqual(original);
+	}
+}
