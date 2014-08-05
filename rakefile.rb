d4919d14307ab99bd915768248472c6e56d068a1
Tom Winzig <thomas@winzig.com>
2014-08-05 10:40:52 -0700

    Full support for 3.5 compatible nuget build
    Should allow complete 3.5 support for Stripe.net's Nuget.

--- rakefile.rb
+++ rakefile.rb
@@ -2,7 +2,7 @@ require 'albacore'
 
 VERSION = "2.2.6"
 
-task :default => [:build, :output]
+task :default => [:build35, :build40, :output35, :output40]
 
 assemblyinfo :assemblyinfo do |asm|
 	asm.version = VERSION
@@ -14,18 +14,34 @@ assemblyinfo :assemblyinfo do |asm|
 	asm.output_file = "src/SharedAssemblyInfo.cs"
 end
 
-desc "Build"
-msbuild :build => :assemblyinfo do |msb|
+desc "Build 3.5"
+msbuild :build35 => :assemblyinfo do |msb|
+	msb.properties :configuration => :Release35
+	msb.targets :Clean, :Build
+	msb.solution = "src/Stripe.sln"
+	puts 'Solution 3.5 built'
+end
+
+desc "Build 4.0"
+msbuild :build40 => :assemblyinfo do |msb|
 	msb.properties :configuration => :Release
 	msb.targets :Clean, :Build
 	msb.solution = "src/Stripe.sln"
-	puts 'Solution built'
+	puts 'Solution 4.0 built'
 end
 
-desc "Output"
-output :output do |out|
+desc "Output 3.5"
+output :output35 do |out|
 	out.from '.'
-	out.to 'working'
+	out.to 'working35'
+	out.file 'src\Stripe\bin\release 3.5\Stripe.net.dll', :as=>'Stripe.net.dll'
+	puts 'Output 3.5 folder created'
+end
+
+desc "Output 4.0"
+output :output40 do |out|
+	out.from '.'
+	out.to 'working40'
 	out.file 'src\Stripe\bin\release\Stripe.net.dll', :as=>'Stripe.net.dll'
-	puts 'Output folder created'
+	puts 'Output 4.0 folder created'
 end
