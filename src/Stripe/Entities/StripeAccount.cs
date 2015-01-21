d7ccb2259d044feff8754038f9b83b9675fef1cb
Jayme Davis <jaymedavis@gmail.com>
2015-01-21 03:08:32 -0500

    updated old property values on account, still have some to do after the api version gets bumped. closes #257, thanks @hsmith825

--- src/Stripe/Entities/StripeAccount.cs
+++ src/Stripe/Entities/StripeAccount.cs
@@ -4,26 +4,14 @@ namespace Stripe
 {
     public class StripeAccount : StripeObject
     {
-        [JsonProperty("email")]
-        public string Email { get; set; }
+        [JsonProperty("object")]
+        public string Object { get; set; }
 
-        [JsonProperty("statement_descriptor")]
-        public string StatementDescriptor { get; set; }
+        [JsonProperty("charges_enabled")]
+        public bool ChargesEnabled { get; set; }
 
-        [JsonProperty("display_name")]
-        public string DisplayName { get; set; }
-
-        [JsonProperty("timezone")]
-        public string Timezone { get; set; }
-
-        [JsonProperty("details_submitted")]
-        public bool DetailsSubmitted { get; set; }
-
-        [JsonProperty("charge_enabled")]
-        public bool ChargeEnabled { get; set; }
-
-        [JsonProperty("transfer_enabled")]
-        public bool TransferEnabled { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
 
         [JsonProperty("currencies_supported")]
         public string[] CurrenciesSupported { get; set; }
@@ -31,7 +19,22 @@ namespace Stripe
         [JsonProperty("default_currency")]
         public string DefaultCurrency { get; set; }
 
-        [JsonProperty("country")]
-        public string Country { get; set; }
+        [JsonProperty("details_submitted")]
+        public bool DetailsSubmitted { get; set; }
+
+        [JsonProperty("transfers_enabled")]
+        public bool TransfersEnabled { get; set; }
+
+        [JsonProperty("display_name")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("statement_descriptor")]
+        public string StatementDescriptor { get; set; }
+
+        [JsonProperty("timezone")]
+        public string Timezone { get; set; }
     }
 }
