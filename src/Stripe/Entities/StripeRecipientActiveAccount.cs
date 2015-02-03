760f37b8461cb069486e8eea7f8f9c068a37b7c3
Jason Tremper <jason@mechavian.com>
2015-02-03 15:33:40 -0500

    Adding RoutingNumber and Status to ActiveAccount

--- src/Stripe/Entities/StripeRecipientActiveAccount.cs
+++ src/Stripe/Entities/StripeRecipientActiveAccount.cs
@@ -20,6 +20,12 @@ namespace Stripe
         [JsonProperty("last4")]
         public string Last4 { get; set; }
 
+        [JsonProperty("routing_number")]
+        public string RoutingNumber { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
         [JsonProperty("bank_name")]
         public string BankName { get; set; }
 
