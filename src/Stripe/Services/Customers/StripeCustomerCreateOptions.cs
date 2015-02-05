4680c7279f2d543bfbd8f6b0d08d6335aae69f0b
Giovani Brady <nieko.brady@live.com>
2015-02-05 09:45:23 -0800

    Merge remote-tracking branch 'jaymedavis/master'
    Conflicts:
    	src/Stripe/Entities/StripeCustomer.cs
    	src/Stripe/Entities/StripeInvoice.cs

--- src/Stripe/Services/Customers/StripeCustomerCreateOptions.cs
+++ src/Stripe/Services/Customers/StripeCustomerCreateOptions.cs
@@ -5,7 +5,7 @@ using Stripe.Infrastructure;
 
 namespace Stripe
 {
-    public class StripeCustomerCreateOptions : CreditCardOptions
+    public class StripeCustomerCreateOptions : ICreditCardOptions
     {
         [JsonProperty("coupon")]
         public string CouponId { get; set; }
@@ -40,5 +40,41 @@ namespace Stripe
 
         [JsonProperty("metadata")]
         public Dictionary<string, string> Metadata { get; set; }
+
+        [JsonProperty(Properties.CARD_NUMBER)]
+        public string CardNumber { get; set; }
+
+        [JsonProperty(Properties.CARD_EXP_MONTH)]
+        public string CardExpirationMonth { get; set; }
+
+        [JsonProperty(Properties.CARD_EXP_YEAR)]
+        public string CardExpirationYear { get; set; }
+
+        [JsonProperty(Properties.CARD_CVC)]
+        public string CardCvc { get; set; }
+
+        [JsonProperty(Properties.CARD_NAME)]
+        public string CardName { get; set; }
+
+        [JsonProperty(Properties.CARD_ADDRESS_LINE_ONE)]
+        public string CardAddressLine1 { get; set; }
+
+        [JsonProperty(Properties.CARD_ADDRESS_LINE_TWO)]
+        public string CardAddressLine2 { get; set; }
+
+        [JsonProperty(Properties.CARD_ADDRESS_ZIP)]
+        public string CardAddressZip { get; set; }
+
+        [JsonProperty(Properties.CARD_ADDRESS_CITY)]
+        public string CardAddressCity { get; set; }
+
+        [JsonProperty(Properties.CARD_ADDRESS_STATE)]
+        public string CardAddressState { get; set; }
+
+        [JsonProperty(Properties.CARD_ADDRESS_COUNTRY)]
+        public string CardAddressCountry { get; set; }
+
+        [JsonProperty(Properties.CARD_TOKEN_ID)]
+        public string TokenId { get; set; }
     }
 }
--- src/Stripe/Services/ICreditCardOptions.cs
+++ src/Stripe/Services/ICreditCardOptions.cs
@@ -2,42 +2,42 @@
 
 namespace Stripe
 {
-    public abstract class CreditCardOptions : ICreditCardOptions
+    public interface ICreditCardOptions
     {
         [JsonProperty(Properties.CARD_NUMBER)]
-        public string CardNumber { get; set; }
+        string CardNumber { get; set; }
 
         [JsonProperty(Properties.CARD_EXP_MONTH)]
-        public string CardExpirationMonth { get; set; }
+        string CardExpirationMonth { get; set; }
 
         [JsonProperty(Properties.CARD_EXP_YEAR)]
-        public string CardExpirationYear { get; set; }
+        string CardExpirationYear { get; set; }
 
         [JsonProperty(Properties.CARD_CVC)]
-        public string CardCvc { get; set; }
+        string CardCvc { get; set; }
 
         [JsonProperty(Properties.CARD_NAME)]
-        public string CardName { get; set; }
+        string CardName { get; set; }
 
         [JsonProperty(Properties.CARD_ADDRESS_LINE_ONE)]
-        public string CardAddressLine1 { get; set; }
+        string CardAddressLine1 { get; set; }
 
         [JsonProperty(Properties.CARD_ADDRESS_LINE_TWO)]
-        public string CardAddressLine2 { get; set; }
+        string CardAddressLine2 { get; set; }
 
         [JsonProperty(Properties.CARD_ADDRESS_ZIP)]
-        public string CardAddressZip { get; set; }
+        string CardAddressZip { get; set; }
 
         [JsonProperty(Properties.CARD_ADDRESS_CITY)]
-        public string CardAddressCity { get; set; }
+        string CardAddressCity { get; set; }
 
         [JsonProperty(Properties.CARD_ADDRESS_STATE)]
-        public string CardAddressState { get; set; }
+        string CardAddressState { get; set; }
 
         [JsonProperty(Properties.CARD_ADDRESS_COUNTRY)]
-        public string CardAddressCountry { get; set; }
+        string CardAddressCountry { get; set; }
 
         [JsonProperty(Properties.CARD_TOKEN_ID)]
-        public string TokenId { get; set; }
+        string TokenId { get; set; }
     }
 }
