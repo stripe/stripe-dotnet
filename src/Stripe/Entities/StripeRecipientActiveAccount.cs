80d7663fe4d3c6af5c644327ba9363e6d9456e5c
Jayme Davis <jaymedavis@gmail.com>
2015-02-07 02:13:22 -0500

    added default_for_currency to StripeRecipientActiveAccount

--- src/Stripe/Entities/StripeRecipientActiveAccount.cs
+++ src/Stripe/Entities/StripeRecipientActiveAccount.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using Newtonsoft.Json;
 
 namespace Stripe
@@ -17,11 +18,11 @@ namespace Stripe
         [JsonProperty("currency")]
         public string Currency { get; set; }
 
+        [JsonProperty("default_for_currency")]
+        public bool DefaultForCurrency { get; set; }
+
         [JsonProperty("last4")]
         public string Last4 { get; set; }
-
-        [JsonProperty("routing_number")]
-        public string RoutingNumber { get; set; }
 
         [JsonProperty("status")]
         public string Status { get; set; }
@@ -29,16 +30,24 @@ namespace Stripe
         [JsonProperty("bank_name")]
         public string BankName { get; set; }
 
-        [JsonProperty("disabled")]
-        public bool? Disabled { get; set; }
-
         [JsonProperty("fingerprint")]
         public string Fingerprint { get; set; }
 
+        [JsonProperty("routing_number")]
+        public string RoutingNumber { get; set; }
+
+        #region Obsolete
+        [JsonProperty("disabled")]
+        [Obsolete("Doesn't show in the latest documentation. May need to check with Stripe.")]
+        public bool? Disabled { get; set; }
+
         [JsonProperty("metadata")]
+        [Obsolete("Doesn't show in the latest documentation. May need to check with Stripe.")]
         public Dictionary<string, string> Metadata { get; set; }
 
         [JsonProperty("validated")]
+        [Obsolete("Doesn't show in the latest documentation. May need to check with Stripe.")]
         public bool? Validated { get; set; }
+        #endregion
     }
 }
