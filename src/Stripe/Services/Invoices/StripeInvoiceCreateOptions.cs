756b3fa7a596913f7f147e1b30bd8193594c9099
Chizzl <kris.van.looy@gmail.com>
2015-03-24 14:36:58 +0100

    invoice create - adding optional parameters

---
+++ src/Stripe/Services/Invoices/StripeInvoiceCreateOptions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Stripe.Services.Invoices
+{
+    public class StripeInvoiceCreateOptions
+    {
+
+        [JsonProperty("customer")]
+        public bool CustomerId { get; set; }
+
+        [JsonProperty("application_fee")]
+        public int? ApplicationFee { get; set; }
+
+        [JsonProperty("description")]
+        public bool Description { get; set; }
+
+        [JsonProperty("metadata")]
+        public Dictionary<string, string> Metadata { get; set; }
+
+        [JsonProperty("statement_descriptor")]
+        public string StatementDescriptor { get; set; }
+
+        [JsonProperty("subscription")]
+        public string SubscriptionId { get; set; }
+
+        [JsonProperty("tax_percent")]
+        public int? TaxPercent { get; set; }
+    }
+}
