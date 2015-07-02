862712a60f316f5d3f35abdf41b255fa88637663
Jayme Davis <jaymedavis@gmail.com>
2015-07-01 22:13:32 -0400

    bumping version to 4.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Jayme Davis")]
 [assembly: AssemblyProduct("Stripe.net")]
 [assembly: AssemblyCopyright("Copyright (C) Jayme Davis 2015")]
-[assembly: AssemblyVersion("3.0.2")]
+[assembly: AssemblyVersion("4.0.0")]
