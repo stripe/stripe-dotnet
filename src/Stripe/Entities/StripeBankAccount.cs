c8531b7f2b256be62a27e2fccf0e98b2c68e1eaf
jaymedavis <jaymedavis@gmail.com>
2015-07-16 16:13:18 -0400

    added some missing properties to StripeBankAccount and StripeToken

--- src/Stripe/Entities/StripeBankAccount.cs
+++ src/Stripe/Entities/StripeBankAccount.cs
@@ -8,11 +8,17 @@ namespace Stripe
         [JsonProperty("object")]
         public string Object { get; set; }
 
+        [JsonProperty("account")]
+        public string AccountId { get; set; }
+
         [JsonProperty("country")]
         public string Country { get; set; }
 
         [JsonProperty("currency")]
         public string Currency { get; set; }
+
+        [JsonProperty("default_for_currency")]
+        public bool DefaultForCurrency { get; set; }
 
         [JsonProperty("last4")]
         public string Last4 { get; set; }
@@ -20,14 +26,14 @@ namespace Stripe
         [JsonProperty("status")]
         public string Status { get; set; }
 
+        [JsonProperty("metadata")]
+        public Dictionary<string, string> Metadata { get; set; }
+
         [JsonProperty("bank_name")]
         public string BankName { get; set; }
 
         [JsonProperty("fingerprint")]
         public string Fingerprint { get; set; }
-
-        [JsonProperty("metadata")]
-        public Dictionary<string, string> Metadata { get; set; }
 
         [JsonProperty("routing_number")]
         public string RoutingNumber { get; set; }
