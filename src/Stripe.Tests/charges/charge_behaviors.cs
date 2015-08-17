4920086d96c4140f4c0c3fcab0acc5b4d2cba83e
Conner Smith <connerpsmith@gmail.com>
2015-08-16 22:15:31 -0500

    Updating tests with statement descriptor.

--- src/Stripe.Tests/charges/charge_behaviors.cs
+++ src/Stripe.Tests/charges/charge_behaviors.cs
@@ -24,6 +24,9 @@ namespace Stripe.Tests
 
         It should_have_the_correct_description = () =>
             StripeCharge.Description.ShouldEqual(StripeChargeCreateOptions.Description);
+            
+        It should_have_the_correct_statement_descriptor = () =>
+            StripeCharge.Description.ShouldEqual(StripeChargeCreateOptions.StatementDescriptor);
 
         It should_have_the_correct_live_mode = () =>
             StripeCharge.LiveMode.ShouldEqual(false);
