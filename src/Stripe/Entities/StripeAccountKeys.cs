833dd833872f6450a6047a0fbe9333da8a067f54
willous <steve@glimse.me>
2015-10-23 14:52:25 +1100

    added account keys

---
+++ src/Stripe/Entities/StripeAccountKeys.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Stripe
+{
+    public class StripeAccountKeys : StripeObject
+    {
+        [JsonProperty("secret")]
+        public string SecretKey { get; set; }
+
+        [JsonProperty("publishable")]
+        public string PublishableKey { get; set; }
+    }
+}
