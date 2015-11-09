6564cdb65388137f41b7179dcd103af8e1beff03
Øystein Heimark <oystein@dogu.no>
2015-11-09 13:32:13 +0100

    Added update method for charges

---
+++ src/Stripe.Tests/charges/test_data/stripe_charge_update_options.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Stripe.Tests.test_data
+{
+    public static class stripe_charge_update_options
+    {
+        public static StripeChargeUpdateOptions Valid()
+        {
+            var stripeChargeUpdateOptions = new StripeChargeUpdateOptions
+            {
+                Description = "Gangster Pancakes (syrup@og.com)",
+                Metadata = new Dictionary<string, string>
+                {
+                    { "A", "Value-A" },
+                    { "B", "Value-B" },
+                    { "C", "Value-C" }
+                },
+                ReceiptEmail = "syrup@og.com"
+            };
+
+            return stripeChargeUpdateOptions;
+        }
+    }
+}
