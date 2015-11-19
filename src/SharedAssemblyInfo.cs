ba0c641fcb42d3972785b245473cc08e7b095ca9
Jayme Davis <jaymedavis@gmail.com>
2015-11-18 20:08:18 -0500

    bumped version to 5.0.1

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("Jayme Davis")]
 [assembly: AssemblyProduct("Stripe.net")]
 [assembly: AssemblyCopyright("Copyright (C) Jayme Davis 2015")]
-[assembly: AssemblyVersion("5.0.0")]
+[assembly: AssemblyVersion("5.0.1")]
