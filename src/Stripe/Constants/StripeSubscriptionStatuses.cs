ab2313ef5d886aa9a223088df099a1e2027935eb
Ryan Whitmire <ryanw51@gmail.com>
2016-01-10 09:53:07 -0500

    add StripeSubscriptionStatuses

---
+++ src/Stripe/Constants/StripeSubscriptionStatuses.cs
@@ -0,0 +1,11 @@
+﻿namespace Stripe
+{
+    public static class StripeSubscriptionStatuses
+    {
+        public const string Trialing = "trialing";
+        public const string Active = "active";
+        public const string PastDue = "past_due";
+        public const string Canceled = "canceled";
+        public const string Unpaid = "unpaid";
+    }
+}
