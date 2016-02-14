e279c3112e81614bb58237f7bec291d940e2f699
Jayme Davis <jaymedavis@gmail.com>
2016-02-14 01:47:12 -0500

    bumped version to 5.3.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 [assembly: AssemblyTitle("Stripe.net")]
-[assembly: AssemblyDescription("A .net client api for http://stripe.com")]
+[assembly: AssemblyDescription("A .net client for http://stripe.com")]
 [assembly: AssemblyCompany("Jayme Davis")]
 [assembly: AssemblyProduct("Stripe.net")]
 [assembly: AssemblyCopyright("Copyright (C) Jayme Davis 2016")]
-[assembly: AssemblyVersion("5.2.0")]
+[assembly: AssemblyVersion("5.3.0")]
