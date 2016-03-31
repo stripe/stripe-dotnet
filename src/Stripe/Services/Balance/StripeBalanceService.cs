31da93d08015417bcd078e330b48a597ee3b01cc
Mary Davis <mary@madroute.com>
2016-03-30 21:28:01 -0400

    added async to BalanceService

--- src/Stripe/Services/Balance/StripeBalanceService.cs
+++ src/Stripe/Services/Balance/StripeBalanceService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Stripe
 {
@@ -8,35 +9,50 @@ namespace Stripe
 
         public virtual StripeBalance Get(StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var response = Requestor.GetString(Urls.Balance, requestOptions);
-
-            return Mapper<StripeBalance>.MapFromJson(response);
+            return Mapper<StripeBalance>.MapFromJson(
+                Requestor.GetString(Urls.Balance, SetupRequestOptions(requestOptions))
+            );
         }
 
         public virtual StripeBalanceTransaction Get(string id, StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var url = string.Format(Urls.SpecificBalanceTransaction, id);
-            url = this.ApplyAllParameters(null, url, false);
-
-            var response = Requestor.GetString(url, requestOptions);
-
-            return Mapper<StripeBalanceTransaction>.MapFromJson(response);
+            return Mapper<StripeBalanceTransaction>.MapFromJson(
+                Requestor.GetString(this.ApplyAllParameters(null, $"{(Urls.SpecificBalanceTransaction}/{id}", false),
+                SetupRequestOptions(requestOptions))
+            );
         }
 
         public virtual IEnumerable<StripeBalanceTransaction> List(StripeBalanceTransactionListOptions options = null, StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var url = Urls.BalanceTransactions;
-            url = this.ApplyAllParameters(options, url, true);
-
-            var response = Requestor.GetString(url, requestOptions);
-
-            return Mapper<StripeBalanceTransaction>.MapCollectionFromJson(response);
+            return Mapper<StripeBalanceTransaction>.MapCollectionFromJson(
+                Requestor.GetString(this.ApplyAllParameters(options, Urls.BalanceTransactions, true),
+                SetupRequestOptions(requestOptions))
+            );
         }
+
+#if !PORTABLE
+        public virtual async Task<StripeBalance> GetAsync(StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeBalance>.MapFromJson(
+                await Requestor.GetString(Urls.Balance, SetupRequestOptions(requestOptions))
+            );
+        }
+
+        public virtual async Task<StripeBalanceTransaction> GetAsync(string id, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeBalanceTransaction>.MapFromJson(
+                await Requestor.GetString(this.ApplyAllParameters(null, $"{(Urls.SpecificBalanceTransaction}/{id}", false),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+
+        public virtual async Task<IEnumerable<StripeBalanceTransaction>> ListAsync(StripeBalanceTransactionListOptions options = null, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeBalanceTransaction>.MapCollectionFromJson(
+                await Requestor.GetString(this.ApplyAllParameters(options, Urls.BalanceTransactions, true),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+#endif
     }
 }
