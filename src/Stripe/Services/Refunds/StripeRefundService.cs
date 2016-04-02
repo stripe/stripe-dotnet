8408f5b0318aa1dceab00d0b5f483f496afd0d27
Mary Davis <mary@madroute.com>
2016-04-01 20:47:16 -0400

    added async to RefundService

--- src/Stripe/Services/Refunds/StripeRefundService.cs
+++ src/Stripe/Services/Refunds/StripeRefundService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Stripe
 {
@@ -11,50 +12,68 @@ namespace Stripe
 
         public virtual StripeRefund Create(string chargeId, StripeRefundCreateOptions createOptions = null, StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var url = string.Format("{0}/{1}/refunds", Urls.Charges, chargeId);
-            url = this.ApplyAllParameters(createOptions, url, false);
-
-            var response = Requestor.PostString(url, requestOptions);
-
-            return Mapper<StripeRefund>.MapFromJson(response);
+            return Mapper<StripeRefund>.MapFromJson(
+                Requestor.PostString(this.ApplyAllParameters(createOptions, $"{Urls.Charges}/{chargeId}/refunds", false),
+                SetupRequestOptions(requestOptions))
+            );
         }
 
         public virtual StripeRefund Get(string chargeId, string refundId, StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var url = string.Format("{0}/{1}/refunds/{2}", Urls.Charges, chargeId, refundId);
-            url = this.ApplyAllParameters(null, url, false);
-
-            var response = Requestor.GetString(url, requestOptions);
-
-            return Mapper<StripeRefund>.MapFromJson(response);
+            return Mapper<StripeRefund>.MapFromJson(
+                Requestor.GetString(this.ApplyAllParameters(null, $"{Urls.Charges}/{chargeId}/refunds/{refundId}", false),
+                SetupRequestOptions(requestOptions))
+            );
         }
 
         public virtual StripeRefund Update(string chargeId, string refundId, StripeRefundUpdateOptions updateOptions, StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var url = string.Format("{0}/{1}/refunds/{2}", Urls.Charges, chargeId, refundId);
-            url = this.ApplyAllParameters(updateOptions, url, false);
-
-            var response = Requestor.PostString(url, requestOptions);
-
-            return Mapper<StripeRefund>.MapFromJson(response);
+            return Mapper<StripeRefund>.MapFromJson(
+                Requestor.PostString(this.ApplyAllParameters(updateOptions, $"{Urls.Charges}/{chargeId}/refunds/{refundId}", false),
+                SetupRequestOptions(requestOptions))
+            );
         }
 
         public virtual IEnumerable<StripeRefund> List(string chargeId, StripeListOptions listOptions = null, StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var url = string.Format("{0}/{1}/refunds", Urls.Charges, chargeId);
-            url = this.ApplyAllParameters(listOptions, url, true);
-
-            var response = Requestor.GetString(url, requestOptions);
-
-            return Mapper<StripeRefund>.MapCollectionFromJson(response);
+            return Mapper<StripeRefund>.MapCollectionFromJson(
+                Requestor.GetString(this.ApplyAllParameters(listOptions, $"{Urls.Charges}/{chargeId}/refunds", true),
+                SetupRequestOptions(requestOptions))
+            );
         }
+
+#if !PORTABLE
+        public virtual async Task<StripeRefund> CreateAsync(string chargeId, StripeRefundCreateOptions createOptions = null, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeRefund>.MapFromJson(
+                await Requestor.PostStringAsync(this.ApplyAllParameters(createOptions, $"{Urls.Charges}/{chargeId}/refunds", false),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+
+        public virtual async Task<StripeRefund> GetAsync(string chargeId, string refundId, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeRefund>.MapFromJson(
+                await Requestor.GetStringAsync(this.ApplyAllParameters(null, $"{Urls.Charges}/{chargeId}/refunds/{refundId}", false),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+
+        public virtual async Task<StripeRefund> UpdateAsync(string chargeId, string refundId, StripeRefundUpdateOptions updateOptions, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeRefund>.MapFromJson(
+                await Requestor.PostStringAsync(this.ApplyAllParameters(updateOptions, $"{Urls.Charges}/{chargeId}/refunds/{refundId}", false),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+
+        public virtual async Task<IEnumerable<StripeRefund>> ListAsync(string chargeId, StripeListOptions listOptions = null, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeRefund>.MapCollectionFromJson(
+                await Requestor.GetStringAsync(this.ApplyAllParameters(listOptions, $"{Urls.Charges}/{chargeId}/refunds", true),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+#endif
     }
 }
