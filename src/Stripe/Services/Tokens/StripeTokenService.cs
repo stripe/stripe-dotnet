53da1b5ced7624960b770d47f505bf00cd554032
Mary Davis <mary@madroute.com>
2016-04-01 21:08:20 -0400

    added async to TokenService

--- src/Stripe/Services/Tokens/StripeTokenService.cs
+++ src/Stripe/Services/Tokens/StripeTokenService.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+
 ﻿namespace Stripe
 {
     public class StripeTokenService : StripeService
@@ -6,24 +8,36 @@
 
         public virtual StripeToken Create(StripeTokenCreateOptions createOptions, StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var url = this.ApplyAllParameters(createOptions, Urls.Tokens, false);
-
-            var response = Requestor.PostString(url, requestOptions);
-
-            return Mapper<StripeToken>.MapFromJson(response);
+            return Mapper<StripeToken>.MapFromJson(
+                Requestor.PostString(this.ApplyAllParameters(createOptions, Urls.Tokens, false),
+                SetupRequestOptions(requestOptions))
+            );
         }
 
         public virtual StripeToken Get(string tokenId, StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var url = string.Format("{0}/{1}", Urls.Tokens, tokenId);
-
-            var response = Requestor.GetString(url, requestOptions);
-
-            return Mapper<StripeToken>.MapFromJson(response);
+            return Mapper<StripeToken>.MapFromJson(
+                Requestor.GetString($"{Urls.Tokens}/{tokenId}",
+                SetupRequestOptions(requestOptions))
+            );
         }
+
+#if !PORTABLE
+        public virtual async Task<StripeToken> CreateAsync(StripeTokenCreateOptions createOptions, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeToken>.MapFromJson(
+                await Requestor.PostStringAsync(this.ApplyAllParameters(createOptions, Urls.Tokens, false),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+
+        public virtual async Task<StripeToken> GetAsync(string tokenId, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeToken>.MapFromJson(
+                await Requestor.GetStringAsync($"{Urls.Tokens}/{tokenId}",
+                SetupRequestOptions(requestOptions))
+            );
+        }
+#endif
     }
 }
