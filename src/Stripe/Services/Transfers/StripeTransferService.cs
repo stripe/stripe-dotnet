f48c89f596eb9dfcd6ff086621d77e1af719b43a
Mary Davis <mary@madroute.com>
2016-04-01 21:14:59 -0400

    added async to TransferService

--- src/Stripe/Services/Transfers/StripeTransferService.cs
+++ src/Stripe/Services/Transfers/StripeTransferService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Stripe
 {
@@ -10,49 +11,68 @@ namespace Stripe
 
         public virtual StripeTransfer Create(StripeTransferCreateOptions createOptions, StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var url = this.ApplyAllParameters(createOptions, Urls.Transfers, false);
-
-            var response = Requestor.PostString(url, requestOptions);
-
-            return Mapper<StripeTransfer>.MapFromJson(response);
+            return Mapper<StripeTransfer>.MapFromJson(
+                Requestor.PostString(this.ApplyAllParameters(createOptions, Urls.Transfers, false),
+                SetupRequestOptions(requestOptions))
+            );
         }
 
         public virtual StripeTransfer Get(string transferId, StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var url = string.Format("{0}/{1}", Urls.Transfers, transferId);
-            url = this.ApplyAllParameters(null, url, false);
-
-            var response = Requestor.GetString(url, requestOptions);
-
-            return Mapper<StripeTransfer>.MapFromJson(response);
+            return Mapper<StripeTransfer>.MapFromJson(
+                Requestor.GetString(this.ApplyAllParameters(null, $"{Urls.Transfers}/{transferId}", false),
+                SetupRequestOptions(requestOptions))
+            );
         }
 
         public virtual StripeTransfer Cancel(string transferId, StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var url = string.Format("{0}/{1}/cancel", Urls.Transfers, transferId);
-            url = this.ApplyAllParameters(null, url, false);
-
-            var response = Requestor.PostString(url, requestOptions);
-
-            return Mapper<StripeTransfer>.MapFromJson(response);
+            return Mapper<StripeTransfer>.MapFromJson(
+                Requestor.PostString(this.ApplyAllParameters(null, $"{Urls.Transfers}/{transferId}/cancel", false),
+                SetupRequestOptions(requestOptions))
+            );
         }
 
         public virtual IEnumerable<StripeTransfer> List(StripeTransferListOptions listOptions = null, StripeRequestOptions requestOptions = null)
         {
-            requestOptions = SetupRequestOptions(requestOptions);
-
-            var url = Urls.Transfers;
-            url = this.ApplyAllParameters(listOptions, url, true);
-
-            var response = Requestor.GetString(url, requestOptions);
-
-            return Mapper<StripeTransfer>.MapCollectionFromJson(response);
+            return Mapper<StripeTransfer>.MapCollectionFromJson(
+                Requestor.GetString(this.ApplyAllParameters(listOptions, Urls.Transfers, true),
+                SetupRequestOptions(requestOptions))
+            );
         }
+
+#if !PORTABLE
+        public virtual async Task<StripeTransfer> CreateAsync(StripeTransferCreateOptions createOptions, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeTransfer>.MapFromJson(
+                await Requestor.PostStringAsync(this.ApplyAllParameters(createOptions, Urls.Transfers, false),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+
+        public virtual async Task<StripeTransfer> GetAsync(string transferId, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeTransfer>.MapFromJson(
+                await Requestor.GetStringAsync(this.ApplyAllParameters(null, $"{Urls.Transfers}/{transferId}", false),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+
+        public virtual async Task<StripeTransfer> CancelAsync(string transferId, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeTransfer>.MapFromJson(
+                await Requestor.PostStringAsync(this.ApplyAllParameters(null, $"{Urls.Transfers}/{transferId}/cancel", false),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+
+        public virtual async Task<IEnumerable<StripeTransfer>> ListAsync(StripeTransferListOptions listOptions = null, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeTransfer>.MapCollectionFromJson(
+                await Requestor.GetStringAsync(this.ApplyAllParameters(listOptions, Urls.Transfers, true),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+#endif
     }
 }
