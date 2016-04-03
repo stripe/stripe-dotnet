99813f5116a52cf65bcd560fb641ea495eaeaa37
Jayme Davis <jaymedavis@gmail.com>
2016-04-03 01:30:49 -0400

    commented out async test (will move to async later)

--- src/Stripe.Tests/account/when_creating_an_account_with_only_an_emailaddress_async.cs
+++ src/Stripe.Tests/account/when_creating_an_account_with_only_an_emailaddress_async.cs
@@ -4,26 +4,26 @@ using Machine.Specifications;
 
 namespace Stripe.Tests
 {
-    public class when_creating_an_account_with_only_an_emailaddress_async
-    {
-        private static Task<StripeAccount> _stripeAccount;
-        private static StripeAccountService _stripeAccountService;
-        private static StripeAccountCreateOptions _stripeAccountCreateOptions;
+    //public class when_creating_an_account_with_only_an_emailaddress_async
+    //{
+    //    private static Task<StripeAccount> _stripeAccount;
+    //    private static StripeAccountService _stripeAccountService;
+    //    private static StripeAccountCreateOptions _stripeAccountCreateOptions;
 
-        Establish context = () =>
-        {
-            _stripeAccountService = new StripeAccountService();
+    //    Establish context = () =>
+    //    {
+    //        _stripeAccountService = new StripeAccountService();
 
-            _stripeAccountCreateOptions = new StripeAccountCreateOptions()
-            {
-                Email = "joe@" + Guid.NewGuid() + ".com"
-            };
-        };
+    //        _stripeAccountCreateOptions = new StripeAccountCreateOptions()
+    //        {
+    //            Email = "joe@" + Guid.NewGuid() + ".com"
+    //        };
+    //    };
 
-        Because of = () =>
-            _stripeAccount = _stripeAccountService.CreateAsync(_stripeAccountCreateOptions);
+    //    Because of = () =>
+    //        _stripeAccount = _stripeAccountService.CreateAsync(_stripeAccountCreateOptions);
 
-        It should_have_the_correct_email_address = () =>
-            _stripeAccount.Result.Email.ShouldEqual(_stripeAccountCreateOptions.Email);
-    }
+    //    It should_have_the_correct_email_address = () =>
+    //        _stripeAccount.Result.Email.ShouldEqual(_stripeAccountCreateOptions.Email);
+    //}
 }
