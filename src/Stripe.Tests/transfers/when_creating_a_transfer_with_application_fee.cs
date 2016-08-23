b6e06f2e0f1e678b606a625fa744e9446e79707a
dplarina <dplarina@inspirehub.com>
2016-08-23 17:11:27 -0400

    Added test for when_creating_a_transfer_with_application_fee on transfer

---
+++ src/Stripe.Tests/transfers/when_creating_a_transfer_with_application_fee.cs
@@ -0,0 +1,29 @@
+﻿// ***** This test needs a bank account setup in order to run correctly, and the ignore attribute wasn't working. Good times.
+
+//using Machine.Specifications;
+
+//namespace Stripe.Tests
+//{
+//    public class when_creating_a_transfer
+//    {
+//        protected static StripeTransferCreateOptions StripeTransferCreateOptions;
+//        protected static StripeTransfer StripeTransfer;
+
+//        private static StripeTransferService _stripeTransferService;
+
+//        Establish context = () =>
+//        {
+//            _stripeTransferService = new StripeTransferService();
+//            StripeTransferCreateOptions = test_data.stripe_transfer_create_options.Valid();
+//            StripeTransferCreateOptions.ApplicationFee = 10;
+//        };
+
+//        Because of = () =>
+//            StripeTransfer = _stripeTransferService.Create(StripeTransferCreateOptions);
+
+//        Behaves_like<transfer_behaviors> behaviors;
+
+//        It should_have_application_fee = () =>
+//            StripeTransfer.ApplicationFee.ShouldEqual(10);
+//    }
+//}
