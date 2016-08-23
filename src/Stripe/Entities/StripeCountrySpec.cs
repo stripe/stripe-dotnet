1bd6b7f0b5e4cb6f972783116d3ed88e79a5208c
dplarina <dplarina@inspirehub.com>
2016-08-23 19:07:12 -0400

    Added support for stripe country_specs

---
+++ src/Stripe/Entities/StripeCountrySpec.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Stripe.Infrastructure;
+
+namespace Stripe
+{
+    public class StripeCountrySpec : StripeObject
+    {
+        [JsonProperty("object")]
+        public string Object { get; set; }
+
+        [JsonProperty("livemode")]
+        public bool LiveMode { get; set; }
+
+        [JsonProperty("default_currency")]
+        public string DefaultCurrency { get; set; }
+
+        [JsonProperty("supported_bank_account_currencies")]
+        public Dictionary<string, IEnumerable<string>> SupportedBankAccountCurrencies { get; set; }
+
+        [JsonProperty("supported_payment_currencies")]
+        public IEnumerable<string> SupportedPaymentCurrencies { get; set; }
+
+        [JsonProperty("supported_payment_methods")]
+        public IEnumerable<string> SupportedPaymentMethods { get; set; }
+
+        [JsonProperty("verification_fields")]
+        public Dictionary<string, Dictionary<string,IEnumerable<string>>> VerificationFields { get; set; }
+    }
+}
---
+++ src/Stripe/Services/CountrySpecs/StripeCountrySpecsService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Stripe
+{
+    public class StripeCountrySpecsService : StripeService
+    {
+        public StripeCountrySpecsService(string apiKey = null) : base(apiKey) { }
+
+        public virtual StripeCountrySpec Get(string country, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeCountrySpec>.MapFromJson(
+                Requestor.GetString(this.ApplyAllParameters(null, $"{Urls.CountrySpecs}/{country}", false),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+
+        public virtual IEnumerable<StripeCountrySpec> List(StripeChargeListOptions listOptions = null, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeCountrySpec>.MapCollectionFromJson(
+                Requestor.GetString(this.ApplyAllParameters(listOptions, $"{Urls.CountrySpecs}", true),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+
+#if !PORTABLE
+        public virtual async Task<StripeCountrySpec> GetAsync(string country, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeCountrySpec>.MapFromJson(
+                await Requestor.GetStringAsync(this.ApplyAllParameters(null, $"{Urls.CountrySpecs}/{country}", false),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+
+        public virtual async Task<IEnumerable<StripeCountrySpec>> ListAsync(StripeChargeListOptions listOptions = null, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeCountrySpec>.MapCollectionFromJson(
+                await Requestor.GetStringAsync(this.ApplyAllParameters(listOptions, $"{Urls.CountrySpecs}", true),
+                SetupRequestOptions(requestOptions))
+            );
+        }
+#endif
+    }
+}
