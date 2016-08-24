5ee0195c3dfc52058222359ded9dc586a770a200
dplarina <dplarina@inspirehub.com>
2016-08-23 21:06:29 -0400

    Added country spec tests

---
+++ src/Stripe.Tests/countryspecs/when_getting_a_country_spec.cs
@@ -0,0 +1,40 @@
+﻿using Machine.Specifications;
+
+namespace Stripe.Tests
+{
+    public class when_getting_a_country_spec
+    {
+        protected static StripeCountrySpec StripeCountrySpec;
+
+        private static StripeCountrySpecsService _stripeCountrySpecsService;
+
+        Establish context = () =>
+        {
+            _stripeCountrySpecsService = new StripeCountrySpecsService();
+        };
+
+        Because of = () =>
+            StripeCountrySpec = _stripeCountrySpecsService.Get("US");
+
+        It should_have_verification_field_with_individual_company = () =>
+                StripeCountrySpec.VerificationFields.ShouldContain("individual", "company");
+
+        It should_have_default_currency = () =>
+                StripeCountrySpec.DefaultCurrency.ShouldNotBeEmpty();
+
+        It should_have_supported_bank_currencies = () =>
+                StripeCountrySpec.SupportedBankAccountCurrencies.ShouldNotBeEmpty();
+
+        It should_have_supported_payment_currencies = () =>
+                StripeCountrySpec.SupportedPaymentCurrencies.ShouldNotBeEmpty();
+
+        It should_have_supported_payment_methods = () =>
+                StripeCountrySpec.SupportedPaymentMethods.ShouldNotBeEmpty();
+
+        It should_have_verification_field_individual_with_minimum_additional = () =>
+                StripeCountrySpec.VerificationFields["individual"].ShouldContain("minimum", "additional");
+
+        It should_have_verification_field_company_with_minimum_additional = () =>
+                StripeCountrySpec.VerificationFields["company"].ShouldContain("minimum", "additional");
+    }
+}
---
+++ src/Stripe.Tests/countryspecs/when_listing_country_specs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+
+namespace Stripe.Tests
+{
+    public class when_listing_country_specs
+    {
+        private static List<StripeCountrySpec> _stripeCountrySpecList;
+        private static StripeCountrySpecsService _stripeCountrySpecsService;
+
+        Establish context = () =>
+        {
+            _stripeCountrySpecsService = new StripeCountrySpecsService();
+        };
+
+        Because of = () =>
+            _stripeCountrySpecList = _stripeCountrySpecsService.List().ToList();
+
+        It should_have_atleast_21_entries = () =>
+            _stripeCountrySpecList.Count.ShouldBeGreaterThanOrEqualTo(21);
+    }
+}
