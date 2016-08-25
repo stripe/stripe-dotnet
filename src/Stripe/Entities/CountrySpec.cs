8819ee88cab1b29368546c9162c98ef92b539c84
dplarina <dplarina@inspirehub.com>
2016-08-25 13:16:07 -0400

    Changed remaining IEnumerable in CountrySpec to list

--- src/Stripe/Entities/CountrySpec.cs
+++ src/Stripe/Entities/CountrySpec.cs
@@ -14,13 +14,13 @@ namespace Stripe
         public string DefaultCurrency { get; set; }
 
         [JsonProperty("supported_bank_account_currencies")]
-        public Dictionary<string, IEnumerable<string>> SupportedBankAccountCurrencies { get; set; }
+        public Dictionary<string, List<string>> SupportedBankAccountCurrencies { get; set; }
 
         [JsonProperty("supported_payment_currencies")]
-        public IEnumerable<string> SupportedPaymentCurrencies { get; set; }
+        public List<string> SupportedPaymentCurrencies { get; set; }
 
         [JsonProperty("supported_payment_methods")]
-        public IEnumerable<string> SupportedPaymentMethods { get; set; }
+        public List<string> SupportedPaymentMethods { get; set; }
 
         [JsonProperty("verification_fields")]
         public Dictionary<string, Dictionary<string,List<string>>> VerificationFields { get; set; }
