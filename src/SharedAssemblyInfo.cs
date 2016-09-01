d44439ea2bfc3266c543368c397531cb657599af
Jayme Davis <jaymedavis@gmail.com>
2016-09-01 13:47:07 -0400

    version bumperama

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("Jayme Davis")]
 [assembly: AssemblyProduct("Stripe.net")]
 [assembly: AssemblyCopyright("Copyright (C) Jayme Davis 2016")]
-[assembly: AssemblyVersion("6.4.0")]
+[assembly: AssemblyVersion("6.5.0")]
