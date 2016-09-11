f9971dcce793f130d92f0da0d5ae7d59e4110341
Jayme Davis <jaymedavis@gmail.com>
2016-09-11 00:48:56 -0400

    added appveyor build (work in progress), StripeCharge now serializes correctly, closes #583

--- src/Stripe.Tests/Setup.cs
+++ src/Stripe.Tests/Setup.cs
@@ -1,4 +1,5 @@
-﻿using Machine.Specifications;
+﻿using System;
+using Machine.Specifications;
 
 namespace Stripe.Tests
 {
@@ -6,7 +7,12 @@ namespace Stripe.Tests
     {
         void IAssemblyContext.OnAssemblyStart()
         {
-            StripeConfiguration.SetApiKey("sk_test_zH6JDWg1Pk2jGnEJ5PfibhW3");
+            var apiKey = "stripe_test_secret_key";
+
+            var envKey = Environment.GetEnvironmentVariable("stripe_test_secret_key");
+            if (!string.IsNullOrEmpty(envKey)) apiKey = envKey;
+
+            StripeConfiguration.SetApiKey(apiKey);
         }
 
         void IAssemblyContext.OnAssemblyComplete()
