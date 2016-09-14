da23e606a2947f23ddd28aa31ea1ba00d5cbde24
Jayme Davis <jaymedavis@gmail.com>
2016-09-14 14:19:23 -0400

    added status to StripeRefund, charge can now expand, refactored the StripeRefundService to not require the charge, closes #434

---
+++ src/Stripe/Services/Refunds/StripeRefundListOptions.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Stripe
+{
+    public class StripeRefundListOptions : StripeListOptions
+    {
+        [JsonProperty("charge")]
+        public string ChargeId { get; set; }
+    }
+}
