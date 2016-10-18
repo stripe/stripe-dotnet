24f6cf7722b3edd8cba21a30dcc6f5a9de68c089
Jayme Davis <jaymedavis@gmail.com>
2016-10-17 22:29:34 -0400

    bumped version to 6.9.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("Jayme Davis")]
 [assembly: AssemblyProduct("Stripe.net")]
 [assembly: AssemblyCopyright("Copyright (C) Jayme Davis 2016")]
-[assembly: AssemblyVersion("6.8.0")]
+[assembly: AssemblyVersion("6.9.0")]
