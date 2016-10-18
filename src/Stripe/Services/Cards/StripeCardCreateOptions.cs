ecd9f11c9e3e37c7fd17c789815a32c8bd105418
Jayme Davis <jaymedavis@gmail.com>
2016-10-17 21:50:47 -0400

    added metadata/default_for_currency when creating a card via token. closes #615

--- src/Stripe/Services/Cards/StripeCardCreateOptions.cs
+++ src/Stripe/Services/Cards/StripeCardCreateOptions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace Stripe
@@ -10,6 +11,12 @@ namespace Stripe
 
         [JsonProperty("source")]
         public SourceCard SourceCard { get; set; }
+
+        [JsonProperty("metadata")]
+        public Dictionary<string, string> Metadata { get; set; }
+
+        [JsonProperty("default_for_currency")]
+        public bool? DefaultForCurrency { get; set; }
 
         [Obsolete("This is going away in Stripe.net 7.0")]
         [JsonProperty("source")]
