2b23f13af763c651f772a7e86e81f871e7992c49
David Impey <david.impey@purestone.co.uk>
2016-10-23 21:17:37 +1100

    Added ThreeDSecure Service

---
+++ src/Stripe/Services/ThreeDSecure/StripeThreeDSecureCreateOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Stripe
+{
+    public class StripeThreeDSecureCreateOptions
+    {
+        [JsonProperty("amount")]
+        public int Amount { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        public bool UseStripeJsIframe { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        [JsonProperty("return_url")]
+        internal string ReturnUrlInternal
+        {
+            get { return UseStripeJsIframe ? "_callback" : ReturnUrl; }
+        }
+
+        [JsonProperty("card")]
+        public string CardId { get; set; }
+    }
+}
