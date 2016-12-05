63e1d6615802be2e09edb8c71ada8614608d8207
Jayme Davis <jaymedavis@gmail.com>
2016-12-05 18:32:56 -0500

    build captions

--- build/build.psm1
+++ build/build.psm1
@@ -1,12 +1,24 @@
 function Invoke-Restore()
-{
+{	
+	blankLines
+	Write-Host $("RESTORING EVERYTHING...") -BackgroundColor DarkCyan
+	blankLines
+	
 	dotnet restore
 }
 
 function Invoke-Build
 {
+	blankLines
+	Write-Host $("BUILDING STRIPE.NET...") -BackgroundColor DarkCyan
+	blankLines
+
 	dotnet build -c Debug src\Stripe.net
 	dotnet build -c Release src\Stripe.net
+
+	blankLines
+	Write-Host $("BUILDING THE TESTS...") -BackgroundColor DarkCyan
+	blankLines
 
 	dotnet build src\Stripe.net.Tests
 	dotnet build src\Stripe.net.Tests.xUnit
@@ -14,13 +26,36 @@ function Invoke-Build
 
 function Invoke-Test
 {
+	blankLines
+	Write-Host $("RUNNING THE XUNIT TESTS...") -BackgroundColor DarkCyan
+	blankLines
+
 	dotnet test src\Stripe.net.Tests.xUnit
+
+	blankLines
+	Write-Host $("RUNNING THE MSPEC TESTS...") -BackgroundColor DarkCyan
+	blankLines
+
 	dotnet test src\Stripe.net.Tests
 }
 
 function Invoke-Pack
 {
+	blankLines
+	Write-Host $("PACKING UP...") -BackgroundColor DarkCyan
+	blankLines
+
 	dotnet pack -c Release src\Stripe.net
 
 	Get-ChildItem Stripe*.nupkg -Recurse | % { Push-AppveyorArtifact $_.FullName -Filename $_.FullName }
+
+	blankLines
+	Write-Host $("HOOOOORAY!!") -BackgroundColor DarkCyan
+	blankLines
+}
+
+function blankLines()
+{
+	Write-Host " "
+	Write-Host " "
 }
