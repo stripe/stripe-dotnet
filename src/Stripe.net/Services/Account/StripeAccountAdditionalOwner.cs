0e579430919365916afb5814419c92b95106c882
Jayme Davis <jaymedavis@gmail.com>
2016-12-05 22:49:30 -0500

    comments for later

--- src/Stripe.net/Services/Account/StripeAccountAdditionalOwner.cs
+++ src/Stripe.net/Services/Account/StripeAccountAdditionalOwner.cs
@@ -4,6 +4,11 @@ namespace Stripe
 {
     public class StripeAccountAdditionalOwner : INestedOptions
     {
+        // it looks like additional owners doesn't update quite the same way, as far as first_name at least.
+        // Or it doesn't work like metadata were you just replace the whole object. However, it doesn't add a new additional user anyway. 
+        // Something to be aware of, but edge case. Also, the date of birth doesn't appear to map the same way in the two objects? Needs more 
+        // research, but since stripe.net uses the same class for this, it's weird.
+
         [JsonProperty("[city]")]
         public string City { get; set; }
 
