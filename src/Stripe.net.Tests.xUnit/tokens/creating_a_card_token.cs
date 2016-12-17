b12ccd633900478c83ed2b6aa5b48e591a6a0b27
Jayme Davis <jaymedavis@gmail.com>
2016-12-17 15:28:34 -0500

    test for fingerprint on card token

---
+++ src/Stripe.net.Tests.xUnit/tokens/creating_a_card_token.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Stripe.Tests.xUnit;
+using Xunit;
+
+namespace Stripe.net.Tests.xUnit
+{
+    public class creating_a_card_token : test
+    {
+        private StripeToken Token { get; set; }
+
+        public creating_a_card_token()
+        {
+            Token = new StripeTokenService(_stripe_api_key).CreateAsync(
+                new StripeTokenCreateOptions
+                {
+                    Card = new StripeCreditCardOptions
+                    {
+                        Name = "Jason Isbell",
+                        AddressCity = "Green Hill",
+                        AddressState = "AL",
+                        AddressLine1 = "I could ask?",
+                        AddressLine2 = "",
+                        AddressZip = "35636",
+                        AddressCountry = "US",
+                        Cvc = "123",
+                        ExpirationMonth = "10",
+                        ExpirationYear = "2025",
+                        Number = "4242424242424242"
+                    }
+                }
+            ).Result;
+        }
+
+        [Fact]
+        public void not_null()
+        {
+            Token.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void has_a_fingerprint()
+        {
+            Token.StripeCard.Fingerprint.Should().NotBeNull();
+        }
+    }
+}
