0e9152597dc45e865eb4860ff4a6bfd64226f9ea
Jayme Davis <jaymedavis@gmail.com>
2017-01-18 22:20:16 -0500

    added test back ?

--- src/Stripe.net.Tests.XUnit/accounts/getting_an_account_with_no_accountid.cs
+++ src/Stripe.net.Tests.XUnit/accounts/getting_an_account_with_no_accountid.cs
@@ -1,33 +1,32 @@
-﻿//using System;
-//using FluentAssertions;
-//using Xunit;
+﻿using System;
+using FluentAssertions;
+using Xunit;
 
-//namespace Stripe.Tests.xUnit
-//{
-//    public class getting_an_account_with_no_accountid : test
-//    {
-//        private StripeAccount RetrievedAccount { get; }
-//        private StripeAccount RetrievedAccountAsync { get; }
+namespace Stripe.Tests.xUnit
+{
+    public class getting_an_account_with_no_accountid : test
+    {
+        private StripeAccount RetrievedAccount { get; }
+        private StripeAccount RetrievedAccountAsync { get; }
 
-//        public getting_an_account_with_no_accountid()
-//        {
-//            RetrievedAccount = new StripeAccountService(_stripe_api_key).Get();
-//            RetrievedAccountAsync = new StripeAccountService(_stripe_api_key).GetAsync().Result;
-//        }
+        public getting_an_account_with_no_accountid()
+        {
+            RetrievedAccount = new StripeAccountService(_stripe_api_key).Get();
+            RetrievedAccountAsync = new StripeAccountService(_stripe_api_key).GetAsync().Result;
+        }
 
-//        [Fact]
-//        public void has_total_count()
-//        {
-//            RetrievedAccount.Should().NotBeNull();
-//            RetrievedAccountAsync.Should().NotBeNull();
-//        }
+        [Fact]
+        public void has_total_count()
+        {
+            RetrievedAccount.Should().NotBeNull();
+            RetrievedAccountAsync.Should().NotBeNull();
+        }
 
-//        [Fact]
-//        public void has_account_id()
-//        {
-//            RetrievedAccount.Id.Should().StartWith("acct_");
-//            RetrievedAccountAsync.Id.Should().StartWith("acct_");
-//        }
-//    }
-//}
-
+        [Fact]
+        public void has_account_id()
+        {
+            RetrievedAccount.Id.Should().StartWith("acct_");
+            RetrievedAccountAsync.Id.Should().StartWith("acct_");
+        }
+    }
+}
