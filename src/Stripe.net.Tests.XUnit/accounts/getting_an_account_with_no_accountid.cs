b38a2be694380f047713d8184d6130469059ca11
Jayme Davis <jaymedavis@gmail.com>
2017-01-18 22:50:42 -0500

    adding back to confirm issue

--- src/Stripe.net.Tests.XUnit/accounts/getting_an_account_with_no_accountid.cs
+++ src/Stripe.net.Tests.XUnit/accounts/getting_an_account_with_no_accountid.cs
@@ -12,21 +12,21 @@ namespace Stripe.Tests.xUnit
         public getting_an_account_with_no_accountid()
         {
             RetrievedAccount = new StripeAccountService(_stripe_api_key).Get();
-            //RetrievedAccountAsync = new StripeAccountService(_stripe_api_key).GetAsync().Result;
+            RetrievedAccountAsync = new StripeAccountService(_stripe_api_key).GetAsync().Result;
         }
 
         [Fact]
         public void has_total_count()
         {
             RetrievedAccount.Should().NotBeNull();
-            //RetrievedAccountAsync.Should().NotBeNull();
+            RetrievedAccountAsync.Should().NotBeNull();
         }
 
         [Fact]
         public void has_account_id()
         {
             RetrievedAccount.Id.Should().StartWith("acct_");
-            //RetrievedAccountAsync.Id.Should().StartWith("acct_");
+            RetrievedAccountAsync.Id.Should().StartWith("acct_");
         }
     }
 }
