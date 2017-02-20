3aae777312b75a1c8fb28ca700b219659c7ab674
aschaible <adam.schaible@gmail.com>
2017-02-20 09:03:08 -0500

    Fixing stripe key

--- src/Stripe.net.Tests.xUnit/test.cs
+++ src/Stripe.net.Tests.xUnit/test.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using FluentAssertions;
-using Xunit;
 
 namespace Stripe.Tests.xUnit
 {
     public abstract class test
     {
-        protected readonly string _stripe_api_key = "sk_test_H6laumw3ZO9BQ4r3eL58fUcM";
-            //Environment.GetEnvironmentVariable("stripe_test_secret_key");
+        protected readonly string _stripe_api_key = Environment.GetEnvironmentVariable("stripe_test_secret_key");
     }
 }
