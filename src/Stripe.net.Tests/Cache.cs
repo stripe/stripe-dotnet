b82a5ecdf6fe39c82bcbd542d9210926bb45f146
Jayme Davis <jaymedavis@gmail.com>
2017-02-22 15:15:37 -0500

    added cache system for managed accounts

---
+++ src/Stripe.net.Tests/Cache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Stripe.Tests
+{
+    public static class Cache
+    {
+        private static StripeAccount _managedAccount { get; set; }
+
+        public static StripeAccount GetManagedAccount()
+        {
+            if(_managedAccount != null) return _managedAccount;
+
+            var accountOptions = test_data.stripe_account_create_options.ValidAccountWithBankAccount();
+            accountOptions.Managed = true;
+
+            return _managedAccount = new StripeAccountService().Create(accountOptions);
+        }
+    }
+}
