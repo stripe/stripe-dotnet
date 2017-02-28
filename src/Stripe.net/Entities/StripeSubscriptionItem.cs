8b465b86df08f0389a62c82e3e76977e73d593a4
TVNielsen <tvnielsen2300@gmail.com>
2017-02-28 11:47:54 +0100

    Added missing entity StripeSubscriptionItem

---
+++ src/Stripe.net/Entities/StripeSubscriptionItem.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stripe
+{
+    public class StripeSubscriptionItem : StripeEntityWithId
+    {
+        [JsonProperty("object")]
+        public string Object { get; set; } = "subscription_item";
+
+        [JsonProperty("created")]
+        public int? Created { get; set; }
+        [JsonProperty("plan")]
+        public StripePlan Plan { get; set; }
+
+        [JsonProperty("quantity")]
+        public int? Quantity { get; set; }
+
+
+    }
+}
