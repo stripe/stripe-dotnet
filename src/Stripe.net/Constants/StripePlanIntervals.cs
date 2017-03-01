79d2135b053e4136cbcbebf752d04d2afb978440
jayme <jayme@DESKTOP-8H8GC9A>
2017-03-01 15:57:29 -0500

    added stripe plan interval constants

---
+++ src/Stripe.net/Constants/StripePlanIntervals.cs
@@ -0,0 +1,10 @@
+﻿namespace Stripe
+{
+    public static class StripePlanIntervals
+    {
+        public const string Day = "day";
+        public const string Week = "week";
+        public const string Month = "month";
+        public const string Year = "year";
+    }
+}
