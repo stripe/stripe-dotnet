5704f6fb3e18b555fe2520dcf4ebac690895f90b
Tuan Pham <tuanphamtam@gmail.com>
2017-03-15 16:17:49 -0600

    Added external accounts service

---
+++ src/Stripe.net/Services/ExternalAccount/StripeAccountExternalAccountService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Stripe.Infrastructure;
+
+namespace Stripe
+{
+    public class StripeAccountExternalAccountService : StripeService
+    {
+        public StripeAccountExternalAccountService(string apiKey = null) : base(apiKey) { }
+
+        //Sync
+        public virtual StripeBankAccount Create(string accountId, StripeAccountExternalAccountCreateOptions createOptions, StripeRequestOptions requestOptions = null)
+        {
+            return Mapper<StripeBankAccount>.MapFromJson(
+                Requestor.PostString(
+                    this.ApplyAllParameters(createOptions, $"{Urls.BaseUrl}/accounts/{accountId}/external_accounts"),
+                    SetupRequestOptions(requestOptions)
+                )
+            );
+        }
+    }
+}
