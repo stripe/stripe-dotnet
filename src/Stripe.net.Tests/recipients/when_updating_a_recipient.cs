0d3aa08d2fd2da303b97a9ce1f2df94b66961507
jaymedavis <jaymedavis@gmail.com>
2017-03-17 16:46:35 -0400

    commented out all recipient tests... guess today is the day they are gone

--- src/Stripe.net.Tests/recipients/when_updating_a_recipient.cs
+++ src/Stripe.net.Tests/recipients/when_updating_a_recipient.cs
@@ -1,40 +1,40 @@
-﻿using Machine.Specifications;
+﻿//using Machine.Specifications;
 
-namespace Stripe.Tests
-{
-    public class when_updating_a_recipient
-    {
-        private static StripeRecipient _stripeRecipient;
-        private static StripeRecipientService _stripeRecipientService;
-        private static string _createdStripeRecipientId;
-        private static StripeRecipientUpdateOptions _stripeRecipientUpdateOptions;
+//namespace Stripe.Tests
+//{
+//    public class when_updating_a_recipient
+//    {
+//        private static StripeRecipient _stripeRecipient;
+//        private static StripeRecipientService _stripeRecipientService;
+//        private static string _createdStripeRecipientId;
+//        private static StripeRecipientUpdateOptions _stripeRecipientUpdateOptions;
 
-        Establish context = () =>
-        {
-            _stripeRecipientService = new StripeRecipientService();
+//        Establish context = () =>
+//        {
+//            _stripeRecipientService = new StripeRecipientService();
 
-            var stripeRecipient = _stripeRecipientService.Create(test_data.stripe_recipient_create_options.ValidIndividual());
-            _createdStripeRecipientId = stripeRecipient.Id;
+//            var stripeRecipient = _stripeRecipientService.Create(test_data.stripe_recipient_create_options.ValidIndividual());
+//            _createdStripeRecipientId = stripeRecipient.Id;
 
-            _stripeRecipientUpdateOptions = test_data.stripe_recipient_update_options.Valid();
-        };
+//            _stripeRecipientUpdateOptions = test_data.stripe_recipient_update_options.Valid();
+//        };
 
-        Because of = () =>
-            _stripeRecipient = _stripeRecipientService.Update(_createdStripeRecipientId, _stripeRecipientUpdateOptions);
+//        Because of = () =>
+//            _stripeRecipient = _stripeRecipientService.Update(_createdStripeRecipientId, _stripeRecipientUpdateOptions);
 
-        It should_have_the_correct_name = () =>
-            _stripeRecipient.Name.ShouldEqual(_stripeRecipientUpdateOptions.Name);
+//        It should_have_the_correct_name = () =>
+//            _stripeRecipient.Name.ShouldEqual(_stripeRecipientUpdateOptions.Name);
 
-        It should_have_the_correct_email = () =>
-            _stripeRecipient.Email.ShouldEqual(_stripeRecipientUpdateOptions.Email);
+//        It should_have_the_correct_email = () =>
+//            _stripeRecipient.Email.ShouldEqual(_stripeRecipientUpdateOptions.Email);
 
-        It should_have_the_correct_description = () =>
-            _stripeRecipient.Description.ShouldEqual(_stripeRecipientUpdateOptions.Description);
+//        It should_have_the_correct_description = () =>
+//            _stripeRecipient.Description.ShouldEqual(_stripeRecipientUpdateOptions.Description);
 
-        It should_have_metadata = () =>
-            _stripeRecipient.Metadata.Count.ShouldBeGreaterThan(0);
+//        It should_have_metadata = () =>
+//            _stripeRecipient.Metadata.Count.ShouldBeGreaterThan(0);
 
-        It should_have_correct_metadata = () =>
-            _stripeRecipient.Metadata.ShouldContainOnly(_stripeRecipientUpdateOptions.Metadata);
-    }
-}
+//        It should_have_correct_metadata = () =>
+//            _stripeRecipient.Metadata.ShouldContainOnly(_stripeRecipientUpdateOptions.Metadata);
+//    }
+//}
