9d50bb7d4606a113a5421f22925d0cd60af669e3
Jayme Davis <jaymedavis@gmail.com>
2017-03-27 18:20:21 -0400

    added the default external account service with support for delete, closes #794 (#796)

---
+++ src/Stripe.Tests.XUnit/accounts/deleting_external_account.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Stripe.Tests.Xunit
+{
+    public class deleting_external_account : IClassFixture<account_fixture>
+    {
+        private readonly account_fixture fixture;
+
+        public deleting_external_account(account_fixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void should_throw_exception_its_default_account()
+        {
+            var exception = Assert.Throws<StripeException>(() =>
+                new StripeExternalAccountService(Cache.ApiKey).Delete(
+                    fixture.Account.Id,
+                    fixture.Account.ExternalAccounts.Data.First().Card.Id
+                )
+            );
+
+            exception.Message.Should().Contain("You cannot delete the default external account for your default currency.");
+        }
+    }
+}
