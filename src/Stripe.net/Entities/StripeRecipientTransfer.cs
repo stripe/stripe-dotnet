e3625938c603d01d3310b1545e175726eeed6560
jaymedavis <jaymedavis@gmail.com>
2017-04-03 16:07:21 -0400

    some recipient transfer code commented out -> something to come back to

--- src/Stripe.net/Entities/StripeRecipientTransfer.cs
+++ src/Stripe.net/Entities/StripeRecipientTransfer.cs
@@ -5,101 +5,105 @@ using Stripe.Infrastructure;
 
 namespace Stripe
 {
-    public class StripeRecipientTransfer : StripeEntityWithId
-    {
-        [JsonProperty("livemode")]
-        public bool LiveMode { get; set; }
+    // this class is a simple type that can be returned from a transfer result when using a recipient. since stripe.net does not support expanding the source
+    // on transfers, this is not needed right now.
+    // an expandable property needs to be added on the stripebalanceservice to expand the source - this is one of many types
 
-        [JsonProperty("amount")]
-        public int Amount { get; set; }
+    //public class StripeRecipientTransfer : StripeEntityWithId
+    //{
+    //    [JsonProperty("livemode")]
+    //    public bool LiveMode { get; set; }
 
-        [JsonProperty("amount_reversed")]
-        public int AmountReversed { get; set; }
+    //    [JsonProperty("amount")]
+    //    public int Amount { get; set; }
 
-        #region Expandable Balance Transaction
-        public string BalanceTransactionId { get; set; }
+    //    [JsonProperty("amount_reversed")]
+    //    public int AmountReversed { get; set; }
 
-        [JsonIgnore]
-        public StripeBalanceTransaction BalanceTransaction { get; set; }
+    //    #region Expandable Balance Transaction
+    //    public string BalanceTransactionId { get; set; }
 
-        [JsonProperty("balance_transaction")]
-        internal object InternalBalanceTransaction
-        {
-            set
-            {
-                ExpandableProperty<StripeBalanceTransaction>.Map(value, s => BalanceTransactionId = s, o => BalanceTransaction = o);
-            }
-        }
-        #endregion
+    //    [JsonIgnore]
+    //    public StripeBalanceTransaction BalanceTransaction { get; set; }
 
-        [JsonProperty("bank_account")]
-        public StripeBankAccount BankAccount { get; set; }
+    //    [JsonProperty("balance_transaction")]
+    //    internal object InternalBalanceTransaction
+    //    {
+    //        set
+    //        {
+    //            ExpandableProperty<StripeBalanceTransaction>.Map(value, s => BalanceTransactionId = s, o => BalanceTransaction = o);
+    //        }
+    //    }
+    //    #endregion
 
-        [JsonProperty("card")]
-        public StripeCard Card { get; set; }
+    //    [JsonProperty("bank_account")]
+    //    public StripeBankAccount BankAccount { get; set; }
 
-        [JsonProperty("created")]
-        [JsonConverter(typeof(StripeDateTimeConverter))]
-        public DateTime Created { get; set; }
+    //    [JsonProperty("card")]
+    //    public StripeCard Card { get; set; }
 
-        [JsonProperty("currency")]
-        public string Currency { get; set; }
+    //    [JsonProperty("created")]
+    //    [JsonConverter(typeof(StripeDateTimeConverter))]
+    //    public DateTime Created { get; set; }
 
-        [JsonProperty("date")]
-        [JsonConverter(typeof(StripeDateTimeConverter))]
-        public DateTime Date { get; set; }
+    //    [JsonProperty("currency")]
+    //    public string Currency { get; set; }
 
-        [JsonProperty("description")]
-        public string Description { get; set; }
+    //    [JsonProperty("date")]
+    //    [JsonConverter(typeof(StripeDateTimeConverter))]
+    //    public DateTime Date { get; set; }
 
-        [JsonProperty("destination")]
-        public string Destination { get; set; }
+    //    [JsonProperty("description")]
+    //    public string Description { get; set; }
 
-        [JsonProperty("failure_code")]
-        public string FailureCode { get; set; }
+    //    [JsonProperty("destination")]
+    //    public string Destination { get; set; }
 
-        [JsonProperty("failure_message")]
-        public string FailureMessage { get; set; }
+    //    [JsonProperty("failure_code")]
+    //    public string FailureCode { get; set; }
 
-        [JsonProperty("metadata")]
-        public Dictionary<string, string> Metadata { get; set; }
+    //    [JsonProperty("failure_message")]
+    //    public string FailureMessage { get; set; }
 
-        [JsonProperty("method")]
-        public string Method { get; set; }
+    //    [JsonProperty("metadata")]
+    //    public Dictionary<string, string> Metadata { get; set; }
 
-        #region Expandable Recipient
+    //    [JsonProperty("method")]
+    //    public string Method { get; set; }
 
-        public string RecipientId { get; set; }
+    //    #region Expandable Recipient
 
-        [JsonIgnore]
-        public StripeRecipient Recipient { get; set; }
+    //    public string RecipientId { get; set; }
 
-        [JsonProperty("recipient")]
-        internal object InternalRecipient
-        {
-            set
-            {
-                ExpandableProperty<StripeRecipient>.Map(value, s => RecipientId = s, o => Recipient = o);
-            }
-        }
-        #endregion
+    //    [JsonIgnore]
+    //    public StripeRecipient Recipient { get; set; }
 
-        [JsonProperty("reversals")]
-        public StripeList<StripeTransferReversal> TransferReversals { get; set; }
+    //    [JsonProperty("recipient")]
+    //    internal object InternalRecipient
+    //    {
+    //        set
+    //        {
+    //            ExpandableProperty<StripeRecipient>.Map(value, s => RecipientId = s, o => Recipient = o);
+    //        }
+    //    }
+    //    #endregion
 
-        [JsonProperty("reversed")]
-        public bool Reversed { get; set; }
+    //    [JsonProperty("reversals")]
+    //    public StripeList<StripeTransferReversal> TransferReversals { get; set; }
 
-        [JsonProperty("source_transaction")]
-        public string SourceTransactionId { get; set; }
+    //    [JsonProperty("reversed")]
+    //    public bool Reversed { get; set; }
 
-        [JsonProperty("statement_descriptor")]
-        public string StatementDescriptor { get; set; }
+    //    [JsonProperty("source_transaction")]
+    //    public string SourceTransactionId { get; set; }
 
-        [JsonProperty("status")]
-        public string Status { get; set; }
+    //    [JsonProperty("statement_descriptor")]
+    //    public string StatementDescriptor { get; set; }
 
-        [JsonProperty("type")]
-        public string Type { get; set; }
-    }
+    //    [JsonProperty("status")]
+    //    public string Status { get; set; }
+
+    //    [JsonProperty("type")]
+    //    public string Type { get; set; }
+    //}
 }
