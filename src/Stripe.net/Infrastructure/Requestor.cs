5a9f317616c43bead750d4d1035dd722489f8f3b
jaymedavis <jaymedavis@gmail.com>
2017-04-25 15:04:19 -0600

    converted test to xunit, fixed

--- src/Stripe.net/Infrastructure/Requestor.cs
+++ src/Stripe.net/Infrastructure/Requestor.cs
@@ -227,7 +227,7 @@ namespace Stripe.Infrastructure
         {
             var result = new StripeResponse
             {
-                RequestId = response.Headers.GetValues("Request-Id") != null ? response.Headers.GetValues("Request-Id").First() : "n/a",
+                RequestId = response.Headers.Contains("RequestId") ? response.Headers.GetValues("Request-Id").First() : "n/a",
                 RequestDate = Convert.ToDateTime(response.Headers.GetValues("Date").First(), CultureInfo.InvariantCulture),
                 ResponseJson = responseText
             };
