6d14ab0ce3468d81fca7c78c2b324e9935f5c79c
jaymedavis <jaymedavis@gmail.com>
2017-04-26 16:57:20 -0600

    added 3dsecure source

---
+++ src/Stripe.net/Entities/Sources/StripeThreeDSecure.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Stripe
+{
+    public class StripeThreeDSecure : StripeEntity
+    {
+        [JsonProperty("card")]
+        public string CardId { get; set; }
+
+        [JsonProperty("customer")]
+        public string CustomerId { get; set; }
+
+        [JsonProperty("authenticated")]
+        public bool Authenticated { get; set; }
+    }
+}
