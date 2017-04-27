81ab2d6b2fb864fc62499627038c9aa7ccaee941
Carlos Boeing <carlosboeing@gmail.com>
2017-04-27 16:27:13 +1000

    Moved properties to alphabetical order

--- src/Stripe.net/Services/TransferReversals/StripeTransferReversalCreateOptions.cs
+++ src/Stripe.net/Services/TransferReversals/StripeTransferReversalCreateOptions.cs
@@ -12,10 +12,10 @@ namespace Stripe
         [JsonProperty("description")]
         public string Description { get; set; }
 
-        [JsonProperty("refund_application_fee")]
-        public bool? RefundApplicationFee { get; set; }
-
         [JsonProperty("metadata")]
         public Dictionary<string, string> Metadata { get; set; }
+
+        [JsonProperty("refund_application_fee")]
+        public bool? RefundApplicationFee { get; set; }
     }
 }
