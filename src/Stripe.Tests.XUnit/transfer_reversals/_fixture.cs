6f44a65cc74064e7685e20e708623bc05b5a725e
Carlos Boeing <carlosboeing@gmail.com>
2017-04-27 16:29:23 +1000

    Added transfer reversal tests

---
+++ src/Stripe.Tests.XUnit/transfer_reversals/_fixture.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Stripe.Tests.Xunit
+{
+    public class transfer_reversals_fixture
+    {
+        public StripeTransferReversalCreateOptions TransferReversalCreateOptions { get; }
+        public StripeTransferReversalUpdateOptions TransferReversalUpdateOptions { get; }
+        public StripeListOptions TransferReversalListOptions { get; }
+
+        public StripeTransfer Transfer { get; }
+        public StripeTransferReversal TransferReversal { get; }
+        public StripeTransferReversal TransferReversalUpdated { get; }
+        public StripeTransferReversal TransferReversalRetrieved { get; }
+        public List<StripeTransferReversal> TransferReversalList { get; }
+
+        public transfer_reversals_fixture()
+        {
+            // make sure the account has sufficient funds first
+            new StripeChargeService(Cache.ApiKey).Create(new StripeChargeCreateOptions
+            {
+                Amount = 10000,
+                Currency = "usd",
+                SourceCard = new SourceCard
+                {
+                    Number = "4000000000000077",
+                    ExpirationMonth = "10",
+                    ExpirationYear = "2019",
+                    Cvc = "123"
+                }
+            });
+
+            // create transfer to be reversed
+            Transfer = new StripeTransferService(Cache.ApiKey).Create(new StripeTransferCreateOptions
+            {
+                Amount = 1000,
+                Currency = "usd",
+                Destination = Cache.GetAccount().Id
+            });
+
+            TransferReversalCreateOptions = new StripeTransferReversalCreateOptions
+            {
+                Amount = 1000,
+                Description = "Transfer reversed"
+            };
+
+            TransferReversalUpdateOptions = new StripeTransferReversalUpdateOptions
+            {
+                Metadata = new Dictionary<string, string>
+                {
+                    { "some-key", "some-value" }
+                }
+            };
+
+            var service = new StripeTransferReversalService(Cache.ApiKey);
+            TransferReversal = service.Create(Transfer.Id, TransferReversalCreateOptions);
+            TransferReversalUpdated = service.Update(Transfer.Id, TransferReversal.Id, TransferReversalUpdateOptions);
+            TransferReversalRetrieved = service.Get(Transfer.Id, TransferReversal.Id);
+            TransferReversalList = service.List(Transfer.Id, new StripeListOptions()).ToList();
+
+            // get transfer reversed
+            Transfer = new StripeTransferService(Cache.ApiKey).Get(Transfer.Id);
+        }
+    }
+}
