0fec46028823824ada4de6729d2b80115f2f0294
jaymedavis <jaymedavis@gmail.com>
2017-04-28 12:55:07 -0600

    start on processing secure webhook

---
+++ src/Stripe.Tests.XUnit/events/when_constructing_an_event.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Stripe.Tests.Xunit
+{
+    public class when_constructing_an_event
+    {
+        // this was sent in the header Stripe-Signature along with the body of event.json
+        private string StripeSignature => "t=1493329224,v1=09b8e30b483570b410390cdac6327c036937e84a6316008a0143db833ac21a09,v0=63f3a72374a733066c4be69ed7f8e5ac85c22c9f0a6a612ab9a025a9e4ee7eef";
+        private string StripeJson         { get; set; }
+        private string StripeSecret =>    "whsec_H68eTX02a4bCbiQOOoSAsIytOvuWZrQC";
+
+        private StripeEvent ConstructedEvent { get; set; }
+
+        public when_constructing_an_event()
+        {
+            StripeJson = new StreamReader(
+                typeof(when_constructing_an_event).GetTypeInfo().Assembly.GetManifestResourceStream("Stripe.Tests.XUnit.events.event.json"),
+                Encoding.UTF8
+            ).ReadToEnd();
+
+            ConstructedEvent = StripeEventUtility.ConstructEvent(StripeJson, StripeSignature, StripeSecret);
+        }
+
+        public StripeAccount Account { get; set; }
+
+        [Fact]
+        public void it_should_validate_with_right_secret()
+        {
+            ConstructedEvent.Should().NotBeNull();
+        }
+    }
+}
--- src/Stripe.net/Services/Events/StripeEventUtility.cs
+++ src/Stripe.net/Services/Events/StripeEventUtility.cs
@@ -1,5 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Stripe
 {
@@ -13,6 +17,40 @@ namespace Stripe
         public static T ParseEventDataItem<T>(dynamic dataItem)
         {
             return JsonConvert.DeserializeObject<T>((dataItem as JObject).ToString());
+        }
+
+        public static StripeEvent ConstructEvent(string json, string stripeSignatureHeader, string secret, int tolerance = 300)
+        {
+            var signatureItems = parseStripeSignature(stripeSignatureHeader);
+
+            var ciphertext = Encipher(secret, signatureItems["t"].FirstOrDefault(), json);
+
+            // make sure the ciphertext exists as a 'v1' key in signatureItems
+            // throw an exception if not
+
+            // make sure the timestamp is not above the tolerance (current utc - timestamp <= tolerance)
+            // throw an exception if not
+
+            return Mapper<StripeEvent>.MapFromJson(json);
+        }
+
+        private static ILookup<string, string> parseStripeSignature(string stripeSignatureHeader)
+        {
+            return stripeSignatureHeader.Trim()
+                .Split(',')
+                .Select(item => item.Trim().Split('='))
+                .ToLookup(item => item[0], item => item[1]);
+        }
+
+        private static string Encipher(string secret, string timestamp, string payload)
+        {
+            var secretBytes = Encoding.UTF8.GetBytes(secret);
+            var payloadBytes = Encoding.UTF8.GetBytes($"{timestamp}.{payload}");
+
+            var cryptographer = new HMACSHA256(secretBytes);
+            var hash = cryptographer.ComputeHash(payloadBytes);
+
+            return BitConverter.ToString(hash).Replace("-", "").ToLower(); ;
         }
     }
 }
