6066aebb8be6fa29d22a6423fc9993b6394eb05a
jaymedavis <jaymedavis@gmail.com>
2017-05-05 13:23:05 -0400

    finished source card response in stripesource

--- src/Stripe.Tests.XUnit/sources/creating_and_updating_card_source.cs
+++ src/Stripe.Tests.XUnit/sources/creating_and_updating_card_source.cs
@@ -25,48 +25,7 @@ namespace Stripe.Tests.Xunit
         public void source_should_not_be_null()
         {
             Source.Should().NotBeNull();
+            Source.Card.Should().NotBeNull();
         }
-
-        // ***** example of the full response
-
-        //{
-        //    "id": "src_1AG8C1LWlyqCpbLpb4Vzk58R",
-        //    "object": "source",
-        //    "amount": 1,
-        //    "client_secret": "src_client_secret_WvVqH07h6yiUuR2PP3XnDZdj",
-        //    "created": 1493997485,
-        //    "currency": "usd",
-        //    "flow": "none",
-        //    "livemode": false,
-        //    "metadata": {},
-        //    "owner": {
-        //        "address": null,
-        //        "email": null,
-        //        "name": "Jason Isbell",
-        //        "phone": null,
-        //        "verified_address": null,
-        //        "verified_email": null,
-        //        "verified_name": null,
-        //        "verified_phone": null
-        //    },
-        //    "status": "chargeable",
-        //    "type": "card",
-        //    "usage": "reusable",
-        //    "card": {
-        //        "exp_month": 10,
-        //        "exp_year": 2025,
-        //        "address_line1_check": "unchecked",
-        //        "address_zip_check": "unchecked",
-        //        "brand": "Visa",
-        //        "country": "US",
-        //        "cvc_check": "unchecked",
-        //        "fingerprint": "s7wTOkEvTa7J0PFf",
-        //        "funding": "credit",
-        //        "last4": "4242",
-        //        "three_d_secure": "optional",
-        //        "tokenization_method": null,
-        //        "dynamic_last4": null
-        //    }
-        //}
     }
 }
---
+++ src/Stripe.net/Entities/Sources/StripeSourceCard.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+
+namespace Stripe
+{
+    public class StripeSourceCard : StripeEntity
+    {
+        [JsonProperty("exp_month")]
+        public int ExpirationMonth { get; set; }
+
+        [JsonProperty("exp_year")]
+        public int ExpirationYear { get; set; }
+
+        [JsonProperty("address_line1_check")]
+        public string AddressLine1Check { get; set; }
+
+        [JsonProperty("address_zip_check")]
+        public string AddressZipCheck { get; set; }
+
+        [JsonProperty("brand")]
+        public string Brand { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("cvc_check")]
+        public string CvcCheck { get; set; }
+
+        [JsonProperty("fingerprint")]
+        public string Fingerprint { get; set; }
+
+        [JsonProperty("funding")]
+        public string Funding { get; set; }
+        
+        [JsonProperty("last4")]
+        public string Last4 { get; set; }
+
+        [JsonProperty("three_d_secure")]
+        public string ThreeDSecure { get; set; }
+
+        [JsonProperty("tokenization_method")]
+        public string TokenizationMethod { get; set; }
+
+        [JsonProperty("dynamic_last4")]
+        public string DynamicLast4 { get; set; }
+    }
+} 
