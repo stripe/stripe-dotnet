877aca5ab94e83a391c501497717c86727d40929
Carlos Boeing <carlosboeing@gmail.com>
2017-05-08 20:08:13 +1000

    Added properties Kana and Kanji to Legal Entity for Japan

--- src/Stripe.net/Entities/StripeLegalEntity.cs
+++ src/Stripe.net/Entities/StripeLegalEntity.cs
@@ -11,8 +11,20 @@ namespace Stripe
         [JsonProperty("address")]
         public StripeAddress Address { get; set; }
 
+        [JsonProperty("address_kana")]
+        public StripeAddress AddressKana { get; set; }
+
+        [JsonProperty("address_kanji")]
+        public StripeAddress AddressKanji { get; set; }
+
         [JsonProperty("business_name")]
         public string BusinessName { get; set; }
+
+        [JsonProperty("business_name_kana")]
+        public string BusinessNameKana { get; set; }
+
+        [JsonProperty("business_name_kanji")]
+        public string BusinessNameKanji { get; set; }
 
         /// <summary>
         /// Whether the business ID number of the legal entity has been provided.
@@ -32,14 +44,32 @@ namespace Stripe
         [JsonProperty("first_name")]
         public string FirstName { get; set; }
 
+        [JsonProperty("first_name_kana")]
+        public string FirstNameKana { get; set; }
+
+        [JsonProperty("first_name_kanji")]
+        public string FirstNameKanji { get; set; }
+
         [JsonProperty("gender")]
         public string Gender { get; set; }
 
         [JsonProperty("last_name")]
         public string LastName { get; set; }
 
+        [JsonProperty("last_name_kana")]
+        public string LastNameKana { get; set; }
+
+        [JsonProperty("last_name_kanji")]
+        public string LastNameKanji { get; set; }
+
         [JsonProperty("personal_address")]
         public StripeAddress PersonalAddress { get; set; }
+
+        [JsonProperty("personal_address_kana")]
+        public StripeAddress PersonalAddressKana { get; set; }
+
+        [JsonProperty("personal_address_kanji")]
+        public StripeAddress PersonalAddressKanji { get; set; }
 
         [JsonProperty("personal_id_number_provided")]
         public bool PersonalIdNumberProvided { get; set; }
