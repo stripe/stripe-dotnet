cdcc44e473de54894e60d3ff1142155b4191cd3d
Jayme Davis <jaymedavis@gmail.com>
2017-05-09 20:55:19 -0400

    added the (expandable) StripeApplication to StripeApplicationFee

---
+++ src/Stripe.net/Entities/StripeApplication.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Stripe
+{
+    public class StripeApplication : StripeEntityWithId
+    {
+        [JsonProperty("object")]
+        public string Object { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+     }
+}
