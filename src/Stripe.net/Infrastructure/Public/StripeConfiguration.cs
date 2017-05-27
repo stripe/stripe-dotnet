0bace9b440bc8b6d2be40ff9d5f244b2ece076c9
Remi Jannel <remi@stripe.com>
2017-05-27 11:04:10 -0400

    Upgrade Stripe.net to the latest API version

--- src/Stripe.net/Infrastructure/Public/StripeConfiguration.cs
+++ src/Stripe.net/Infrastructure/Public/StripeConfiguration.cs
@@ -7,7 +7,7 @@ namespace Stripe
 {
     public static class StripeConfiguration
     {
-        public static string StripeApiVersion = "2017-04-06";
+        public static string StripeApiVersion = "2017-05-25";
         public static string StripeNetVersion { get; }
 
         /// <summary>
