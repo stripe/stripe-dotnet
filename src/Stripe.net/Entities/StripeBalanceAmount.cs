5362662a36a6d6c34e048c19878b87e625a4a518
jaymedavis <jaymedavis@gmail.com>
2017-06-26 20:32:20 -0400

    changed balance amount back to int

--- src/Stripe.net/Entities/StripeBalanceAmount.cs
+++ src/Stripe.net/Entities/StripeBalanceAmount.cs
@@ -5,7 +5,7 @@ namespace Stripe
     public class StripeBalanceAmount : StripeEntity
     {
         [JsonProperty("amount")]
-        public long Amount { get; set; }
+        public int Amount { get; set; }
 
         [JsonProperty("currency")]
         public string Currency { get; set; }
