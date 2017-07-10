8869b04715382be2f7fbe076bd7de326fc2bab59
Remi Jannel <remi@stripe.com>
2017-07-10 17:17:31 -0400

    Amount is optional on source creation for reusable sources

---
+++ src/Stripe.Tests.XUnit/sources/creating_reusable_card_source.cs
@@ -0,0 +1,29 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Stripe.Tests.Xunit
+{
+    public class creating_reusable_card_source
+    {
+        private StripeSource Source { get; set; }
+
+        public creating_reusable_card_source()
+        {
+            var options = new StripeSourceCreateOptions
+            {
+                Type = StripeSourceType.Card,
+                Usage = StripeSourceUsage.Reusable,
+                Token = Cache.GetToken().Id,
+            };
+
+            Source = new StripeSourceService(Cache.ApiKey).Create(options);
+        }
+
+        [Fact]
+        public void source_should_not_be_null()
+        {
+            Source.Should().NotBeNull();
+            Source.Card.Should().NotBeNull();
+        }
+    }
+}
