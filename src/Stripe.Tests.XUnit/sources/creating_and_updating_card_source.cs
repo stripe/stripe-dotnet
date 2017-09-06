365b544be41890ae9544b52a33b0fba98adea6ba
Remi Jannel <remi@stripe.com>
2017-09-06 11:03:23 +0200

    Added support for updating a Card Source expiration date

--- src/Stripe.Tests.XUnit/sources/creating_and_updating_card_source.cs
+++ src/Stripe.Tests.XUnit/sources/creating_and_updating_card_source.cs
@@ -5,27 +5,52 @@ namespace Stripe.Tests.Xunit
 {
     public class creating_and_updating_card_source
     {
-        private StripeSource Source { get; set; }
+        public StripeSourceCreateOptions SourceCardCreateOptions { get; }
+        public StripeSourceUpdateOptions SourceCardUpdateOptions { get; }
+
+        public StripeSource SourceCard { get; }
+        public StripeSource SourceCardUpdated { get; }
 
         public creating_and_updating_card_source()
         {
-            var options = new StripeSourceCreateOptions
+            SourceCardCreateOptions = new StripeSourceCreateOptions
             {
                 Type = StripeSourceType.Card,
-                Amount = 100,
-                Currency = "usd",
-                Token = "tok_visa",
-                RedirectReturnUrl = "http://no.where/webhooks"
+                Token = "tok_visa"
             };
 
-            Source = new StripeSourceService(Cache.ApiKey).Create(options);
+            SourceCardUpdateOptions = new StripeSourceUpdateOptions
+            {
+                Card = new StripeSourceCardUpdateOptions
+                {
+                    ExpirationMonth = 12,
+                    ExpirationYear = 2028
+                }
+            };
+
+            var service = new StripeSourceService(Cache.ApiKey);
+            SourceCard = service.Create(SourceCardCreateOptions);
+            SourceCardUpdated = service.Update(SourceCard.Id, SourceCardUpdateOptions);
         }
 
         [Fact]
         public void source_should_not_be_null()
         {
-            Source.Should().NotBeNull();
-            Source.Card.Should().NotBeNull();
+            SourceCard.Should().NotBeNull();
+            SourceCard.Card.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void source_should_be_the_right_type()
+        {
+            SourceCard.Type.Should().Be(SourceCardCreateOptions.Type);
+        }
+
+        [Fact]
+        public void source_updated_should_have_the_correct_expiration_date()
+        {
+            SourceCard.Card.ExpirationMonth.Should().Be(SourceCardUpdateOptions.Card.ExpirationMonth);
+            SourceCard.Card.ExpirationYear.Should().Be(SourceCardUpdateOptions.Card.ExpirationYear);
         }
     }
 }
