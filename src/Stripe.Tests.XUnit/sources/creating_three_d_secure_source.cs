952ee86a105e7bb136ba84983a18a91cc64385cf
Remi Jannel <remi@stripe.com>
2017-12-04 08:15:10 -0800

    Allow setting three_d_secure[customer] on 3DS Source creation

--- src/Stripe.Tests.XUnit/sources/creating_three_d_secure_source.cs
+++ src/Stripe.Tests.XUnit/sources/creating_three_d_secure_source.cs
@@ -7,36 +7,54 @@ namespace Stripe.Tests.Xunit
     {
         private StripeSource Source { get; }
         private StripeSource ThreeDSecure { get; }
+        private StripeCustomer Customer { get; }
+        private StripeSource SourceCustomer { get; }
+        private StripeSource ThreeDSecureCustomer { get; }
 
         public creating_three_d_secure_source()
         {
-            Source = new StripeSourceService(Cache.ApiKey).Create(
-                new StripeSourceCreateOptions
+            var sourceCreateOptions = new StripeSourceCreateOptions
+            {
+                Type = StripeSourceType.Card,
+                Amount = 1234,
+                Currency = "eur",
+                RedirectReturnUrl = "http://no.where/webhooks",
+                Card = new StripeCreditCardOptions
                 {
-                    Type = StripeSourceType.Card,
-                    Amount = 8675309,
-                    Currency = "eur",
-                    RedirectReturnUrl = "http://no.where/webhooks",
-                    Card = new StripeCreditCardOptions
-                    {
-                        // Using PAN as we don't have a 3DS test token yet
-                        Number = "4000000000003063",
-                        ExpirationMonth = 12,
-                        ExpirationYear = 2020
-                    }
+                    // Using PAN as we don't have a 3DS test token yet
+                    Number = "4000000000003063",
+                    ExpirationMonth = 12,
+                    ExpirationYear = 2020
                 }
-            );
+            };
 
-            ThreeDSecure = new StripeSourceService(Cache.ApiKey).Create(
-                new StripeSourceCreateOptions
-                {
-                    Type = StripeSourceType.ThreeDSecure,
-                    Amount = 8675309,
-                    Currency = "eur",
-                    RedirectReturnUrl = "http://no.where/webhooks",
-                    ThreeDSecureCardOrSourceId = Source.Id
-                }
-            );
+            var threeDSCreateOptions = new StripeSourceCreateOptions
+            {
+                Type = StripeSourceType.ThreeDSecure,
+                Amount = 8675309,
+                Currency = "eur",
+                RedirectReturnUrl = "http://no.where/webhooks",
+            };
+
+            var sourceService = new StripeSourceService(Cache.ApiKey);
+            var customerService = new StripeCustomerService(Cache.ApiKey);
+            Customer = customerService.Create(new StripeCustomerCreateOptions{});
+
+            Source = sourceService.Create(sourceCreateOptions);
+            threeDSCreateOptions.ThreeDSecureCardOrSourceId = Source.Id;
+            ThreeDSecure = sourceService.Create(threeDSCreateOptions);
+
+            SourceCustomer = sourceService.Create(sourceCreateOptions);
+            var SourceAttachOptions = new StripeSourceAttachOptions
+            {
+                Source = SourceCustomer.Id
+            };
+            sourceService.Attach(Customer.Id, SourceAttachOptions);
+
+            threeDSCreateOptions.ThreeDSecureCardOrSourceId = SourceCustomer.Id;
+            threeDSCreateOptions.ThreeDSecureCustomer = Customer.Id;
+            ThreeDSecureCustomer = sourceService.Create(threeDSCreateOptions);
+
 
             // from here, you have to go to the threeDSecure.Redirect.Url and click success
 
@@ -56,6 +74,11 @@ namespace Stripe.Tests.Xunit
             Source.Should().NotBeNull();
             ThreeDSecure.Should().NotBeNull();
             ThreeDSecure.Redirect.Url.Should().NotBeNull();
+
+            SourceCustomer.Should().NotBeNull();
+            ThreeDSecureCustomer.Should().NotBeNull();
+            ThreeDSecureCustomer.ThreeDSecure.CardId.Should().Be(SourceCustomer.Id);
+            ThreeDSecureCustomer.ThreeDSecure.CustomerId.Should().Be(Customer.Id);
         }
     }
 }
