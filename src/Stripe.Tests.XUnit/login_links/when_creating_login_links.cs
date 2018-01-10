e3dd87c9388d0456eed296f6fe6b8eaf8702ed0a
Remi Jannel <remi@stripe.com>
2018-01-10 18:47:37 -0500

    Fix synchronous login link creation
    * The method for synchronous Creation was not working at all and is fixed
    * Remove old test approach from the old framework
    * Moved new test to XUnit
    * Comment out the test as it would break everyone's local test suite.

---
+++ src/Stripe.Tests.XUnit/login_links/when_creating_login_links.cs
@@ -0,0 +1,29 @@
+// This test is commented out as it require a real Express account on your platform
+// before you can create a login link for this account. Every developer would have
+// to change this code to have the test suite pass on their account.
+//
+//using System;
+//using FluentAssertions;
+//using Xunit;
+//
+//namespace Stripe.Tests.Xunit
+//{
+//    public class creating_login_links
+//    {
+//        StripeLoginLink LoginLink;
+//
+//        public creating_login_links()
+//        {
+//            // This is the id of an Express account for the account associated with the test suite.
+//            // When testing locally you need to put an id valid for your platform.
+//            LoginLink = new StripeLoginLinkService(Cache.ApiKey).Create("acct_1ATVm2ETkVWzzLxp");
+//        }
+//
+//        [Fact]
+//        public void login_link_has_url()
+//        {
+//            LoginLink.Url.Should().NotBeNull();
+//            LoginLink.Url.Should().Contain("http");
+//        }
+//    }
+//}
