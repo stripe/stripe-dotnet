6bab8a26232822d8f3840522d6166bbadce0412f
Olivier Bellone <ob@stripe.com>
2018-02-26 15:48:50 +0100

    Update subscription request parameters and add documentation

--- src/Stripe.net/Services/Subscriptions/StripeSubscriptionItemOption.cs
+++ src/Stripe.net/Services/Subscriptions/StripeSubscriptionItemOption.cs
@@ -4,9 +4,15 @@ namespace Stripe
 {
     public class StripeSubscriptionItemOption : INestedOptions
     {
+        /// <summary>
+        /// Plan ID for this item.
+        /// </summary>
         [JsonProperty("plan")]
         public string PlanId { get; set; }
 
+        /// <summary>
+        /// Quantity for this item.
+        /// </summary>
         [JsonProperty("quantity")]
         public int? Quantity { get; set; }
     }
