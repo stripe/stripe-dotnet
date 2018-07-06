dfc3f25a6b5cb4703536300f4bccaddf587b92f7
Remi Jannel <remi@stripe.com>
2018-07-06 08:20:26 -0400

    Add missing property/params to the InvoiceItem resource
    * Add unit_amount everywhere
    * Add quantity on create and update
    * Remove currency on update as it's not valid

--- src/Stripe.net/Services/InvoiceItems/StripeInvoiceItemCreateOptions.cs
+++ src/Stripe.net/Services/InvoiceItems/StripeInvoiceItemCreateOptions.cs
@@ -5,20 +5,14 @@ namespace Stripe
 {
     public class StripeInvoiceItemCreateOptions : StripeBaseOptions, ISupportMetadata
     {
-        [JsonProperty("customer")]
-        public string CustomerId { get; set; }
-
         [JsonProperty("amount")]
         public int Amount { get; set; }
 
         [JsonProperty("currency")]
         public string Currency { get; set; }
 
-        [JsonProperty("invoice")]
-        public string InvoiceId { get; set; }
-
-        [JsonProperty("subscription")]
-        public string SubscriptionId { get; set; }
+        [JsonProperty("customer")]
+        public string CustomerId { get; set; }
 
         [JsonProperty("description")]
         public string Description { get; set; }
@@ -26,7 +20,19 @@ namespace Stripe
         [JsonProperty("discountable")]
         public bool Discountable { get; set; }
 
+        [JsonProperty("invoice")]
+        public string InvoiceId { get; set; }
+
         [JsonProperty("metadata")]
         public Dictionary<string, string> Metadata { get; set; }
+
+        [JsonProperty("quantity")]
+        public int? Quantity { get; set; }
+
+        [JsonProperty("subscription")]
+        public string SubscriptionId { get; set; }
+
+        [JsonProperty("unit_amount")]
+        public int? UnitAmount { get; set; }
     }
 }
