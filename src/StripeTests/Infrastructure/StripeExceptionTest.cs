c0e55c004f4016a43600bc862dc3380cef8871d1
Remi Jannel <remi@stripe.com>
2018-08-02 11:18:47 +0200

    Add exception tests

---
+++ src/StripeTests/Infrastructure/StripeExceptionTest.cs
@@ -0,0 +1,27 @@
+namespace StripeTests
+{
+    using System.Linq;
+    using System.Net.Http;
+    using Newtonsoft.Json.Linq;
+    using Stripe;
+    using Stripe.Infrastructure;
+    using Xunit;
+
+    public class StripeExceptionTest : BaseStripeTest
+    {
+        public StripeExceptionTest()
+        {
+        }
+
+        [Fact]
+        public void SetsStripeResponse()
+        {
+            var exception = Assert.Throws<StripeException>(() =>
+                new StripeCouponService().Create(new StripeCouponCreateOptions()));
+
+            Assert.NotNull(exception);
+            Assert.NotNull(exception.StripeError);
+            Assert.NotNull(exception.StripeError.StripeResponse);
+        }
+    }
+}
