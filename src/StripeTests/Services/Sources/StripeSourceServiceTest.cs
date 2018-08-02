0a507cf5babdb572947787170444a4990127cc87
Remi Jannel <remi@stripe.com>
2018-08-02 11:21:29 +0200

    Move sources to stripe-mock

---
+++ src/StripeTests/Services/Sources/StripeSourceServiceTest.cs
@@ -0,0 +1,50 @@
+namespace StripeTests
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Stripe;
+    using Xunit;
+
+    public class StripeSourceServiceTest : BaseStripeTest
+    {
+        private const string SourceId = "src_123";
+
+        private StripeSourceService service;
+        private StripeSourceCreateOptions createOptions;
+        private StripeSourceUpdateOptions updateOptions;
+        private StripeSourceListOptions listOptions;
+
+        public StripeSourceServiceTest()
+        {
+            this.service = new StripeSourceService();
+
+            this.createOptions = new StripeSourceCreateOptions
+            {
+                Type = StripeSourceType.AchCreditTransfer,
+                Currency = "usd"
+            };
+
+            this.updateOptions = new StripeSourceUpdateOptions
+            {
+                Metadata = new Dictionary<string, string>()
+                {
+                    { "key", "value" },
+                },
+            };
+
+            this.listOptions = new StripeSourceListOptions()
+            {
+                Limit = 1,
+            };
+        }
+
+        [Fact]
+        public void Create()
+        {
+            var source = this.service.Create(this.createOptions);
+            Assert.NotNull(source);
+            Assert.Equal("source", source.Object);
+        }
+    }
+}
