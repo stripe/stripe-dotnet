7ca345494f46b8c3e65017f6098c7cc26ec1564f
Remi Jannel <remi@stripe.com>
2018-08-02 10:09:28 +0200

    Move datetime serialization tests

---
+++ src/StripeTests/Infrastructure/SerializationTest.cs
@@ -0,0 +1,42 @@
+namespace StripeTests
+{
+    using System;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using Stripe.Infrastructure;
+    using StripeTests.Infrastructure.TestData;
+    using Xunit;
+
+    public class SerializationTest : BaseStripeTest
+    {
+        public SerializationTest()
+        {
+        }
+
+        [Fact]
+        public void RoundtripDatetime()
+        {
+            var date = DateTime.UtcNow;
+            var roundedDate = date.AddTicks(-1 * (date.Ticks % TimeSpan.TicksPerSecond));
+            var obj = new TestObjectDateTime
+            {
+                Date = roundedDate,
+            };
+
+            var reloaded = JsonConvert.DeserializeObject<TestObjectDateTime>(JsonConvert.SerializeObject(obj));
+            Assert.Equal(reloaded.Date, obj.Date);
+        }
+
+        [Fact]
+        public void HandleNull()
+        {
+            var obj = new TestObjectDateTime
+            {
+                Date = null
+            };
+
+            var reloaded = JsonConvert.DeserializeObject<TestObjectDateTime>(JsonConvert.SerializeObject(obj));
+            Assert.Null(reloaded.Date);
+        }
+    }
+}
