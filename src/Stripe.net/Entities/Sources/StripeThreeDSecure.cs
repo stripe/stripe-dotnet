22c9a40f2de5e424f2634af2b083291d55948356
Pradeep Annepu <pradeepannepu@gmail.com>
2018-09-08 07:39:18 +0530

    added brand and last4

--- src/Stripe.net/Entities/Sources/StripeThreeDSecure.cs
+++ src/Stripe.net/Entities/Sources/StripeThreeDSecure.cs
@@ -12,5 +12,11 @@
 
         [JsonProperty("authenticated")]
         public bool Authenticated { get; set; }
+
+        [JsonProperty("brand")]
+        public string Brand { get; set; }
+
+        [JsonProperty("last4")]
+        public string Last4 { get; set; }
     }
 }
