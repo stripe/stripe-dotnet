578e21ae99af97fff72bff1b3357ce7501336804
Olivier Bellone <ob@stripe.com>
2018-09-16 18:15:39 +0200

    Fix services

--- src/Stripe.net/Services/Sigma/ScheduledQueryRuns/ScheduledQueryRunListOptions.cs
+++ src/Stripe.net/Services/Sigma/ScheduledQueryRuns/ScheduledQueryRunListOptions.cs
@@ -1,10 +1,10 @@
-﻿namespace Stripe
+﻿namespace Stripe.Sigma
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
 
-    public class ScheduledQueryListOptions : ListOptions
+    public class ScheduledQueryRunListOptions : ListOptions
     {
     }
 }
