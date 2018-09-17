754b14f97905593c177aa8fcca5b656a4c935d35
Olivier Bellone <ob@stripe.com>
2018-09-17 10:27:50 +0200

    RefundService fixes

--- src/Stripe.net/Services/Refunds/RefundService.cs
+++ src/Stripe.net/Services/Refunds/RefundService.cs
@@ -8,6 +8,8 @@
 
     public class RefundService : StripeService
     {
+        private static string classUrl = Urls.BaseUrl + "/refunds";
+
         public RefundService()
             : base(null)
         {
@@ -24,70 +26,70 @@
 
         public bool ExpandFailureBalanceTransaction { get; set; }
 
-        public virtual Refund Create(string chargeId, RefundCreateOptions createOptions = null, RequestOptions requestOptions = null)
+        public virtual Refund Create(RefundCreateOptions createOptions = null, RequestOptions requestOptions = null)
         {
             return Mapper<Refund>.MapFromJson(
                 Requestor.PostString(
-                    this.ApplyAllParameters(createOptions, $"{Urls.Charges}/{chargeId}/refunds", false),
+                    this.ApplyAllParameters(createOptions, classUrl, false),
                     this.SetupRequestOptions(requestOptions)));
+        }
+
+        public virtual async Task<Refund> CreateAsync(RefundCreateOptions createOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Mapper<Refund>.MapFromJson(
+                await Requestor.PostStringAsync(
+                    this.ApplyAllParameters(createOptions, classUrl, false),
+                    this.SetupRequestOptions(requestOptions),
+                    cancellationToken).ConfigureAwait(false));
         }
 
         public virtual Refund Get(string refundId, RequestOptions requestOptions = null)
         {
             return Mapper<Refund>.MapFromJson(
                 Requestor.GetString(
-                    this.ApplyAllParameters(null, $"{Urls.BaseUrl}/refunds/{refundId}"),
+                    this.ApplyAllParameters(null, $"{classUrl}/{refundId}"),
                     this.SetupRequestOptions(requestOptions)));
-        }
-
-        public virtual Refund Update(string refundId, RefundUpdateOptions updateOptions, RequestOptions requestOptions = null)
-        {
-            return Mapper<Refund>.MapFromJson(
-                Requestor.PostString(
-                    this.ApplyAllParameters(updateOptions, $"{Urls.BaseUrl}/refunds/{refundId}"),
-                    this.SetupRequestOptions(requestOptions)));
-        }
-
-        public virtual StripeList<Refund> List(RefundListOptions listOptions = null, RequestOptions requestOptions = null)
-        {
-            return Mapper<StripeList<Refund>>.MapFromJson(
-                Requestor.GetString(
-                    this.ApplyAllParameters(listOptions, $"{Urls.BaseUrl}/refunds", true),
-                    this.SetupRequestOptions(requestOptions)));
-        }
-
-        public virtual async Task<Refund> CreateAsync(string chargeId, RefundCreateOptions createOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            return Mapper<Refund>.MapFromJson(
-                await Requestor.PostStringAsync(
-                    this.ApplyAllParameters(createOptions, $"{Urls.Charges}/{chargeId}/refunds", false),
-                    this.SetupRequestOptions(requestOptions),
-                    cancellationToken).ConfigureAwait(false));
         }
 
         public virtual async Task<Refund> GetAsync(string refundId, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
         {
             return Mapper<Refund>.MapFromJson(
                 await Requestor.GetStringAsync(
-                    this.ApplyAllParameters(null, $"{Urls.BaseUrl}/refunds/{refundId}"),
+                    this.ApplyAllParameters(null, $"{classUrl}/{refundId}"),
                     this.SetupRequestOptions(requestOptions),
                     cancellationToken).ConfigureAwait(false));
         }
 
-        public virtual async Task<Refund> UpdateAsync(string refundId, RefundUpdateOptions updateOptions, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
+        public virtual StripeList<Refund> List(RefundListOptions listOptions = null, RequestOptions requestOptions = null)
         {
-            return Mapper<Refund>.MapFromJson(
-                await Requestor.PostStringAsync(
-                    this.ApplyAllParameters(updateOptions, $"{Urls.BaseUrl}/refunds/{refundId}"),
-                    this.SetupRequestOptions(requestOptions),
-                    cancellationToken).ConfigureAwait(false));
+            return Mapper<StripeList<Refund>>.MapFromJson(
+                Requestor.GetString(
+                    this.ApplyAllParameters(listOptions, classUrl, true),
+                    this.SetupRequestOptions(requestOptions)));
         }
 
         public virtual async Task<StripeList<Refund>> ListAsync(RefundListOptions listOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
         {
             return Mapper<StripeList<Refund>>.MapFromJson(
                 await Requestor.GetStringAsync(
-                    this.ApplyAllParameters(listOptions, $"{Urls.BaseUrl}/refunds", true),
+                    this.ApplyAllParameters(listOptions, classUrl, true),
+                    this.SetupRequestOptions(requestOptions),
+                    cancellationToken).ConfigureAwait(false));
+        }
+
+        public virtual Refund Update(string refundId, RefundUpdateOptions updateOptions, RequestOptions requestOptions = null)
+        {
+            return Mapper<Refund>.MapFromJson(
+                Requestor.PostString(
+                    this.ApplyAllParameters(updateOptions, $"{classUrl}/{refundId}"),
+                    this.SetupRequestOptions(requestOptions)));
+        }
+
+        public virtual async Task<Refund> UpdateAsync(string refundId, RefundUpdateOptions updateOptions, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Mapper<Refund>.MapFromJson(
+                await Requestor.PostStringAsync(
+                    this.ApplyAllParameters(updateOptions, $"{classUrl}/{refundId}"),
                     this.SetupRequestOptions(requestOptions),
                     cancellationToken).ConfigureAwait(false));
         }
