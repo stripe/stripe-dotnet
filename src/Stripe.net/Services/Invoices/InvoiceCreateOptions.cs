203b563725c9e22f8a353bfcc27b11a52b5770fb
Olivier Bellone <ob@stripe.com>
2018-09-17 10:15:40 +0200

    InvoiceService fixes

--- src/Stripe.net/Services/Invoices/InvoiceCreateOptions.cs
+++ src/Stripe.net/Services/Invoices/InvoiceCreateOptions.cs
@@ -7,17 +7,33 @@
 
     public class InvoiceCreateOptions : BaseOptions, ISupportMetadata
     {
+        /// <summary>
+        /// A fee in cents that will be applied to the invoice and transferred to the application
+        /// owner’s Stripe account. The request must be made with an OAuth key or the Stripe-Account
+        /// header in order to take an application fee. For more information, see the application
+        /// fees <see href="https://stripe.com/docs/connect/subscriptions#working-with-invoices">documentation</see>.
+        /// </summary>
         [JsonProperty("application_fee")]
         public int? ApplicationFee { get; set; }
 
         /// <summary>
-        /// One of <see cref="Billing" />. When charging automatically, Stripe will attempt to pay this invoice using the default source attached to the customer. When sending an invoice, Stripe will email this invoice to the customer with payment instructions. Defaults to charge_automatically.
+        /// One of <see cref="Billing" />. When charging automatically, Stripe will attempt to pay
+        /// this invoice using the default source attached to the customer. When sending an invoice,
+        /// Stripe will email this invoice to the customer with payment instructions. Defaults to
+        /// <c>charge_automatically</c>.
         /// </summary>
         [JsonProperty("billing")]
         public Billing? Billing { get; set; }
 
         /// <summary>
-        /// The number of days from which the invoice is created until it is due. Only valid for invoices where billing=send_invoice.
+        /// REQUIRED
+        /// </summary>
+        [JsonProperty("customer")]
+        public string CustomerId { get; set; }
+
+        /// <summary>
+        /// The number of days from which the invoice is created until it is due. Only valid for
+        /// invoices where <c>billing=send_invoice</c>.
         /// </summary>
         [JsonProperty("days_until_due")]
         public int? DaysUntilDue { get; set; }
@@ -26,7 +42,8 @@
         public string Description { get; set; }
 
         /// <summary>
-        /// The date on which payment for this invoice is due. Only valid for invoices where billing=send_invoice.
+        /// The date on which payment for this invoice is due. Only valid for invoices where
+        /// <c>billing=send_invoice</c>.
         /// </summary>
         [JsonProperty("due_date")]
         public DateTime? DueDate { get; set; }
@@ -34,12 +51,26 @@
         [JsonProperty("metadata")]
         public Dictionary<string, string> Metadata { get; set; }
 
+        /// <summary>
+        /// Extra information about a charge for the customer’s credit card statement. It must
+        /// contain at least one letter. If not specified and this invoice is part of a
+        /// subscription, the default <c>statement_descriptor</c> will be set to the first
+        /// subscription item’s product’s <c>statement_descriptor</c>.
+        /// </summary>
         [JsonProperty("statement_descriptor")]
         public string StatementDescriptor { get; set; }
 
+        /// <summary>
+        /// The ID of the subscription to invoice. If not set, the created invoice will include all
+        /// pending invoice items for the customer. If set, the created invoice will exclude pending
+        /// invoice items that pertain to other subscriptions.
+        /// </summary>
         [JsonProperty("subscription")]
         public string SubscriptionId { get; set; }
 
+        /// <summary>
+        /// The percent tax rate applied to the invoice, represented as a decimal number.
+        /// </summary>
         [JsonProperty("tax_percent")]
         public decimal? TaxPercent { get; set; }
     }
--- src/Stripe.net/Services/Invoices/InvoiceService.cs
+++ src/Stripe.net/Services/Invoices/InvoiceService.cs
@@ -23,73 +23,28 @@
 
         public bool ExpandSubscription { get; set; }
 
+        public virtual Invoice Create(InvoiceCreateOptions createOptions = null, RequestOptions requestOptions = null)
+        {
+            return Mapper<Invoice>.MapFromJson(
+                Requestor.PostString(
+                    this.ApplyAllParameters(createOptions, Urls.Invoices, false),
+                    this.SetupRequestOptions(requestOptions)));
+        }
+
+        public virtual async Task<Invoice> CreateAsync(InvoiceCreateOptions createOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Mapper<Invoice>.MapFromJson(
+                await Requestor.PostStringAsync(
+                    this.ApplyAllParameters(createOptions, Urls.Invoices, false),
+                    this.SetupRequestOptions(requestOptions),
+                    cancellationToken).ConfigureAwait(false));
+        }
+
         public virtual Invoice Get(string invoiceId, RequestOptions requestOptions = null)
         {
             return Mapper<Invoice>.MapFromJson(
                 Requestor.GetString(
                     this.ApplyAllParameters(null, $"{Urls.Invoices}/{invoiceId}", false),
-                    this.SetupRequestOptions(requestOptions)));
-        }
-
-        public virtual Invoice Upcoming(string customerId, UpcomingInvoiceOptions upcomingOptions = null, RequestOptions requestOptions = null)
-        {
-            var url = ParameterBuilder.ApplyParameterToUrl($"{Urls.Invoices}/upcoming", "customer", customerId);
-
-            return Mapper<Invoice>.MapFromJson(
-                Requestor.GetString(
-                    this.ApplyAllParameters(upcomingOptions, url, false),
-                    this.SetupRequestOptions(requestOptions)));
-        }
-
-        public virtual Invoice Update(string invoiceId, InvoiceUpdateOptions updateOptions, RequestOptions requestOptions = null)
-        {
-            return Mapper<Invoice>.MapFromJson(
-                Requestor.PostString(
-                    this.ApplyAllParameters(updateOptions, $"{Urls.Invoices}/{invoiceId}", false),
-                    this.SetupRequestOptions(requestOptions)));
-        }
-
-        public virtual Invoice Pay(string invoiceId, InvoicePayOptions payOptions, RequestOptions requestOptions = null)
-        {
-            return Mapper<Invoice>.MapFromJson(
-                Requestor.PostString(
-                    this.ApplyAllParameters(payOptions, $"{Urls.Invoices}/{invoiceId}/pay", false),
-                    this.SetupRequestOptions(requestOptions)));
-        }
-
-        public virtual StripeList<Invoice> List(InvoiceListOptions listOptions = null, RequestOptions requestOptions = null)
-        {
-            return Mapper<StripeList<Invoice>>.MapFromJson(
-                Requestor.GetString(
-                    this.ApplyAllParameters(listOptions, Urls.Invoices, true),
-                    this.SetupRequestOptions(requestOptions)));
-        }
-
-        public virtual StripeList<InvoiceLineItem> ListLineItems(string invoiceId, InvoiceListLineItemsOptions listOptions = null, RequestOptions requestOptions = null)
-        {
-            return Mapper<StripeList<InvoiceLineItem>>.MapFromJson(
-                Requestor.GetString(
-                    this.ApplyAllParameters(listOptions, $"{Urls.Invoices}/{invoiceId}/lines", true),
-                    this.SetupRequestOptions(requestOptions)));
-        }
-
-        public virtual StripeList<InvoiceLineItem> ListUpcomingLineItems(string customerId, UpcomingInvoiceOptions listOptions = null, RequestOptions requestOptions = null)
-        {
-            var url = ParameterBuilder.ApplyParameterToUrl($"{Urls.Invoices}/upcoming/lines", "customer", customerId);
-
-            return Mapper<StripeList<InvoiceLineItem>>.MapFromJson(
-                Requestor.GetString(
-                    this.ApplyAllParameters(listOptions, url, true),
-                    this.SetupRequestOptions(requestOptions)));
-        }
-
-        public virtual Invoice Create(string customerId, InvoiceCreateOptions createOptions = null, RequestOptions requestOptions = null)
-        {
-            var url = ParameterBuilder.ApplyParameterToUrl(Urls.Invoices, "customer", customerId);
-
-            return Mapper<Invoice>.MapFromJson(
-                Requestor.PostString(
-                    this.ApplyAllParameters(createOptions, url, false),
                     this.SetupRequestOptions(requestOptions)));
         }
 
@@ -102,33 +57,12 @@
                     cancellationToken).ConfigureAwait(false));
         }
 
-        public virtual async Task<Invoice> UpcomingAsync(string customerId, UpcomingInvoiceOptions upcomingOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
+        public virtual StripeList<Invoice> List(InvoiceListOptions listOptions = null, RequestOptions requestOptions = null)
         {
-            var url = ParameterBuilder.ApplyParameterToUrl($"{Urls.Invoices}/upcoming", "customer", customerId);
-
-            return Mapper<Invoice>.MapFromJson(
-                await Requestor.GetStringAsync(
-                    this.ApplyAllParameters(upcomingOptions, url, false),
-                    this.SetupRequestOptions(requestOptions),
-                    cancellationToken).ConfigureAwait(false));
-        }
-
-        public virtual async Task<Invoice> UpdateAsync(string invoiceId, InvoiceUpdateOptions updateOptions, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            return Mapper<Invoice>.MapFromJson(
-                await Requestor.PostStringAsync(
-                    this.ApplyAllParameters(updateOptions, $"{Urls.Invoices}/{invoiceId}", false),
-                    this.SetupRequestOptions(requestOptions),
-                    cancellationToken).ConfigureAwait(false));
-        }
-
-        public virtual async Task<Invoice> PayAsync(string invoiceId, InvoicePayOptions payOptions, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            return Mapper<Invoice>.MapFromJson(
-                await Requestor.PostStringAsync(
-                    this.ApplyAllParameters(payOptions, $"{Urls.Invoices}/{invoiceId}/pay", false),
-                    this.SetupRequestOptions(requestOptions),
-                    cancellationToken).ConfigureAwait(false));
+            return Mapper<StripeList<Invoice>>.MapFromJson(
+                Requestor.GetString(
+                    this.ApplyAllParameters(listOptions, Urls.Invoices, true),
+                    this.SetupRequestOptions(requestOptions)));
         }
 
         public virtual async Task<StripeList<Invoice>> ListAsync(InvoiceListOptions listOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
@@ -140,6 +74,14 @@
                     cancellationToken).ConfigureAwait(false));
         }
 
+        public virtual StripeList<InvoiceLineItem> ListLineItems(string invoiceId, InvoiceListLineItemsOptions listOptions = null, RequestOptions requestOptions = null)
+        {
+            return Mapper<StripeList<InvoiceLineItem>>.MapFromJson(
+                Requestor.GetString(
+                    this.ApplyAllParameters(listOptions, $"{Urls.Invoices}/{invoiceId}/lines", true),
+                    this.SetupRequestOptions(requestOptions)));
+        }
+
         public virtual async Task<StripeList<InvoiceLineItem>> ListLineItemsAsync(string invoiceId, InvoiceListLineItemsOptions listOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
         {
             return Mapper<StripeList<InvoiceLineItem>>.MapFromJson(
@@ -149,24 +91,70 @@
                     cancellationToken).ConfigureAwait(false));
         }
 
-        public virtual async Task<StripeList<InvoiceLineItem>> ListUpcomingLineItemsAsync(string customerId, UpcomingInvoiceOptions listOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
+        public virtual StripeList<InvoiceLineItem> ListUpcomingLineItems(UpcomingInvoiceOptions listOptions = null, RequestOptions requestOptions = null)
         {
-            var url = ParameterBuilder.ApplyParameterToUrl($"{Urls.Invoices}/upcoming/lines", "customer", customerId);
+            return Mapper<StripeList<InvoiceLineItem>>.MapFromJson(
+                Requestor.GetString(
+                    this.ApplyAllParameters(listOptions, $"{Urls.Invoices}/upcoming/lines", true),
+                    this.SetupRequestOptions(requestOptions)));
+        }
 
+        public virtual async Task<StripeList<InvoiceLineItem>> ListUpcomingLineItemsAsync(UpcomingInvoiceOptions listOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
             return Mapper<StripeList<InvoiceLineItem>>.MapFromJson(
                 await Requestor.GetStringAsync(
-                    this.ApplyAllParameters(listOptions, url, true),
+                    this.ApplyAllParameters(listOptions, $"{Urls.Invoices}/upcoming/lines", true),
                     this.SetupRequestOptions(requestOptions),
                     cancellationToken).ConfigureAwait(false));
         }
 
-        public virtual async Task<Invoice> CreateAsync(string customerId, InvoiceCreateOptions createOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
+        public virtual Invoice Pay(string invoiceId, InvoicePayOptions payOptions, RequestOptions requestOptions = null)
         {
-            var url = ParameterBuilder.ApplyParameterToUrl(Urls.Invoices, "customer", customerId);
+            return Mapper<Invoice>.MapFromJson(
+                Requestor.PostString(
+                    this.ApplyAllParameters(payOptions, $"{Urls.Invoices}/{invoiceId}/pay", false),
+                    this.SetupRequestOptions(requestOptions)));
+        }
 
+        public virtual async Task<Invoice> PayAsync(string invoiceId, InvoicePayOptions payOptions, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
             return Mapper<Invoice>.MapFromJson(
                 await Requestor.PostStringAsync(
-                    this.ApplyAllParameters(createOptions, url),
+                    this.ApplyAllParameters(payOptions, $"{Urls.Invoices}/{invoiceId}/pay", false),
+                    this.SetupRequestOptions(requestOptions),
+                    cancellationToken).ConfigureAwait(false));
+        }
+
+        public virtual Invoice Upcoming(UpcomingInvoiceOptions upcomingOptions = null, RequestOptions requestOptions = null)
+        {
+            return Mapper<Invoice>.MapFromJson(
+                Requestor.GetString(
+                    this.ApplyAllParameters(upcomingOptions, $"{Urls.Invoices}/upcoming", false),
+                    this.SetupRequestOptions(requestOptions)));
+        }
+
+        public virtual async Task<Invoice> UpcomingAsync(UpcomingInvoiceOptions upcomingOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Mapper<Invoice>.MapFromJson(
+                await Requestor.GetStringAsync(
+                    this.ApplyAllParameters(upcomingOptions, $"{Urls.Invoices}/upcoming", false),
+                    this.SetupRequestOptions(requestOptions),
+                    cancellationToken).ConfigureAwait(false));
+        }
+
+        public virtual Invoice Update(string invoiceId, InvoiceUpdateOptions updateOptions, RequestOptions requestOptions = null)
+        {
+            return Mapper<Invoice>.MapFromJson(
+                Requestor.PostString(
+                    this.ApplyAllParameters(updateOptions, $"{Urls.Invoices}/{invoiceId}", false),
+                    this.SetupRequestOptions(requestOptions)));
+        }
+
+        public virtual async Task<Invoice> UpdateAsync(string invoiceId, InvoiceUpdateOptions updateOptions, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Mapper<Invoice>.MapFromJson(
+                await Requestor.PostStringAsync(
+                    this.ApplyAllParameters(updateOptions, $"{Urls.Invoices}/{invoiceId}", false),
                     this.SetupRequestOptions(requestOptions),
                     cancellationToken).ConfigureAwait(false));
         }
