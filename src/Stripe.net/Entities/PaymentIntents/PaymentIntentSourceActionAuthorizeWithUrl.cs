dc4d5f1aac1166a1d04d46c0b479e8100ec8f042
Olivier Bellone <ob@stripe.com>
2018-10-09 17:11:08 +0200

    Use an interface for PaymentIntentSourceAction

--- src/Stripe.net/Entities/PaymentIntents/PaymentIntentSourceActionAuthorizeWithUrl.cs
+++ src/Stripe.net/Entities/PaymentIntents/PaymentIntentSourceActionAuthorizeWithUrl.cs
@@ -2,7 +2,7 @@ namespace Stripe
 {
     using Newtonsoft.Json;
 
-    public class PaymentIntentSourceActionAuthorizeWithUrl : StripeEntity
+    public class PaymentIntentSourceActionAuthorizeWithUrl : StripeEntity, IPaymentIntentSourceActionValue
     {
         [JsonProperty("url")]
         public string Url { get; set; }
