3aca026f4586d4b2b86c9b26cfef6575ff1df5da
Remi Jannel <remi@stripe.com>
2018-10-15 08:08:06 -0700

    Fix docs and move parameters to the shared struct as they work on both

--- src/Stripe.net/Services/Subscriptions/SubscriptionUpdateOptions.cs
+++ src/Stripe.net/Services/Subscriptions/SubscriptionUpdateOptions.cs
@@ -33,19 +33,13 @@ namespace Stripe
         #endregion
 
         /// <summary>
-        /// If set, the proration will be calculated as though the subscription was updated at the given time. This can be used to apply exactly the same proration that was previewed with <see href="https://stripe.com/docs/api#upcoming_invoice">upcoming invoice</see> endpoint. It can also be used to implement custom proration logic, such as prorating by day instead of by second, by providing the time that you wish to use for proration calculations.
-        /// </summary>
-        [JsonProperty("cancel_at_period_end")]
-        public bool? CancelAtPeriodEnd { get; set; }
-
-        /// <summary>
         /// List of subscription items, each with an attached plan.
         /// </summary>
         [JsonProperty("items")]
         public List<SubscriptionItemUpdateOption> Items { get; set; }
 
         /// <summary>
-        /// Boolean indicating whether this subscription should cancel at the end of the current period.
+        /// If set, the proration will be calculated as though the subscription was updated at the given time. This can be used to apply exactly the same proration that was previewed with <see href="https://stripe.com/docs/api#upcoming_invoice">upcoming invoice</see> endpoint. It can also be used to implement custom proration logic, such as prorating by day instead of by second, by providing the time that you wish to use for proration calculations.
         /// </summary>
         [JsonProperty("proration_date")]
         public DateTime? ProrationDate { get; set; }
