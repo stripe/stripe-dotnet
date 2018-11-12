dfad56389385f7567c34b0a8c49cff435b6bfecd
Olivier Bellone <ob@stripe.com>
2018-11-12 14:16:16 +0100

    Add XML comments on interfaces

--- src/Stripe.net/Entities/_interfaces/IExternalAccount.cs
+++ src/Stripe.net/Entities/_interfaces/IExternalAccount.cs
@@ -3,6 +3,15 @@ namespace Stripe
     /// <summary>
     /// Resources that implement this interface can be used as external accounts, i.e. they can
     /// be attached to a Stripe account to receive payouts.
+    /// <para>Possible concrete classes:</para>
+    /// <list type="bullet">
+    /// <item>
+    /// <description><see cref="BankAccount" /></description>
+    /// </item>
+    /// <item>
+    /// <description><see cref="Card" /></description>
+    /// </item>
+    /// </list>
     /// </summary>
     public interface IExternalAccount : IStripeEntity, IHasId, IHasObject
     {
--- src/Stripe.net/Entities/_interfaces/IPaymentSource.cs
+++ src/Stripe.net/Entities/_interfaces/IPaymentSource.cs
@@ -1,7 +1,23 @@
 namespace Stripe
 {
     /// <summary>
-    /// Resources that implement this interface can be used to create charges.
+    /// Resources that implement this interface can be used as payment sources when creating
+    /// charges.
+    /// <para>Possible concrete classes:</para>
+    /// <list type="bullet">
+    /// <item>
+    /// <description><see cref="Account" /></description>
+    /// </item>
+    /// <item>
+    /// <description><see cref="BankAccount" /></description>
+    /// </item>
+    /// <item>
+    /// <description><see cref="Card" /></description>
+    /// </item>
+    /// <item>
+    /// <description><see cref="Source" /></description>
+    /// </item>
+    /// </list>
     /// </summary>
     public interface IPaymentSource : IStripeEntity, IHasId, IHasObject
     {
