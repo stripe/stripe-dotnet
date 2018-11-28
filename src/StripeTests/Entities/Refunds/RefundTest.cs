a1e44bb3e1a36b94df62b613ea2d09d5917b2641
Remi Jannel <remi@stripe.com>
2018-11-28 10:05:13 -0500

    Add missing properties on the Refund resource

--- src/StripeTests/Entities/Refunds/RefundTest.cs
+++ src/StripeTests/Entities/Refunds/RefundTest.cs
@@ -25,23 +25,31 @@ namespace StripeTests
               "balance_transaction",
               "charge",
               "failure_balance_transaction",
+              "source_transfer_reversal",
+              "transfer_reversal",
             };
 
             string json = GetFixture("/v1/refunds/re_123", expansions);
-            var charge = JsonConvert.DeserializeObject<Refund>(json);
-            Assert.NotNull(charge);
-            Assert.IsType<Refund>(charge);
-            Assert.NotNull(charge.Id);
-            Assert.Equal("refund", charge.Object);
+            var refund = JsonConvert.DeserializeObject<Refund>(json);
+            Assert.NotNull(refund);
+            Assert.IsType<Refund>(refund);
+            Assert.NotNull(refund.Id);
+            Assert.Equal("refund", refund.Object);
 
-            Assert.NotNull(charge.BalanceTransaction);
-            Assert.Equal("balance_transaction", charge.BalanceTransaction.Object);
+            Assert.NotNull(refund.BalanceTransaction);
+            Assert.Equal("balance_transaction", refund.BalanceTransaction.Object);
 
-            Assert.NotNull(charge.Charge);
-            Assert.Equal("charge", charge.Charge.Object);
+            Assert.NotNull(refund.Charge);
+            Assert.Equal("charge", refund.Charge.Object);
 
-            Assert.NotNull(charge.FailureBalanceTransaction);
-            Assert.Equal("balance_transaction", charge.FailureBalanceTransaction.Object);
+            Assert.NotNull(refund.FailureBalanceTransaction);
+            Assert.Equal("balance_transaction", refund.FailureBalanceTransaction.Object);
+
+            Assert.NotNull(refund.SourceTransferReversal);
+            Assert.Equal("transfer_reversal", refund.SourceTransferReversal.Object);
+
+            Assert.NotNull(refund.SourceTransferReversal);
+            Assert.Equal("transfer_reversal", refund.SourceTransferReversal.Object);
         }
     }
 }
