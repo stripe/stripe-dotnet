b2c56b5ee96681b500d3b08dbc0b0d53d33fc4b7
Olivier Bellone <ob@stripe.com>
2018-12-11 19:25:20 -0800

    Replace constants with static read-only properties

--- src/Stripe.net/Constants/FilePurpose.cs
+++ src/Stripe.net/Constants/FilePurpose.cs
@@ -2,12 +2,18 @@ namespace Stripe
 {
     public static class FilePurpose
     {
-        public const string BusinessLogo = "business_logo";
-        public const string DisputeEvidence = "dispute_evidence";
-        public const string IdentityDocument = "identity_document";
-        public const string IncorporationArticle = "incorporation_article";
-        public const string IncorporationDocument = "incorporation_document";
-        public const string PaymentProviderTransfer = "payment_provider_transfer";
-        public const string ProductFeed = "product_feed";
+        public static string BusinessLogo => "business_logo";
+
+        public static string DisputeEvidence => "dispute_evidence";
+
+        public static string IdentityDocument => "identity_document";
+
+        public static string IncorporationArticle => "incorporation_article";
+
+        public static string IncorporationDocument => "incorporation_document";
+
+        public static string PaymentProviderTransfer => "payment_provider_transfer";
+
+        public static string ProductFeed => "product_feed";
     }
 }
--- src/Stripe.net/Constants/TransferFailureCodes.cs
+++ src/Stripe.net/Constants/TransferFailureCodes.cs
@@ -2,15 +2,24 @@ namespace Stripe
 {
     public static class TransferFailureCodes
     {
-        public const string InsufficientFunds = "insufficient_funds";
-        public const string AccountClosed = "account_closed";
-        public const string NoAccount = "no_account";
-        public const string InvalidAccountNumber = "invalid_account_number";
-        public const string DebitNotAuthorized = "debit_not_authorized";
-        public const string BankOwnershipChanged = "bank_ownership_changed";
-        public const string AccountFrozen = "account_frozen";
-        public const string CouldNotProcess = "could_not_process";
-        public const string BankAccountRestricted = "bank_account_restricted";
-        public const string InvalidCurrency = "invalid_currency";
+        public static string InsufficientFunds => "insufficient_funds";
+
+        public static string AccountClosed => "account_closed";
+
+        public static string NoAccount => "no_account";
+
+        public static string InvalidAccountNumber => "invalid_account_number";
+
+        public static string DebitNotAuthorized => "debit_not_authorized";
+
+        public static string BankOwnershipChanged => "bank_ownership_changed";
+
+        public static string AccountFrozen => "account_frozen";
+
+        public static string CouldNotProcess => "could_not_process";
+
+        public static string BankAccountRestricted => "bank_account_restricted";
+
+        public static string InvalidCurrency => "invalid_currency";
     }
 }
