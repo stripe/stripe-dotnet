49f37a250d04b8bd97f696dc8550449f66d32422
Olivier Bellone <ob@stripe.com>
2018-12-17 09:51:44 +0100

    Fix some remaining StyleCop rules violations

--- src/Stripe.net/Enums/Billing.cs
+++ src/Stripe.net/Enums/Billing.cs
@@ -7,9 +7,17 @@ namespace Stripe
     [JsonConverter(typeof(StringEnumConverter))]
     public enum Billing
     {
+        /// <summary>
+        /// When charging automatically, Stripe will attempt to pay this invoice or subscription
+        /// using the default source attached to the customer.
+        /// </summary>
         [EnumMember(Value = "charge_automatically")]
         ChargeAutomatically,
 
+        /// <summary>
+        /// When sending an invoice, Stripe will email your customer an invoice with payment
+        /// instructions.
+        /// </summary>
         [EnumMember(Value = "send_invoice")]
         SendInvoice,
     }
