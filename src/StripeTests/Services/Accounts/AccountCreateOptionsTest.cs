81833eb148de3e86490ae2c55e38c25902298fc1
Olivier Bellone <ob@stripe.com>
2019-01-18 17:43:11 +0100

    Better serialization

--- src/StripeTests/Services/Accounts/AccountCreateOptionsTest.cs
+++ src/StripeTests/Services/Accounts/AccountCreateOptionsTest.cs
@@ -1,23 +1,12 @@
 namespace StripeTests
 {
-    using System;
     using System.Collections.Generic;
-    using System.Threading.Tasks;
-
     using Stripe;
-    using Stripe.Infrastructure;
+    using Stripe.Infrastructure.Extensions;
     using Xunit;
 
     public class AccountCreateOptionsTest : BaseStripeTest
     {
-        private readonly AccountService service;
-
-        public AccountCreateOptionsTest(MockHttpClientFixture mockHttpClientFixture)
-            : base(mockHttpClientFixture)
-        {
-            this.service = new AccountService();
-        }
-
         [Fact]
         public void Serialize()
         {
@@ -30,8 +19,9 @@ namespace StripeTests
                 },
             };
 
-            var url = this.service.ApplyAllParameters(options, string.Empty, false);
-            Assert.Equal("?individual[first_name]=first+name&individual[last_name]=last+name", url);
+            Assert.Equal(
+                "individual[first_name]=first+name&individual[last_name]=last+name",
+                options.ToQueryString());
         }
     }
 }
