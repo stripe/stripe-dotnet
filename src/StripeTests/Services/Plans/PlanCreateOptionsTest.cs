7bdbda78c4711ebef4c336b0875fe81be70a38ea
Olivier Bellone <ob@stripe.com>
2019-01-18 17:43:11 +0100

    Better serialization

--- src/StripeTests/Services/Plans/PlanCreateOptionsTest.cs
+++ src/StripeTests/Services/Plans/PlanCreateOptionsTest.cs
@@ -1,23 +1,12 @@
 namespace StripeTests
 {
-    using System;
     using System.Collections.Generic;
-    using System.Threading.Tasks;
-
     using Stripe;
-    using Stripe.Infrastructure;
+    using Stripe.Infrastructure.Extensions;
     using Xunit;
 
     public class PlanCreateOptionsTest : BaseStripeTest
     {
-        private readonly PlanService service;
-
-        public PlanCreateOptionsTest(MockHttpClientFixture mockHttpClientFixture)
-            : base(mockHttpClientFixture)
-        {
-            this.service = new PlanService();
-        }
-
         [Fact]
         public void SerializeTiersProperly()
         {
@@ -41,8 +30,10 @@ namespace StripeTests
                 },
             };
 
-            var url = this.service.ApplyAllParameters(options, string.Empty, false);
-            Assert.Equal("?tiers[0][unit_amount]=1000&tiers[0][up_to]=10&tiers[1][unit_amount]=2000&tiers[1][up_to]=inf", url);
+            Assert.Equal(
+                "tiers[0][unit_amount]=1000&tiers[0][up_to]=10&" +
+                "tiers[1][unit_amount]=2000&tiers[1][up_to]=inf",
+                options.ToQueryString());
         }
     }
 }
