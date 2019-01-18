d9bc29cac9172aa24aff31114fec5d915a06bb5f
Paul Asjes <paulasjes@stripe.com>
2019-01-17 16:58:21 -0800

    Added paid_out_of_band to invoices

--- src/Stripe.net/Services/Invoices/InvoicePayOptions.cs
+++ src/Stripe.net/Services/Invoices/InvoicePayOptions.cs
@@ -12,5 +12,12 @@ namespace Stripe
 
         [JsonProperty("source")]
         public string SourceId { get; set; }
+
+        /// <summary>
+        /// Boolean representing whether an invoice is paid outside of Stripe. This will result in
+        /// no charge being made.
+        /// </summary>
+        [JsonProperty("paid_out_of_band")]
+        public bool? PaidOutOfBand { get; set; }
     }
 }
