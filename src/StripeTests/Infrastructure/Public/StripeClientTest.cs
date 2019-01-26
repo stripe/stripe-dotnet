7d7981789dd056c7fd4b320c67c1618ea158a82b
Olivier Bellone <ob@stripe.com>
2019-01-26 22:26:59 +0100

    Revamp HTTP client requestor

---
+++ src/StripeTests/Infrastructure/Public/StripeClientTest.cs
@@ -0,0 +1,112 @@
+namespace StripeTests
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Stripe;
+    using Xunit;
+
+    public class StripeClientTest : BaseStripeTest
+    {
+        private readonly DummyHttpClient httpClient;
+        private readonly StripeClient stripeClient;
+        private readonly BaseOptions options;
+        private readonly RequestOptions requestOptions;
+
+        public StripeClientTest()
+        {
+            this.httpClient = new DummyHttpClient();
+            this.stripeClient = new StripeClient(this.httpClient);
+            this.options = new ChargeCreateOptions
+            {
+                Amount = 123,
+                Currency = "usd",
+                SourceId = "tok_visa",
+            };
+            this.requestOptions = new RequestOptions();
+        }
+
+        [Fact]
+        public async Task RequestAsync_OkResponse()
+        {
+            var response = new StripeResponse(HttpStatusCode.OK, null, "{\"id\": \"ch_123\"}");
+            this.httpClient.Response = response;
+
+            var charge = await this.stripeClient.RequestAsync<Charge>(
+                HttpMethod.Post,
+                "/v1/charges",
+                this.options,
+                this.requestOptions);
+
+            Assert.NotNull(charge);
+            Assert.Equal("ch_123", charge.Id);
+            Assert.Equal(response, charge.StripeResponse);
+        }
+
+        [Fact]
+        public async Task RequestAsync_ApiError()
+        {
+            var response = new StripeResponse(
+                HttpStatusCode.PaymentRequired,
+                null,
+                "{\"error\": {\"type\": \"card_error\"}}");
+            this.httpClient.Response = response;
+
+            var exception = await Assert.ThrowsAsync<StripeException>(async () =>
+                await this.stripeClient.RequestAsync<Charge>(
+                    HttpMethod.Post,
+                    "/v1/charges",
+                    this.options,
+                    this.requestOptions));
+
+            Assert.NotNull(exception);
+            Assert.Equal(HttpStatusCode.PaymentRequired, exception.HttpStatusCode);
+            Assert.Equal("card_error", exception.StripeError.ErrorType);
+            Assert.Equal(response, exception.StripeResponse);
+        }
+
+        [Fact]
+        public async Task RequestAsync_OAuthError()
+        {
+            var response = new StripeResponse(
+                HttpStatusCode.BadRequest,
+                null,
+                "{\"error\": \"invalid_request\"}");
+            this.httpClient.Response = response;
+
+            var exception = await Assert.ThrowsAsync<StripeException>(async () =>
+                await this.stripeClient.RequestAsync<OAuthToken>(
+                    HttpMethod.Post,
+                    "/oauth/token",
+                    this.options,
+                    this.requestOptions));
+
+            Assert.NotNull(exception);
+            Assert.Equal(HttpStatusCode.BadRequest, exception.HttpStatusCode);
+            Assert.Equal("invalid_request", exception.StripeError.Error);
+            Assert.Equal(response, exception.StripeResponse);
+        }
+
+        private class DummyHttpClient : IHttpClient
+        {
+            public DummyHttpClient()
+            {
+            }
+
+            public StripeResponse Response { get; set; }
+
+            public Task<StripeResponse> MakeRequestAsync(
+                StripeRequest request,
+                CancellationToken cancellationToken = default(CancellationToken))
+            {
+                if (this.Response == null)
+                {
+                    throw new StripeTestException("Response is null");
+                }
+
+                return Task.FromResult<StripeResponse>(this.Response);
+            }
+        }
+    }
+}
