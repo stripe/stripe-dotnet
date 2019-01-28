4c5af0efd752e2ad91470744bcb7634df94e1aeb
Olivier Bellone <ob@stripe.com>
2019-01-28 21:45:33 +0100

    Move base URLs out of resource services where possible

--- src/Stripe.net/Services/OAuth/OAuthTokenService.cs
+++ src/Stripe.net/Services/OAuth/OAuthTokenService.cs
@@ -19,24 +19,26 @@ namespace Stripe
 
         public override string BasePath => null;
 
+        public override string BaseUrl => StripeConfiguration.ConnectBase;
+
         public virtual OAuthToken Create(OAuthTokenCreateOptions options, RequestOptions requestOptions = null)
         {
-            return this.Request<OAuthToken>(HttpMethod.Post, $"{StripeConfiguration.ConnectBase}/oauth/token", options, requestOptions);
+            return this.Request(HttpMethod.Post, "/oauth/token", options, requestOptions);
         }
 
         public virtual Task<OAuthToken> CreateAsync(OAuthTokenCreateOptions options, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
         {
-            return this.RequestAsync<OAuthToken>(HttpMethod.Post, $"{StripeConfiguration.ConnectBase}/oauth/token", options, requestOptions, cancellationToken);
+            return this.RequestAsync(HttpMethod.Post, "/oauth/token", options, requestOptions, cancellationToken);
         }
 
         public virtual OAuthDeauthorize Deauthorize(OAuthTokenDeauthorizeOptions options, RequestOptions requestOptions = null)
         {
-            return this.Request<OAuthDeauthorize>(HttpMethod.Post, $"{StripeConfiguration.ConnectBase}/oauth/deauthorize", options, requestOptions);
+            return this.Request<OAuthDeauthorize>(HttpMethod.Post, "/oauth/deauthorize", options, requestOptions);
         }
 
         public virtual Task<OAuthDeauthorize> DeauthorizeAsync(OAuthTokenDeauthorizeOptions options, RequestOptions requestOptions = null, CancellationToken cancellationToken = default(CancellationToken))
         {
-            return this.RequestAsync<OAuthDeauthorize>(HttpMethod.Post, $"{StripeConfiguration.ConnectBase}/oauth/deauthorize", options, requestOptions, cancellationToken);
+            return this.RequestAsync<OAuthDeauthorize>(HttpMethod.Post, "/oauth/deauthorize", options, requestOptions, cancellationToken);
         }
     }
 }
