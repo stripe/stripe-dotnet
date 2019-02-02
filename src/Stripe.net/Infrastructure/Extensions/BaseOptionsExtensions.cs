ecda39e1c98391988c5a441e4ca2aec95750f7cf
Olivier Bellone <ob@stripe.com>
2019-02-02 23:36:19 +0100

    Simplify handling of Expand and ExtraParams

--- src/Stripe.net/Infrastructure/Extensions/BaseOptionsExtensions.cs
+++ src/Stripe.net/Infrastructure/Extensions/BaseOptionsExtensions.cs
@@ -7,15 +7,10 @@ namespace Stripe.Infrastructure.Extensions
     {
         /// <summary>Serializes the parameters with form encoding.</summary>
         /// <param name="options">The options class containing the parameters.</param>
-        /// <param name="includeExtraParams">Whether to include extra parameters in the query string or not.</param>
-        /// <param name="includeExpandParams">Whether to include expand parameters in the query string or not.</param>
         /// <returns>A form-encoded string represensation of the parameters.</returns>
-        public static string ToQueryString(this BaseOptions options, bool includeExtraParams = true, bool includeExpandParams = true)
+        public static string ToQueryString(this BaseOptions options)
         {
-            return FormEncoder.JoinQueries(
-                FormEncoder.EncodeOptions(options),
-                includeExtraParams ? FormEncoder.EncodeDictionary(options.ExtraParams) : null,
-                includeExpandParams ? FormEncoder.EncodeList(options.Expand, "expand") : null);
+            return FormEncoder.EncodeOptions(options);
         }
     }
 }
