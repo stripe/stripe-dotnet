c6de799d3deb44cec95dbbd63ee8a1daa5632786
Olivier Bellone <ob@stripe.com>
2019-02-03 19:55:37 +0100

    Minor improvements in EventUtility

--- src/StripeTests/Infrastructure/StringUtilsTest.cs
+++ src/StripeTests/Infrastructure/StringUtilsTest.cs
@@ -25,5 +25,28 @@ namespace StripeTests
                 Assert.Equal(testCase.want, StringUtils.ToSnakeCase(testCase.data));
             }
         }
+
+        [Fact]
+        public void SecureEquals()
+        {
+            var testCases = new[]
+            {
+                new { data = new { a = "Hello", b = "Hello" }, want = true },
+                new { data = new { a = "Hello", b = "hello" }, want = false },
+                new { data = new { a = "Hello", b = "Helloo" }, want = false },
+                new { data = new { a = "Hello", b = "Hell" }, want = false },
+                new { data = new { a = "Hello", b = string.Empty }, want = false },
+                new { data = new { a = string.Empty, b = "Hello" }, want = false },
+                new { data = new { a = string.Empty, b = string.Empty }, want = true },
+                new { data = new { a = "\0AAAAAAAAA", b = "\0BBBBBBBBBBBB" }, want = false },
+            };
+
+            foreach (var testCase in testCases)
+            {
+                Assert.Equal(
+                    testCase.want,
+                    StringUtils.SecureEquals(testCase.data.a, testCase.data.b));
+            }
+        }
     }
 }
