e8a09152c75ad217e7652d5934e579dcddfabb6a
Olivier Bellone <ob@stripe.com>
2019-02-12 01:00:26 +0100

    Remove BaseOptionsExtensions

--- src/StripeTests/Services/Coupons/CouponCreateOptionsTest.cs
+++ src/StripeTests/Services/Coupons/CouponCreateOptionsTest.cs
@@ -1,7 +1,7 @@
 namespace StripeTests
 {
     using Stripe;
-    using Stripe.Infrastructure.Extensions;
+    using Stripe.Infrastructure.FormEncoding;
     using Xunit;
 
     public class CouponCreateOptionsTest : BaseStripeTest
@@ -15,7 +15,7 @@ namespace StripeTests
                 Duration = "forever",
             };
 
-            Assert.Equal("percent_off=25&duration=forever", options.ToQueryString());
+            Assert.Equal("percent_off=25&duration=forever", FormEncoder.CreateQueryString(options));
         }
     }
 }
