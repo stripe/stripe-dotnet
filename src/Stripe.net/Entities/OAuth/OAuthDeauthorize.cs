4aea6876b0b4855dd3a71a35a7f5f30e0d69e9ea
Olivier Bellone <ob@stripe.com>
2019-03-28 08:41:17 -0700

    Improved OAuth support (#1542)

--- src/Stripe.net/Entities/OAuth/OAuthDeauthorize.cs
+++ src/Stripe.net/Entities/OAuth/OAuthDeauthorize.cs
@@ -4,13 +4,12 @@ namespace Stripe
 
     public class OAuthDeauthorize : StripeEntity<OAuthDeauthorize>
     {
+        /// <summary>
+        /// The unique id of the account you have revoked access to, as a string. This is the same
+        /// as the <see cref="OAuthDeauthorizeOptions.StripeUserId"/> you passed in. If this is
+        /// returned, the revocation was successful.
+        /// </summary>
         [JsonProperty("stripe_user_id")]
         public string StripeUserId { get; set; }
-
-        [JsonProperty("error")]
-        public string Error { get; set; }
-
-        [JsonProperty("error_description")]
-        public string ErrorDescription { get; set; }
     }
 }
