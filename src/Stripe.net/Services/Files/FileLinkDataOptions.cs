9c07dfdf6257a6800817f21030ff5f57759f7b92
Olivier Bellone <ob@stripe.com>
2019-04-30 20:54:30 -0700

    Add support for file_link_data (#1598)

---
+++ src/Stripe.net/Services/Files/FileLinkDataOptions.cs
@@ -0,0 +1,31 @@
+namespace Stripe
+{
+    using System;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    public class FileLinkDataOptions : BaseOptions
+    {
+        /// <summary>
+        /// Set this to <c>true</c> to create a file link for the newly created file. Creating a
+        /// link is only possible when the file’s purpose is one of the following:
+        /// <c>business_icon</c>, <c>business_logo</c>, <c>customer_signature</c>,
+        /// <c>dispute_evidence</c>, <c>pci_document</c>, or <c>tax_document_user_upload</c>.
+        /// </summary>
+        [JsonProperty("create")]
+        public bool? Create { get; set; }
+
+        /// <summary>
+        /// A future timestamp after which the link will no longer be usable.
+        /// </summary>
+        [JsonProperty("expires_at")]
+        public DateTime? ExpiresAt { get; set; }
+
+        /// <summary>
+        /// Set of key-value pairs that you can attach to an object. This can be useful for storing
+        /// additional information about the object in a structured format.
+        /// </summary>
+        [JsonProperty("metadata")]
+        public Dictionary<string, string> Metadata { get; set; }
+    }
+}
