046111a62a8e8589b1516329fa5c23776f36de93
Olivier Bellone <ob@stripe.com>
2019-05-24 09:53:03 -0700

    Add missing description parameter to PayoutCreateOptions

--- src/Stripe.net/Services/Payouts/PayoutListOptions.cs
+++ src/Stripe.net/Services/Payouts/PayoutListOptions.cs
@@ -5,15 +5,28 @@ namespace Stripe
 
     public class PayoutListOptions : ListOptionsWithCreated
     {
+        /// <summary>
+        /// A filter on the list based on the object <c>arrival_date</c> field.
+        /// </summary>
         [JsonProperty("arrival_date")]
         public DateTime? ArrivalDate { get; set; }
 
+        /// <summary>
+        /// A filter on the list based on the object <c>arrival_date</c> field.
+        /// </summary>
         [JsonProperty("arrival_date")]
         public DateRangeOptions ArrivalDateRange { get; set; }
 
+        /// <summary>
+        /// The ID of an external account - only return payouts sent to this external account.
+        /// </summary>
         [JsonProperty("destination")]
         public string Destination { get; set; }
 
+        /// <summary>
+        /// Only return payouts that have the given status: <c>pending</c>, <c>paid</c>,
+        /// <c>failed</c>, or <c>canceled</c>.
+        /// </summary>
         [JsonProperty("status")]
         public string Status { get; set; }
     }
--- src/Stripe.net/Services/Payouts/PayoutUpdateOptions.cs
+++ src/Stripe.net/Services/Payouts/PayoutUpdateOptions.cs
@@ -5,6 +5,10 @@ namespace Stripe
 
     public class PayoutUpdateOptions : BaseOptions
     {
+        /// <summary>
+        /// A set of key-value pairs that you can attach to a payout object. It can be useful for
+        /// storing additional information about the payout in a structured format.
+        /// </summary>
         [JsonProperty("metadata")]
         public Dictionary<string, string> Metadata { get; set; }
     }
