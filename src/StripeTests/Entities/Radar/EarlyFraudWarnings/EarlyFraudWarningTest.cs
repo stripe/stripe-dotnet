703be2b86283a38efcd89fd96fc3d0bb6331085f
Olivier Bellone <ob@stripe.com>
2019-08-01 10:45:18 +0800

    Add missing expansion test for EarlyFraudWarning (#1728)

--- src/StripeTests/Entities/Radar/EarlyFraudWarnings/EarlyFraudWarningTest.cs
+++ src/StripeTests/Entities/Radar/EarlyFraudWarnings/EarlyFraudWarningTest.cs
@@ -22,5 +22,24 @@ namespace StripeTests.Radar
             Assert.NotNull(warning.Id);
             Assert.Equal("radar.early_fraud_warning", warning.Object);
         }
+
+        [Fact]
+        public void DeserializeWithExpansions()
+        {
+            string[] expansions =
+            {
+              "charge",
+            };
+
+            string json = this.GetFixture("/v1/radar/early_fraud_warnings/issfr_123", expansions);
+            var warning = JsonConvert.DeserializeObject<EarlyFraudWarning>(json);
+            Assert.NotNull(warning);
+            Assert.IsType<EarlyFraudWarning>(warning);
+            Assert.NotNull(warning.Id);
+            Assert.Equal("radar.early_fraud_warning", warning.Object);
+
+            Assert.NotNull(warning.Charge);
+            Assert.Equal("charge", warning.Charge.Object);
+        }
     }
 }
