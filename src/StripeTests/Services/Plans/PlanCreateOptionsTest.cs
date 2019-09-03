da181e9547bf342f54bca71a22675050e7acf2d3
Unknown <simongparamore@gmail.com>
2019-09-03 14:14:53 +0100

    Add decimal properties to PlanTierOptions for plan create

--- src/StripeTests/Services/Plans/PlanCreateOptionsTest.cs
+++ src/StripeTests/Services/Plans/PlanCreateOptionsTest.cs
@@ -32,5 +32,33 @@ namespace StripeTests
                 "tiers[1][unit_amount]=2000&tiers[1][up_to]=inf",
                 FormEncoder.CreateQueryString(options));
         }
+
+        [Fact]
+        public void SerializeDecimalTiersProperly()
+        {
+            var options = new PlanCreateOptions
+            {
+                Tiers = new List<PlanTierOptions>
+                {
+                    new PlanTierOptions
+                    {
+                        UnitAmountDecimal = 0.003m,
+                        FlatAmountDecimal = 0.12m,
+                        UpTo = 10,
+                    },
+                    new PlanTierOptions
+                    {
+                        UnitAmountDecimal = 0.004m,
+                        FlatAmountDecimal = 0.24m,
+                        UpTo = PlanTierUpTo.Inf,
+                    }
+                },
+            };
+
+            Assert.Equal(
+                "tiers[0][flat_amount_decimal]=0.12&tiers[0][unit_amount_decimal]=0.003&tiers[0][up_to]=10&" +
+                "tiers[1][flat_amount_decimal]=0.24&tiers[1][unit_amount_decimal]=0.004&tiers[1][up_to]=inf",
+                FormEncoder.CreateQueryString(options));
+        }
     }
 }
