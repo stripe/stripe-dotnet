85d835b92758e85f829c31dde1ea5d3b48244ef5
Remi Jannel <remi@stripe.com>
2019-09-03 17:12:13 -0700

    Add support for `Authentication` and `Url` on Issuing `Authorization`

--- src/Stripe.net/Entities/Issuing/Authorizations/VerificationData.cs
+++ src/Stripe.net/Entities/Issuing/Authorizations/VerificationData.cs
@@ -4,12 +4,27 @@ namespace Stripe.Issuing
 
     public class VerificationData : StripeEntity<VerificationData>
     {
+        /// <summary>
+        /// One of <c>match</c>, <c>mismatch</c>, or <c>not_provided</c>.
+        /// </summary>
         [JsonProperty("address_line1_check")]
         public string AddressLine1Check { get; set; }
 
+        /// <summary>
+        /// One of <c>match</c>, <c>mismatch</c>, or <c>not_provided</c>.
+        /// </summary>
         [JsonProperty("address_zip_check")]
         public string AddressZipCheck { get; set; }
 
+        /// <summary>
+        /// One of <c>success</c>, <c>failure</c>, <c>exempt</c>, or <c>none</c>.
+        /// </summary>
+        [JsonProperty("authentication")]
+        public string Authentication { get; set; }
+
+        /// <summary>
+        /// One of <c>match</c>, <c>mismatch</c>, or <c>not_provided</c>.
+        /// </summary>
         [JsonProperty("cvc_check")]
         public string CvcCheck { get; set; }
     }
