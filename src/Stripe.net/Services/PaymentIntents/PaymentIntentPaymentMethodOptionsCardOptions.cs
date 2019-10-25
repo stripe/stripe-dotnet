6d9ee1b4931db77219168b20d1b6f502c5fecbb9
Remi Jannel <remi@stripe.com>
2019-10-24 17:21:28 -0700

    Add missing `Installments` option in `PaymentIntentPaymentMethodOptionsCardOptions`

--- src/Stripe.net/Services/PaymentIntents/PaymentIntentPaymentMethodOptionsCardOptions.cs
+++ src/Stripe.net/Services/PaymentIntents/PaymentIntentPaymentMethodOptionsCardOptions.cs
@@ -6,6 +6,12 @@ namespace Stripe
     public class PaymentIntentPaymentMethodOptionsCardOptions : INestedOptions
     {
         /// <summary>
+        /// Installment configuration for payments attempted on this PaymentIntent (Mexico Only).
+        /// </summary>
+        [JsonProperty("installments")]
+        public PaymentIntentPaymentMethodOptionsCardInstallmentsOptions Installments { get; set; }
+
+        /// <summary>
         /// When specified, this parameter indicates that a transaction will be marked as MOTO
         /// (Mail Order Telephone Order) and thus out of scope for SCA.
         /// </summary>
