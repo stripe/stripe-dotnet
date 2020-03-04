2cb53fdeeeebda4068c1fa31250ce61990df83fb
Remi Jannel <remi@stripe.com>
2020-03-03 18:14:25 -0800

    Add support for `Errors` in `Requirements` on `Account`, `Capability` and `Person`

--- src/Stripe.net/Entities/Capabilities/CapabilityRequirements.cs
+++ src/Stripe.net/Entities/Capabilities/CapabilityRequirements.cs
@@ -32,6 +32,13 @@ namespace Stripe
         public string DisabledReason { get; set; }
 
         /// <summary>
+        /// The fields that need to be collected again because validation or verification failed
+        /// for some reason.
+        /// </summary>
+        [JsonProperty("errors")]
+        public List<AccountRequirementsError> Errors { get; set; }
+
+        /// <summary>
         /// The fields that need to be collected assuming all volume thresholds are reached. As
         /// they become required, these fields appear in <c>currently_due</c> as well, and the
         /// <c>current_deadline</c> is set.
