e325f729d84aea8d48a9b9e02651775a2b0fe7fc
Remi Jannel <remi@stripe.com>
2020-03-23 10:53:25 -0700

    Add new capabilities

--- src/Stripe.net/Entities/Accounts/AccountCapabilities.cs
+++ src/Stripe.net/Entities/Accounts/AccountCapabilities.cs
@@ -6,6 +6,13 @@ namespace Stripe
     public class AccountCapabilities : StripeEntity<AccountCapabilities>
     {
         /// <summary>
+        /// The status of the BECS Direct Debit (AU) payments capability of the account, or whether
+        /// the account can directly process BECS Direct Debit (AU) charges.
+        /// </summary>
+        [JsonProperty("au_becs_debit_payments")]
+        public string AuBecsDebitPayments { get; set; }
+
+        /// <summary>
         /// The status of the card payments capability of the account, or whether the account can
         /// directly process credit and debit card charges.
         /// </summary>
@@ -17,6 +24,18 @@ namespace Stripe
         /// </summary>
         [JsonProperty("legacy_payments")]
         public string LegacyPayments { get; set; }
+
+        /// <summary>
+        /// The status of the tax reporting 1099-K (US) capability of the account.
+        /// </summary>
+        [JsonProperty("tax_reporting_us_1099_k")]
+        public string TaxReportingUs1099K { get; set; }
+
+        /// <summary>
+        /// The status of the tax reporting 1099-MISC (US) capability of the account.
+        /// </summary>
+        [JsonProperty("tax_reporting_us_1099_misc")]
+        public string TaxReportingUs1099Misc { get; set; }
 
         /// <summary>
         /// The status of the transfers capability of the account, or whether your platform can
