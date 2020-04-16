0eedcbba686f9ccc89eade7acef07a74662d2e49
Remi Jannel <remi@stripe.com>
2020-04-15 18:45:34 -0700

    Remove deprecated features
    Remove deprecated API features
    * Remove `Percentage` filter when listing `TaxRate` as this does not work
    * Remove `Country` and `RoutingNumber` from `ChargePaymentMethodDetailsAcssDebit`
    * Remove `RenewalInterval` on `SubscriptionSchedule`
    * Rename `Parameters` to `ReportRunParameters` on `ReportRun`

--- src/Stripe.net/Entities/Charges/ChargePaymentMethodDetails/ChargePaymentMethodDetailsAcssDebit.cs
+++ src/Stripe.net/Entities/Charges/ChargePaymentMethodDetails/ChargePaymentMethodDetailsAcssDebit.cs
@@ -5,16 +5,10 @@ namespace Stripe
 
     public class ChargePaymentMethodDetailsAcssDebit : StripeEntity<ChargePaymentMethodDetailsAcssDebit>
     {
-        [JsonProperty("country")]
-        public string Country { get; set; }
-
         [JsonProperty("fingerprint")]
         public string Fingerprint { get; set; }
 
         [JsonProperty("last4")]
         public string Last4 { get; set; }
-
-        [JsonProperty("routing_number")]
-        public string RoutingNumber { get; set; }
     }
 }
