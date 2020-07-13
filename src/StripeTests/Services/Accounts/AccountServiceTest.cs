975f118f68bb2a5cf43eb8606efe5b06b782cbc8
CJ Avilla <cjavilla@stripe.com>
2020-07-13 15:56:07 -0700

    Several API changes

--- src/StripeTests/Services/Accounts/AccountServiceTest.cs
+++ src/StripeTests/Services/Accounts/AccountServiceTest.cs
@@ -82,10 +82,6 @@ namespace StripeTests
                         },
                         StatementDescriptorPrefix = "STR",
                     },
-                    Dashboard = new AccountSettingsDashboardOptions
-                    {
-                        DisplayName = "dashboard_name",
-                    },
                     Payments = new AccountSettingsPaymentsOptions
                     {
                         StatementDescriptor = "STRIPE 123",
