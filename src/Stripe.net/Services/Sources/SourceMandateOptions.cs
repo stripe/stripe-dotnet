21cb7f47966ef234e8f7bdca90a164d65bdc115d
CJ Avilla <cjavilla@stripe.com>
2020-07-13 13:15:25 -0700

    Adds several breaking changes to bring inline with the spec

--- src/Stripe.net/Services/Sources/SourceMandateOptions.cs
+++ src/Stripe.net/Services/Sources/SourceMandateOptions.cs
@@ -6,5 +6,17 @@ namespace Stripe
     {
         [JsonProperty("acceptance")]
         public SourceMandateAcceptanceOptions Acceptance { get; set; }
+
+        [JsonProperty("amount")]
+        public long? Amount { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("interval")]
+        public string Interval { get; set; }
+
+        [JsonProperty("notification_method")]
+        public string NotificationMethod { get; set; }
     }
 }
