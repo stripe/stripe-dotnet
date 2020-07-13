c9bf9f3e6c111e7194eb2d61bbb07c3d779e1c2b
CJ Avilla <cjavilla@stripe.com>
2020-07-13 13:15:25 -0700

    Adds several breaking changes to bring inline with the spec

--- src/Stripe.net/Services/Invoices/InvoiceListOptions.cs
+++ src/Stripe.net/Services/Invoices/InvoiceListOptions.cs
@@ -4,21 +4,13 @@ namespace Stripe
     using Newtonsoft.Json;
     using Stripe.Infrastructure;
 
-    public class InvoiceListOptions : ListOptions
+    public class InvoiceListOptions : ListOptionsWithCreated
     {
         /// <summary>
         /// Either <c>charge_automatically</c>, or <c>send_invoice</c>.
         /// </summary>
         [JsonProperty("collection_method")]
         public string CollectionMethod { get; set; }
-
-        /// <summary>
-        /// A filter on the list based on the object <c>created</c> field. The value can be a
-        /// <see cref="DateTime"/> or a <see cref="DateRangeOptions"/>.
-        /// </summary>
-        [JsonProperty("created")]
-        [JsonConverter(typeof(AnyOfConverter))]
-        public AnyOf<DateTime?, DateRangeOptions> Created { get; set; }
 
         /// <summary>
         /// Only return invoices for the customer specified by this customer ID.
@@ -33,12 +25,6 @@ namespace Stripe
         [JsonProperty("due_date")]
         [JsonConverter(typeof(AnyOfConverter))]
         public AnyOf<DateTime?, DateRangeOptions> DueDate { get; set; }
-
-        /// <summary>
-        /// A filter on the list based on the object paid field.
-        /// </summary>
-        [JsonProperty("paid")]
-        public bool? Paid { get; set; }
 
         /// <summary>
         /// Only return invoices with a given status. Must be one of
