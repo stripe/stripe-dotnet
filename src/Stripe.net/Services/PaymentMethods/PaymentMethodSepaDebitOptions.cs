9f11accc3394e0ab55162a13c54784a2cc2c978b
CJ Avilla <cjavilla@stripe.com>
2020-07-24 17:40:12 -0700

    Stop sharing from PaymentMethod

--- src/Stripe.net/Services/PaymentMethods/PaymentMethodSepaDebitOptions.cs
+++ src/Stripe.net/Services/PaymentMethods/PaymentMethodSepaDebitOptions.cs
@@ -2,7 +2,7 @@ namespace Stripe
 {
     using Newtonsoft.Json;
 
-    public class PaymentMethodSepaDebitCreateOptions : INestedOptions
+    public class PaymentMethodSepaDebitOptions : INestedOptions
     {
         /// <summary>
         /// IBAN associated with this payment method. This only works on creation.
