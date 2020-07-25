4027b4bac99b051c066ee596826fe6b5bbaf4f9a
CJ Avilla <cjavilla@stripe.com>
2020-07-24 17:15:18 -0700

    Stop sharing from PaymentIntentPaymentMethodDataOptions

--- src/Stripe.net/Services/PaymentIntents/PaymentIntentPaymentMethodDataOptions.cs
+++ src/Stripe.net/Services/PaymentIntents/PaymentIntentPaymentMethodDataOptions.cs
@@ -9,7 +9,21 @@ namespace Stripe
         /// The details associated with the AU BECS Debit payment method for this payment intent.
         /// </summary>
         [JsonProperty("au_becs_debit")]
-        public PaymentMethodAuBecsDebitCreateOptions AuBecsDebit { get; set; }
+        public PaymentIntentPaymentMethodDataAuBecsDebitOptions AuBecsDebit { get; set; }
+
+        /// <summary>
+        /// If this is a Bacs Debig PaymentMethod, this hash contains details about the Bacs Direct
+        /// Debit bank account.
+        /// </summary>
+        [JsonProperty("bacs_debit")]
+        public PaymentIntentPaymentMethodDataBacsDebitOptions BacsDebit { get; set; }
+
+        /// <summary>
+        /// If this is a Bancontact PaymentMethod, this hash contains details about the Bancontact
+        /// payment method.
+        /// </summary>
+        [JsonProperty("bancontact")]
+        public PaymentIntentPaymentMethodDataBancontactOptions Bancontact { get; set; }
 
         /// <summary>
         /// Billing information associated with the payment method for this payment intent.
@@ -21,19 +35,40 @@ namespace Stripe
         /// The details associated with the card payment method for this payment intent.
         /// </summary>
         [JsonProperty("card")]
-        public PaymentMethodCardCreateOptions Card { get; set; }
+        public PaymentIntentPaymentMethodDataCardOptions Card { get; set; }
+
+        /// <summary>
+        /// If this is an EPS PaymentMethod, this hash contains details about the EPS payment
+        /// method.
+        /// </summary>
+        [JsonProperty("eps")]
+        public PaymentIntentPaymentMethodDataEpsOptions Eps { get; set; }
 
         /// <summary>
         /// The details associated with the FPX payment method for this payment intent.
         /// </summary>
         [JsonProperty("fpx")]
-        public PaymentMethodFpxCreateOptions Fpx { get; set; }
+        public PaymentIntentPaymentMethodDataFpxOptions Fpx { get; set; }
+
+        /// <summary>
+        /// If this is a Giropay PaymentMethod, this hash contains details about the Giropay payment
+        /// method.
+        /// </summary>
+        [JsonProperty("giropay")]
+        public PaymentIntentPaymentMethodDataGiropayOptions Giropay { get; set; }
 
         /// <summary>
         /// The details associated with the iDEAL payment method for this payment intent.
         /// </summary>
         [JsonProperty("ideal")]
-        public PaymentMethodIdealCreateOptions Ideal { get; set; }
+        public PaymentIntentPaymentMethodDataIdealOptions Ideal { get; set; }
+
+        /// <summary>
+        /// If this is an Interac Present PaymentMethod, this hash contains details about the
+        /// Interac Present payment method.
+        /// </summary>
+        [JsonProperty("interac_present")]
+        public PaymentIntentPaymentMethodDataInteracPresentOptions InteracPresent { get; set; }
 
         /// <summary>
         /// Set of key-value pairs that you can attach to an object. This can be useful
@@ -44,10 +79,16 @@ namespace Stripe
         public Dictionary<string, string> Metadata { get; set; }
 
         /// <summary>
+        /// If this is a P24 PaymentMethod, this hash contains details about the P24 payment method.
+        /// </summary>
+        [JsonProperty("p24")]
+        public PaymentIntentPaymentMethodDataP24Options P24 { get; set; }
+
+        /// <summary>
         /// The details associated with the SEPA Debit payment method for this payment intent.
         /// </summary>
         [JsonProperty("sepa_debit")]
-        public PaymentMethodSepaDebitCreateOptions SepaDebit { get; set; }
+        public PaymentIntentPaymentMethodDataSepaDebitOptions SepaDebit { get; set; }
 
         /// <summary>
         /// The type of payment method for this payment intent.
