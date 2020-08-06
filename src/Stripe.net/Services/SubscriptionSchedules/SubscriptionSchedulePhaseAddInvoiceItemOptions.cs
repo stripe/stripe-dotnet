971f7247c1450a268e46ff539b9d72f2424a8de5
CJ Avilla <cjavilla@stripe.com>
2020-08-05 19:43:58 -0700

    Unshare SubscriptionSchedule objecst

--- src/Stripe.net/Services/SubscriptionSchedules/SubscriptionSchedulePhaseAddInvoiceItemOptions.cs
+++ src/Stripe.net/Services/SubscriptionSchedules/SubscriptionSchedulePhaseAddInvoiceItemOptions.cs
@@ -2,13 +2,13 @@ namespace Stripe
 {
     using Newtonsoft.Json;
 
-    public class SubscriptionAddInvoiceItemOptions : INestedOptions
+    public class SubscriptionSchedulePhaseAddInvoiceItemOptions : INestedOptions
     {
         [JsonProperty("price")]
         public string Price { get; set; }
 
         [JsonProperty("price_data")]
-        public SubscriptionAddInvoiceItemPriceDataOptions PriceData { get; set; }
+        public SubscriptionSchedulePhaseAddInvoiceItemPriceDataOptions PriceData { get; set; }
 
         [JsonProperty("quantity")]
         public long? Quantity { get; set; }
