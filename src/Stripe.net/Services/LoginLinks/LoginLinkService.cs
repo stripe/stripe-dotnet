49bcb16d7da3bf99337e3931910eed1d5f3cc874
CJ Avilla <cjavilla@stripe.com>
2020-08-07 16:24:32 -0700

    Update LoginLinkService to use new parentId pattern

--- src/Stripe.net/Services/LoginLinks/LoginLinkService.cs
+++ src/Stripe.net/Services/LoginLinks/LoginLinkService.cs
@@ -1,9 +1,7 @@
 namespace Stripe
 {
-    using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
-    using Stripe.Infrastructure;
 
     public class LoginLinkService : ServiceNested<LoginLink>,
         INestedCreatable<LoginLink, LoginLinkCreateOptions>
@@ -20,14 +18,14 @@ namespace Stripe
 
         public override string BasePath => "/v1/accounts/{PARENT_ID}/login_links";
 
-        public virtual LoginLink Create(string id, LoginLinkCreateOptions options = null, RequestOptions requestOptions = null)
+        public virtual LoginLink Create(string parentId, LoginLinkCreateOptions options = null, RequestOptions requestOptions = null)
         {
-            return this.CreateNestedEntity(id, options, requestOptions);
+            return this.CreateNestedEntity(parentId, options, requestOptions);
         }
 
-        public virtual Task<LoginLink> CreateAsync(string id, LoginLinkCreateOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
+        public virtual Task<LoginLink> CreateAsync(string parentId, LoginLinkCreateOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
         {
-            return this.CreateNestedEntityAsync(id, options, requestOptions, cancellationToken);
+            return this.CreateNestedEntityAsync(parentId, options, requestOptions, cancellationToken);
         }
     }
 }
