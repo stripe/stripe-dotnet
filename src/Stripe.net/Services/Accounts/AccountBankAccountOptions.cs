551ee39dbd345ac6756071e6b66581b8dba9f6bf
CJ Avilla <cjavilla@stripe.com>
2020-08-10 10:41:48 -0700

    No-op clean up pre-codegen

--- src/Stripe.net/Services/Accounts/AccountBankAccountOptions.cs
+++ src/Stripe.net/Services/Accounts/AccountBankAccountOptions.cs
@@ -1,12 +1,17 @@
 namespace Stripe
 {
-    using System;
     using Newtonsoft.Json;
 
     public class AccountBankAccountOptions : INestedOptions
     {
         [JsonProperty("object")]
         internal string Object => "bank_account";
+
+        [JsonProperty("account_holder_name")]
+        public string AccountHolderName { get; set; }
+
+        [JsonProperty("account_holder_type")]
+        public string AccountHolderType { get; set; }
 
         [JsonProperty("account_number")]
         public string AccountNumber { get; set; }
@@ -16,12 +21,6 @@ namespace Stripe
 
         [JsonProperty("currency")]
         public string Currency { get; set; }
-
-        [JsonProperty("account_holder_type")]
-        public string AccountHolderType { get; set; }
-
-        [JsonProperty("account_holder_name")]
-        public string AccountHolderName { get; set; }
 
         [JsonProperty("routing_number")]
         public string RoutingNumber { get; set; }
