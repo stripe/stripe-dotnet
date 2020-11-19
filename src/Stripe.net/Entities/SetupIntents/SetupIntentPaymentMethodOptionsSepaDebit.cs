04cfc1a6124dc96a4ba4f9a11ce541766cdf2522
Richard Marmorstein <richardm@stripe.com>
2020-11-19 11:48:34 -0500

    Codegen for openapi 3723465

--- src/Stripe.net/Entities/SetupIntents/SetupIntentPaymentMethodOptionsSepaDebit.cs
+++ src/Stripe.net/Entities/SetupIntents/SetupIntentPaymentMethodOptionsSepaDebit.cs
@@ -1,7 +1,11 @@
 // File generated from our OpenAPI spec
 namespace Stripe
 {
+    using Newtonsoft.Json;
+
     public class SetupIntentPaymentMethodOptionsSepaDebit : StripeEntity<SetupIntentPaymentMethodOptionsSepaDebit>
     {
+        [JsonProperty("mandate_options")]
+        public SetupIntentPaymentMethodOptionsSepaDebitMandateOptions MandateOptions { get; set; }
     }
 }
