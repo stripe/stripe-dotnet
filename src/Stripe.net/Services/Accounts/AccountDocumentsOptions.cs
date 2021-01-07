5c09fe3f5672fab0ae5a34b16acba360d12067a4
Christopher Trudeau <ctrudeau@stripe.com>
2021-01-07 10:58:10 -0800

    Codegen for openapi 383c876

--- src/Stripe.net/Services/Accounts/AccountDocumentsOptions.cs
+++ src/Stripe.net/Services/Accounts/AccountDocumentsOptions.cs
@@ -14,5 +14,37 @@ namespace Stripe
         /// </summary>
         [JsonProperty("bank_account_ownership_verification")]
         public AccountDocumentsBankAccountOwnershipVerificationOptions BankAccountOwnershipVerification { get; set; }
+
+        /// <summary>
+        /// One or more documents that demonstrate proof of a company's license to operate.
+        /// </summary>
+        [JsonProperty("company_license")]
+        public AccountDocumentsCompanyLicenseOptions CompanyLicense { get; set; }
+
+        /// <summary>
+        /// One or more documents showing the company's Memorandum of Association.
+        /// </summary>
+        [JsonProperty("company_memorandum_of_association")]
+        public AccountDocumentsCompanyMemorandumOfAssociationOptions CompanyMemorandumOfAssociation { get; set; }
+
+        /// <summary>
+        /// (Certain countries only) One or more documents showing the ministerial decree legalizing
+        /// the company's establishment.
+        /// </summary>
+        [JsonProperty("company_ministerial_decree")]
+        public AccountDocumentsCompanyMinisterialDecreeOptions CompanyMinisterialDecree { get; set; }
+
+        /// <summary>
+        /// One or more documents that demonstrate proof of a company's registration with the
+        /// appropriate local authorities.
+        /// </summary>
+        [JsonProperty("company_registration_verification")]
+        public AccountDocumentsCompanyRegistrationVerificationOptions CompanyRegistrationVerification { get; set; }
+
+        /// <summary>
+        /// One or more documents that demonstrate proof of a company's tax ID.
+        /// </summary>
+        [JsonProperty("company_tax_id_verification")]
+        public AccountDocumentsCompanyTaxIdVerificationOptions CompanyTaxIdVerification { get; set; }
     }
 }
