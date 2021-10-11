2d8ae170eeeb3d4ef94a144d9804450ddf08d8c4
Richard Marmorstein <richardm@stripe.com>
2021-10-11 10:57:12 -0400

    bump stripe-mock-version

--- src/StripeTests/StripeMockFixture.cs
+++ src/StripeTests/StripeMockFixture.cs
@@ -13,7 +13,7 @@ namespace StripeTests
         /// If you bump this, don't forget to bump <c>STRIPE_MOCK_VERSION</c> in <c>appveyor.yml</c>
         /// as well.
         /// </remarks>
-        private const string MockMinimumVersion = "0.109.0";
+        private const string MockMinimumVersion = "0.112.0";
 
         private readonly string port;
 
