d3d960c054840a4fe7bfa9e3910bf51aea632a82
Dominic Charley-Roy <78050200+dcr-stripe@users.noreply.github.com>
2021-11-04 09:06:42 -0400

    API Updates (#2391)

--- src/Stripe.net/Services/Tokens/TokenAccountCompanyOptions.cs
+++ src/Stripe.net/Services/Tokens/TokenAccountCompanyOptions.cs
@@ -77,6 +77,13 @@ namespace Stripe
         public TokenAccountCompanyOwnershipDeclarationOptions OwnershipDeclaration { get; set; }
 
         /// <summary>
+        /// Whether the user described by the data in the token has been shown the Ownership
+        /// Declaration and indicated that it is correct.
+        /// </summary>
+        [JsonProperty("ownership_declaration_shown_and_signed")]
+        public bool? OwnershipDeclarationShownAndSigned { get; set; }
+
+        /// <summary>
         /// The company's phone number (used for verification).
         /// </summary>
         [JsonProperty("phone")]
