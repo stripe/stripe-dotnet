061ae4ab7067449b5e06e84482ae1c87f5bc3d58
Yejia Chen <yejia@stripe.com>
2021-12-09 15:18:25 -0500

    Codegen for openapi 2abe516

--- src/Stripe.net/Entities/BillingPortal/Configurations/Configuration.cs
+++ src/Stripe.net/Entities/BillingPortal/Configurations/Configuration.cs
@@ -2,13 +2,14 @@
 namespace Stripe.BillingPortal
 {
     using System;
+    using System.Collections.Generic;
     using Newtonsoft.Json;
     using Stripe.Infrastructure;
 
     /// <summary>
     /// A portal configuration describes the functionality and behavior of a portal session.
     /// </summary>
-    public class Configuration : StripeEntity<Configuration>, IHasId, IHasObject
+    public class Configuration : StripeEntity<Configuration>, IHasId, IHasMetadata, IHasObject
     {
         /// <summary>
         /// Unique identifier for the object.
@@ -70,6 +71,14 @@ namespace Stripe.BillingPortal
         /// </summary>
         [JsonProperty("livemode")]
         public bool Livemode { get; set; }
+
+        /// <summary>
+        /// Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can
+        /// attach to an object. This can be useful for storing additional information about the
+        /// object in a structured format.
+        /// </summary>
+        [JsonProperty("metadata")]
+        public Dictionary<string, string> Metadata { get; set; }
 
         /// <summary>
         /// Time at which the object was last updated. Measured in seconds since the Unix epoch.
--- src/Stripe.net/Services/BillingPortal/Configurations/ConfigurationUpdateOptions.cs
+++ src/Stripe.net/Services/BillingPortal/Configurations/ConfigurationUpdateOptions.cs
@@ -1,9 +1,10 @@
 // File generated from our OpenAPI spec
 namespace Stripe.BillingPortal
 {
+    using System.Collections.Generic;
     using Newtonsoft.Json;
 
-    public class ConfigurationUpdateOptions : BaseOptions
+    public class ConfigurationUpdateOptions : BaseOptions, IHasMetadata
     {
         /// <summary>
         /// Whether the configuration is active and can be used to create portal sessions.
@@ -31,5 +32,14 @@ namespace Stripe.BillingPortal
         /// </summary>
         [JsonProperty("features")]
         public ConfigurationFeaturesOptions Features { get; set; }
+
+        /// <summary>
+        /// Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can
+        /// attach to an object. This can be useful for storing additional information about the
+        /// object in a structured format. Individual keys can be unset by posting an empty value to
+        /// them. All keys can be unset by posting an empty value to <c>metadata</c>.
+        /// </summary>
+        [JsonProperty("metadata")]
+        public Dictionary<string, string> Metadata { get; set; }
     }
 }
