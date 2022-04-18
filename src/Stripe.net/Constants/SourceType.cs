d6ba69ed6e1b050b161519630dc3b1d08b5f39f6
Kamil Pajdzik <kamil@stripe.com>
2022-04-18 14:04:07 -0700

    Update SourceType constants

--- src/Stripe.net/Constants/SourceType.cs
+++ src/Stripe.net/Constants/SourceType.cs
@@ -2,6 +2,8 @@ namespace Stripe
 {
     public static class SourceType
     {
+        public const string Account = "account";
+
         public const string AchCreditTransfer = "ach_credit_transfer";
 
         public const string AchDebit = "ach_debit";
@@ -10,7 +12,13 @@ namespace Stripe
 
         public const string Bancontact = "bancontact";
 
+        public const string Bitcoin = "bitcoin";
+
+        public const string BitcoinSource = "bitcoin_source";
+
         public const string Card = "card";
+
+        public const string CardPresent = "card_present";
 
         public const string Eps = "eps";
 
@@ -20,7 +28,13 @@ namespace Stripe
 
         public const string Klarna = "klarna";
 
+        public const string Multibanco = "multibanco";
+
         public const string P24 = "p24";
+
+        public const string PaperCheck = "paper_check";
+
+        public const string PayPal = "paypal";
 
         public const string SepaCreditTransfer = "sepa_credit_transfer";
 
@@ -29,5 +43,7 @@ namespace Stripe
         public const string Sofort = "sofort";
 
         public const string ThreeDSecure = "three_d_secure";
+
+        public const string WeChat = "wechat";
     }
 }
