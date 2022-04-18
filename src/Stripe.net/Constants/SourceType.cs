8ae34dba3a42c937f6fef16d8b0051bc9b6a3d75
Kamil Pajdzik <kamil@stripe.com>
2022-04-18 14:04:07 -0700

    Update SourceType constants

--- src/Stripe.net/Constants/SourceType.cs
+++ src/Stripe.net/Constants/SourceType.cs
@@ -6,21 +6,41 @@ namespace Stripe
 
         public const string AchDebit = "ach_debit";
 
+        public const string AcssDebit = "acss_debit";
+
+        public const string AfterpayClearpay = "afterpay_clearpay";
+
         public const string Alipay = "alipay";
 
+        public const string AuBecsDebit = "au_becs_debit";
+
+        public const string BacsDebit = "bacs_debit";
+
         public const string Bancontact = "bancontact";
+
+        public const string Boleto = "boleto";
 
         public const string Card = "card";
 
         public const string Eps = "eps";
 
+        public const string Fpx = "fpx";
+
         public const string Giropay = "giropay";
+
+        public const string Grabpay = "grabpay";
 
         public const string Ideal = "ideal";
 
         public const string Klarna = "klarna";
 
+        public const string Konbini = "konbini";
+
+        public const string Oxxo = "oxxo";
+
         public const string P24 = "p24";
+
+        public const string Paynow = "paynow";
 
         public const string SepaCreditTransfer = "sepa_credit_transfer";
 
@@ -29,5 +49,9 @@ namespace Stripe
         public const string Sofort = "sofort";
 
         public const string ThreeDSecure = "three_d_secure";
+
+        public const string UsBankAccount = "us_bank_account";
+
+        public const string WechatPay = "wechat_pay";
     }
 }
