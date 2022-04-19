644afe429ec1a46f222a183dd342bb347ab2b9d4
Dominic Charley-Roy <dcr@stripe.com>
2022-04-19 10:19:12 -0400

    Auto-generate constant files for resource properties.

--- src/Stripe.net/Constants/SubscriptionStatuses.cs
+++ src/Stripe.net/Constants/SubscriptionStatuses.cs
@@ -1,19 +1,20 @@
+// File generated from our OpenAPI spec
 namespace Stripe
 {
     public static class SubscriptionStatuses
     {
-        public const string Trialing = "trialing";
-
         public const string Active = "active";
 
-        public const string PastDue = "past_due";
-
         public const string Canceled = "canceled";
-
-        public const string Unpaid = "unpaid";
 
         public const string Incomplete = "incomplete";
 
         public const string IncompleteExpired = "incomplete_expired";
+
+        public const string PastDue = "past_due";
+
+        public const string Trialing = "trialing";
+
+        public const string Unpaid = "unpaid";
     }
 }
