793ab9d526ae8243e1f4c8a684d33d6cd5418695
Pavel Krymets <pakrym@stripe.com>
2022-05-19 14:14:45 -0700

    fix test helper standard methods

--- src/Stripe.net/Services/TestHelpers/Treasury/ReceivedDebits/ReceivedDebitService.cs
+++ src/Stripe.net/Services/TestHelpers/Treasury/ReceivedDebits/ReceivedDebitService.cs
@@ -1,7 +1,6 @@
 // File generated from our OpenAPI spec
 namespace Stripe.TestHelpers.Treasury
 {
-    using System.Net.Http;
     using System.Threading;
     using System.Threading.Tasks;
     using Stripe.Treasury;
@@ -20,14 +19,14 @@ namespace Stripe.TestHelpers.Treasury
 
         public override string BasePath => "/v1/test_helpers/treasury/received_debits";
 
-        public virtual ReceivedDebit Create(ReceivedDebitCreateOptions options = null, RequestOptions requestOptions = null)
+        public virtual ReceivedDebit Create(ReceivedDebitCreateOptions options, RequestOptions requestOptions = null)
         {
-            return this.Request(HttpMethod.Post, $"{this.InstanceUrl("undefined")}", options, requestOptions);
+            return this.CreateEntity(options, requestOptions);
         }
 
-        public virtual Task<ReceivedDebit> CreateAsync(ReceivedDebitCreateOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
+        public virtual Task<ReceivedDebit> CreateAsync(ReceivedDebitCreateOptions options, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
         {
-            return this.RequestAsync(HttpMethod.Post, $"{this.InstanceUrl("undefined")}", options, requestOptions, cancellationToken);
+            return this.CreateEntityAsync(options, requestOptions, cancellationToken);
         }
     }
 }
