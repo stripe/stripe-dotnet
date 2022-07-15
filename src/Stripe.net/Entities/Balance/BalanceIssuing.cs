00cc0bf7aba91e412e2580d1de204e308626db65
Dominic Charley-Roy <dcr@stripe.com>
2022-07-15 16:52:40 -0400

    Rename BalanceDetails to BalanceIssuing.

--- src/Stripe.net/Entities/Balance/BalanceIssuing.cs
+++ src/Stripe.net/Entities/Balance/BalanceIssuing.cs
@@ -1,10 +1,14 @@
+// File generated from our OpenAPI spec
 namespace Stripe
 {
     using System.Collections.Generic;
     using Newtonsoft.Json;
 
-    public class BalanceDetails : StripeEntity<BalanceDetails>
+    public class BalanceIssuing : StripeEntity<BalanceIssuing>
     {
+        /// <summary>
+        /// Funds that are available for use.
+        /// </summary>
         [JsonProperty("available")]
         public List<BalanceAmount> Available { get; set; }
     }
