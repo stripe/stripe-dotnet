0f34b2a505cfc2b662fbb8da6ea08231e26cb121
Kamil Pajdzik <kamil@stripe.com>
2022-07-27 14:07:09 -0700

    Fix tests

--- src/StripeTests/Services/Customers/CustomerCreateOptionsTest.cs
+++ src/StripeTests/Services/Customers/CustomerCreateOptionsTest.cs
@@ -16,7 +16,6 @@ namespace StripeTests
                 {
                     options = new CustomerCreateOptions
                     {
-                        TrialEnd = DateTime.Parse("Fri, 13 Feb 2009 23:31:30Z"),
                     },
                     want = "trial_end=1234567890",
                 },
@@ -24,7 +23,6 @@ namespace StripeTests
                 {
                     options = new CustomerCreateOptions
                     {
-                        TrialEnd = SubscriptionTrialEnd.Now,
                     },
                     want = "trial_end=now",
                 },
