361614c73b16a774559536b1fac98d6e4d6c5561
Yejia Chen <yejia@stripe.com>
2022-09-06 16:28:30 -0400

    Codegen for openapi v189

--- src/Stripe.net/Services/FileLinks/FileLinkCreateOptions.cs
+++ src/Stripe.net/Services/FileLinks/FileLinkCreateOptions.cs
@@ -20,7 +20,8 @@ namespace Stripe
         /// <c>business_icon</c>, <c>business_logo</c>, <c>customer_signature</c>,
         /// <c>dispute_evidence</c>, <c>finance_report_run</c>,
         /// <c>identity_document_downloadable</c>, <c>pci_document</c>, <c>selfie</c>,
-        /// <c>sigma_scheduled_query</c>, or <c>tax_document_user_upload</c>.
+        /// <c>sigma_scheduled_query</c>, <c>tax_document_user_upload</c>, or
+        /// <c>terminal_reader_splashscreen</c>.
         /// </summary>
         [JsonProperty("file")]
         public string File { get; set; }
