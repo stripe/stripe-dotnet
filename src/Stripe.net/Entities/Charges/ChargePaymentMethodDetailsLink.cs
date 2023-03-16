5305600bb58637d7f1999ebb71dcfa2367fb47d8
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-03-16 20:52:03 +0000

    Update generated code for v263

--- src/Stripe.net/Entities/Charges/ChargePaymentMethodDetailsLink.cs
+++ src/Stripe.net/Entities/Charges/ChargePaymentMethodDetailsLink.cs
@@ -1,7 +1,15 @@
 // File generated from our OpenAPI spec
 namespace Stripe
 {
+    using Newtonsoft.Json;
+
     public class ChargePaymentMethodDetailsLink : StripeEntity<ChargePaymentMethodDetailsLink>
     {
+        /// <summary>
+        /// Two-letter ISO code representing the funding source country beneath the Link payment.
+        /// You could use this attribute to get a sense of international fees.
+        /// </summary>
+        [JsonProperty("country")]
+        public string Country { get; set; }
     }
 }
