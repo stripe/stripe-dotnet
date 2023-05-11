3420cc49847bc984124cb8e79e85103a1b6577e7
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-05-11 15:22:13 +0000

    Update generated code for v336

--- src/Stripe.net/Entities/InvoiceLineItems/InvoiceLineItemTaxAmount.cs
+++ src/Stripe.net/Entities/InvoiceLineItems/InvoiceLineItemTaxAmount.cs
@@ -48,5 +48,23 @@ namespace Stripe
         [JsonConverter(typeof(ExpandableFieldConverter<TaxRate>))]
         internal ExpandableField<TaxRate> InternalTaxRate { get; set; }
         #endregion
+
+        /// <summary>
+        /// The reasoning behind this tax, for example, if the product is tax exempt. The possible
+        /// values for this field may be extended as new tax rules are supported.
+        /// One of: <c>customer_exempt</c>, <c>not_collecting</c>, <c>not_subject_to_tax</c>,
+        /// <c>not_supported</c>, <c>portion_product_exempt</c>, <c>portion_reduced_rated</c>,
+        /// <c>portion_standard_rated</c>, <c>product_exempt</c>, <c>product_exempt_holiday</c>,
+        /// <c>proportionally_rated</c>, <c>reduced_rated</c>, <c>reverse_charge</c>,
+        /// <c>standard_rated</c>, <c>taxable_basis_reduced</c>, or <c>zero_rated</c>.
+        /// </summary>
+        [JsonProperty("taxability_reason")]
+        public string TaxabilityReason { get; set; }
+
+        /// <summary>
+        /// The amount on which tax is calculated, in %s.
+        /// </summary>
+        [JsonProperty("taxable_amount")]
+        public long? TaxableAmount { get; set; }
     }
 }
