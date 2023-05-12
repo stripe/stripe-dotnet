c2ad4a3ef72fd839a88c9c08f7d0e08cef0923f9
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-05-12 00:14:22 +0000

    Update generated code for v339

--- src/Stripe.net/Services/Subscriptions/SubscriptionUpdateOptions.cs
+++ src/Stripe.net/Services/Subscriptions/SubscriptionUpdateOptions.cs
@@ -239,10 +239,9 @@ namespace Stripe
         /// <summary>
         /// If set, the proration will be calculated as though the subscription was updated at the
         /// given time. This can be used to apply exactly the same proration that was previewed with
-        /// <a href="https://stripe.com/docs/api#retrieve_customer_invoice">upcoming invoice</a>
-        /// endpoint. It can also be used to implement custom proration logic, such as prorating by
-        /// day instead of by second, by providing the time that you wish to use for proration
-        /// calculations.
+        /// <a href="https://stripe.com/docs/api#upcoming_invoice">upcoming invoice</a> endpoint. It
+        /// can also be used to implement custom proration logic, such as prorating by day instead
+        /// of by second, by providing the time that you wish to use for proration calculations.
         /// </summary>
         [JsonProperty("proration_date")]
         [JsonConverter(typeof(UnixDateTimeConverter))]
