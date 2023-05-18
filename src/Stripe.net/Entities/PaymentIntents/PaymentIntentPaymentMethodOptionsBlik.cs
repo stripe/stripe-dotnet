71b681dd23378f21cae1204c1c1780dbe2ca528b
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-05-18 22:59:41 +0000

    Update generated code for v350

--- src/Stripe.net/Entities/PaymentIntents/PaymentIntentPaymentMethodOptionsBlik.cs
+++ src/Stripe.net/Entities/PaymentIntents/PaymentIntentPaymentMethodOptionsBlik.cs
@@ -1,11 +1,7 @@
 // File generated from our OpenAPI spec
 namespace Stripe
 {
-    using Newtonsoft.Json;
-
     public class PaymentIntentPaymentMethodOptionsBlik : StripeEntity<PaymentIntentPaymentMethodOptionsBlik>
     {
-        [JsonProperty("mandate_options")]
-        public PaymentIntentPaymentMethodOptionsBlikMandateOptions MandateOptions { get; set; }
     }
 }
