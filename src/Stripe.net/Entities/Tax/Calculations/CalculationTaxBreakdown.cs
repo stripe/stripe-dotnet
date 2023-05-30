126dbc9688fcd70c8587e28c87c61ddd4daff4a9
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-05-30 17:56:57 +0000

    Update generated code for v363

--- src/Stripe.net/Entities/Tax/Calculations/CalculationTaxBreakdown.cs
+++ src/Stripe.net/Entities/Tax/Calculations/CalculationTaxBreakdown.cs
@@ -21,18 +21,6 @@ namespace Stripe.Tax
         public CalculationTaxBreakdownTaxRateDetails TaxRateDetails { get; set; }
 
         /// <summary>
-        /// The reasoning behind this tax, for example, if the product is tax exempt. The possible
-        /// values for this field may be extended as new tax rules are supported.
-        /// One of: <c>customer_exempt</c>, <c>not_collecting</c>, <c>not_subject_to_tax</c>,
-        /// <c>not_supported</c>, <c>portion_product_exempt</c>, <c>portion_reduced_rated</c>,
-        /// <c>portion_standard_rated</c>, <c>product_exempt</c>, <c>product_exempt_holiday</c>,
-        /// <c>proportionally_rated</c>, <c>reduced_rated</c>, <c>reverse_charge</c>,
-        /// <c>standard_rated</c>, <c>taxable_basis_reduced</c>, or <c>zero_rated</c>.
-        /// </summary>
-        [JsonProperty("taxability_reason")]
-        public string TaxabilityReason { get; set; }
-
-        /// <summary>
         /// The amount on which tax is calculated, in integer cents.
         /// </summary>
         [JsonProperty("taxable_amount")]
