a4dececf794734a17426507614034e8399f1386b
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-06-01 15:50:14 +0000

    Update generated code for v368

--- src/Stripe.net/Entities/PaymentMethods/PaymentMethodPaypal.cs
+++ src/Stripe.net/Entities/PaymentMethods/PaymentMethodPaypal.cs
@@ -13,6 +13,13 @@ namespace Stripe
         public string Fingerprint { get; set; }
 
         /// <summary>
+        /// Owner's email. Values are provided by PayPal directly (if supported) at the time of
+        /// authorization or settlement. They cannot be set or mutated.
+        /// </summary>
+        [JsonProperty("payer_email")]
+        public string PayerEmail { get; set; }
+
+        /// <summary>
         /// PayPal account PayerID. This identifier uniquely identifies the PayPal customer.
         /// </summary>
         [JsonProperty("payer_id")]
