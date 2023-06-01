105be1215217ab417a88e77f2e9c25fea3d34959
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-06-01 19:34:05 +0000

    Update generated code for v369

--- src/Stripe.net/Entities/Checkout/Sessions/SessionCustomFieldText.cs
+++ src/Stripe.net/Entities/Checkout/Sessions/SessionCustomFieldText.cs
@@ -6,6 +6,18 @@ namespace Stripe.Checkout
     public class SessionCustomFieldText : StripeEntity<SessionCustomFieldText>
     {
         /// <summary>
+        /// The maximum character length constraint for the customer's input.
+        /// </summary>
+        [JsonProperty("maximum_length")]
+        public long? MaximumLength { get; set; }
+
+        /// <summary>
+        /// The minimum character length requirement for the customer's input.
+        /// </summary>
+        [JsonProperty("minimum_length")]
+        public long? MinimumLength { get; set; }
+
+        /// <summary>
         /// The value entered by the customer.
         /// </summary>
         [JsonProperty("value")]
