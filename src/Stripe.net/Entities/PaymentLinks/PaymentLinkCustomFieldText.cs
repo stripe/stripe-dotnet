d422a92f8c656d50140217b7b340900110d8abba
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-07-03 13:16:06 +0000

    Update generated code for v407

---
+++ src/Stripe.net/Entities/PaymentLinks/PaymentLinkCustomFieldText.cs
@@ -0,0 +1,20 @@
+// File generated from our OpenAPI spec
+namespace Stripe
+{
+    using Newtonsoft.Json;
+
+    public class PaymentLinkCustomFieldText : StripeEntity<PaymentLinkCustomFieldText>
+    {
+        /// <summary>
+        /// The maximum character length constraint for the customer's input.
+        /// </summary>
+        [JsonProperty("maximum_length")]
+        public long? MaximumLength { get; set; }
+
+        /// <summary>
+        /// The minimum character length requirement for the customer's input.
+        /// </summary>
+        [JsonProperty("minimum_length")]
+        public long? MinimumLength { get; set; }
+    }
+}
