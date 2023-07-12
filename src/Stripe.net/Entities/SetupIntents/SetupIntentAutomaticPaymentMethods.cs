f30872f93035fe773d632e548444b922843e0857
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-07-12 21:39:47 +0000

    Update generated code for v417

--- src/Stripe.net/Entities/SetupIntents/SetupIntentAutomaticPaymentMethods.cs
+++ src/Stripe.net/Entities/SetupIntents/SetupIntentAutomaticPaymentMethods.cs
@@ -6,6 +6,19 @@ namespace Stripe
     public class SetupIntentAutomaticPaymentMethods : StripeEntity<SetupIntentAutomaticPaymentMethods>
     {
         /// <summary>
+        /// Controls whether this SetupIntent will accept redirect-based payment methods.
+        ///
+        /// Redirect-based payment methods may require your customer to be redirected to a payment
+        /// method's app or site for authentication or additional steps. To <a
+        /// href="https://stripe.com/docs/api/setup_intents/confirm">confirm</a> this SetupIntent,
+        /// you may be required to provide a <c>return_url</c> to redirect customers back to your
+        /// site after they authenticate or complete the setup.
+        /// One of: <c>always</c>, or <c>never</c>.
+        /// </summary>
+        [JsonProperty("allow_redirects")]
+        public string AllowRedirects { get; set; }
+
+        /// <summary>
         /// Automatically calculates compatible payment methods.
         /// </summary>
         [JsonProperty("enabled")]
