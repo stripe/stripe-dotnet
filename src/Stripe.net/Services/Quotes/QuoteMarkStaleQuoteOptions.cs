7fad9f3e73e9f46ed4d3ea74c324a5abbcffe594
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-08-02 20:07:49 +0000

    Update generated code for v440

--- src/Stripe.net/Services/Quotes/QuoteMarkStaleQuoteOptions.cs
+++ src/Stripe.net/Services/Quotes/QuoteMarkStaleQuoteOptions.cs
@@ -1,7 +1,14 @@
 // File generated from our OpenAPI spec
 namespace Stripe
 {
+    using Newtonsoft.Json;
+
     public class QuoteMarkStaleQuoteOptions : BaseOptions
     {
+        /// <summary>
+        /// Reason the Quote is being marked stale.
+        /// </summary>
+        [JsonProperty("reason")]
+        public string Reason { get; set; }
     }
 }
