b7418b3ebc7172f57a81d9d8fa636491c24da62c
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-08-03 19:21:27 +0000

    Update generated code for v446

--- src/Stripe.net/Entities/Prices/PriceCurrencyOptions.cs
+++ src/Stripe.net/Entities/Prices/PriceCurrencyOptions.cs
@@ -33,15 +33,15 @@ namespace Stripe
         public List<PriceCurrencyOptionsTier> Tiers { get; set; }
 
         /// <summary>
-        /// The unit amount in %s to be charged, represented as a whole integer if possible. Only
-        /// set if <c>billing_scheme=per_unit</c>.
+        /// The unit amount in cents (or local equivalent) to be charged, represented as a whole
+        /// integer if possible. Only set if <c>billing_scheme=per_unit</c>.
         /// </summary>
         [JsonProperty("unit_amount")]
         public long? UnitAmount { get; set; }
 
         /// <summary>
-        /// The unit amount in %s to be charged, represented as a decimal string with at most 12
-        /// decimal places. Only set if <c>billing_scheme=per_unit</c>.
+        /// The unit amount in cents (or local equivalent) to be charged, represented as a decimal
+        /// string with at most 12 decimal places. Only set if <c>billing_scheme=per_unit</c>.
         /// </summary>
         [JsonProperty("unit_amount_decimal")]
         public decimal? UnitAmountDecimal { get; set; }
