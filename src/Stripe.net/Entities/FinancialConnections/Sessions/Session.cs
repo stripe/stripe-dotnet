52d9436578b0082516fe53ca8667d520784eac5c
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-08-21 23:05:48 +0000

    Update generated code for v475

--- src/Stripe.net/Entities/FinancialConnections/Sessions/Session.cs
+++ src/Stripe.net/Entities/FinancialConnections/Sessions/Session.cs
@@ -57,6 +57,12 @@ namespace Stripe.FinancialConnections
         public List<string> Permissions { get; set; }
 
         /// <summary>
+        /// Data features requested to be retrieved upon account creation.
+        /// </summary>
+        [JsonProperty("prefetch")]
+        public List<string> Prefetch { get; set; }
+
+        /// <summary>
         /// For webview integrations only. Upon completing OAuth login in the native browser, the
         /// user will be redirected to this URL to return to your app.
         /// </summary>
