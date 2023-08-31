62b8d965d33d5ffd2355b8b6a76b2f6af10250b5
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-08-31 17:19:20 +0000

    Update generated code for v498

--- src/Stripe.net/Services/Checkout/Sessions/SessionPaymentMethodOptionsCustomerBalanceBankTransferOptions.cs
+++ src/Stripe.net/Services/Checkout/Sessions/SessionPaymentMethodOptionsCustomerBalanceBankTransferOptions.cs
@@ -23,11 +23,6 @@ namespace Stripe.Checkout
 
         /// <summary>
         /// The list of bank transfer types that this PaymentIntent is allowed to use for funding.
-        /// Permitted values include: <c>us_bank_account</c>, <c>eu_bank_account</c>,
-        /// <c>id_bank_account</c>, <c>gb_bank_account</c>, <c>jp_bank_account</c>,
-        /// <c>mx_bank_account</c>, <c>eu_bank_transfer</c>, <c>gb_bank_transfer</c>,
-        /// <c>id_bank_transfer</c>, <c>jp_bank_transfer</c>, <c>mx_bank_transfer</c>, or
-        /// <c>us_bank_transfer</c>.
         /// One of: <c>eu_bank_transfer</c>, <c>gb_bank_transfer</c>, <c>jp_bank_transfer</c>,
         /// <c>mx_bank_transfer</c>, or <c>us_bank_transfer</c>.
         /// </summary>
