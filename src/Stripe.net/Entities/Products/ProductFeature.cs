f885e4d88a153731bdbf81ee323911fa5524693c
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-01 16:45:40 +0000

    Update generated code for v500

---
+++ src/Stripe.net/Entities/Products/ProductFeature.cs
@@ -0,0 +1,14 @@
+// File generated from our OpenAPI spec
+namespace Stripe
+{
+    using Newtonsoft.Json;
+
+    public class ProductFeature : StripeEntity<ProductFeature>
+    {
+        /// <summary>
+        /// The feature's name. Up to 80 characters long.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
