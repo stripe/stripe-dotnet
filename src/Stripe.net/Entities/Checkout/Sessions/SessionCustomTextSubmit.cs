bc4faa5117e1348f51ff49b412d7ab29fc416f72
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-21 17:59:22 +0000

    Update generated code for v545

--- src/Stripe.net/Entities/Checkout/Sessions/SessionCustomTextSubmit.cs
+++ src/Stripe.net/Entities/Checkout/Sessions/SessionCustomTextSubmit.cs
@@ -6,7 +6,7 @@ namespace Stripe.Checkout
     public class SessionCustomTextSubmit : StripeEntity<SessionCustomTextSubmit>
     {
         /// <summary>
-        /// Text may be up to 1000 characters in length.
+        /// Text may be up to 1200 characters in length.
         /// </summary>
         [JsonProperty("message")]
         public string Message { get; set; }
