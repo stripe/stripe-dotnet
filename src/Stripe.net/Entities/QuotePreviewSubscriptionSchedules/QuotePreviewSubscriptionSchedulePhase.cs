931e8d42ce72b66c5ff06027016223073772d2b9
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-10-24 17:46:17 +0000

    Update generated code for v617

--- src/Stripe.net/Entities/QuotePreviewSubscriptionSchedules/QuotePreviewSubscriptionSchedulePhase.cs
+++ src/Stripe.net/Entities/QuotePreviewSubscriptionSchedules/QuotePreviewSubscriptionSchedulePhase.cs
@@ -138,7 +138,8 @@ namespace Stripe
 
         /// <summary>
         /// Subscription description, meant to be displayable to the customer. Use this field to
-        /// optionally store an explanation of the subscription.
+        /// optionally store an explanation of the subscription for rendering in Stripe surfaces and
+        /// certain local payment methods UIs.
         /// </summary>
         [JsonProperty("description")]
         public string Description { get; set; }
