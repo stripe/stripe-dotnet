b81312ed577204b5dd0998dd31b4b56feb7b69b4
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-12-04 18:34:20 +0000

    Update generated code for v695

--- src/Stripe.net/Services/PaymentLinks/PaymentLinkSubscriptionDataOptions.cs
+++ src/Stripe.net/Services/PaymentLinks/PaymentLinkSubscriptionDataOptions.cs
@@ -30,5 +30,11 @@ namespace Stripe
         /// </summary>
         [JsonProperty("trial_period_days")]
         public long? TrialPeriodDays { get; set; }
+
+        /// <summary>
+        /// Settings related to subscription trials.
+        /// </summary>
+        [JsonProperty("trial_settings")]
+        public PaymentLinkSubscriptionDataTrialSettingsOptions TrialSettings { get; set; }
     }
 }
