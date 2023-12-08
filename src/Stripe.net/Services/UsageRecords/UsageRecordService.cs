d67447bbd3584b3ab85c96257bd8c95e205a7223
Pavel Krymets <pakrym@stripe.com>
2023-12-08 14:57:57 -0800

    Add backcompat check

--- src/Stripe.net/Services/UsageRecords/UsageRecordService.cs
+++ src/Stripe.net/Services/UsageRecords/UsageRecordService.cs
@@ -5,8 +5,7 @@ namespace Stripe
     using System.Threading;
     using System.Threading.Tasks;
 
-    public class UsageRecordService : ServiceNested<UsageRecord>,
-        INestedCreatable<UsageRecord, UsageRecordCreateOptions>
+    public class UsageRecordService : ServiceNested<UsageRecord>
     {
         public UsageRecordService()
             : base(null)
@@ -20,9 +19,9 @@ namespace Stripe
 
         public override string BasePath => "/v1/subscription_items/{PARENT_ID}/usage_records";
 
-        public virtual UsageRecord Create(string parentId, UsageRecordCreateOptions options = null, RequestOptions requestOptions = null)
+        public virtual UsageRecord Create(string parentId, UsageRecordCreateOptions options = null)
         {
-            return this.Request<UsageRecord>(HttpMethod.Post, $"/v1/subscription_items/{parentId}/usage_records", options, requestOptions);
+            return this.Request<UsageRecord>(HttpMethod.Post, $"/v1/subscription_items/{parentId}/usage_records", options, null);
         }
 
         public virtual Task<UsageRecord> CreateAsync(string parentId, UsageRecordCreateOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
