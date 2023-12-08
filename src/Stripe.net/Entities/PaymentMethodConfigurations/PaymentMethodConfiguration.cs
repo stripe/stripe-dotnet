c02c34d3275669be14ac053973ecbc903e2e9fb3
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-12-08 23:47:39 +0000

    Update generated code for v709

--- src/Stripe.net/Entities/PaymentMethodConfigurations/PaymentMethodConfiguration.cs
+++ src/Stripe.net/Entities/PaymentMethodConfigurations/PaymentMethodConfiguration.cs
@@ -113,6 +113,9 @@ namespace Stripe
         [JsonProperty("grabpay")]
         public PaymentMethodConfigurationGrabpay Grabpay { get; set; }
 
+        [JsonProperty("id_bank_transfer")]
+        public PaymentMethodConfigurationIdBankTransfer IdBankTransfer { get; set; }
+
         [JsonProperty("ideal")]
         public PaymentMethodConfigurationIdeal Ideal { get; set; }
 
@@ -142,11 +145,17 @@ namespace Stripe
         [JsonProperty("livemode")]
         public bool Livemode { get; set; }
 
+        [JsonProperty("multibanco")]
+        public PaymentMethodConfigurationMultibanco Multibanco { get; set; }
+
         /// <summary>
         /// The configuration's name.
         /// </summary>
         [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("netbanking")]
+        public PaymentMethodConfigurationNetbanking Netbanking { get; set; }
 
         [JsonProperty("oxxo")]
         public PaymentMethodConfigurationOxxo Oxxo { get; set; }
@@ -159,6 +168,9 @@ namespace Stripe
         /// </summary>
         [JsonProperty("parent")]
         public string Parent { get; set; }
+
+        [JsonProperty("pay_by_bank")]
+        public PaymentMethodConfigurationPayByBank PayByBank { get; set; }
 
         [JsonProperty("paynow")]
         public PaymentMethodConfigurationPaynow Paynow { get; set; }
@@ -174,6 +186,9 @@ namespace Stripe
 
         [JsonProperty("sofort")]
         public PaymentMethodConfigurationSofort Sofort { get; set; }
+
+        [JsonProperty("upi")]
+        public PaymentMethodConfigurationUpi Upi { get; set; }
 
         [JsonProperty("us_bank_account")]
         public PaymentMethodConfigurationUsBankAccount UsBankAccount { get; set; }
