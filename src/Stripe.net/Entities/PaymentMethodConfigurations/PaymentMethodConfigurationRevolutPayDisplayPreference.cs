1a47ade355a52c34bb0b03a50e874aa673b6543a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-12-18 18:00:35 +0000

    Update generated code for v721

---
+++ src/Stripe.net/Entities/PaymentMethodConfigurations/PaymentMethodConfigurationRevolutPayDisplayPreference.cs
@@ -0,0 +1,29 @@
+// File generated from our OpenAPI spec
+namespace Stripe
+{
+    using Newtonsoft.Json;
+
+    public class PaymentMethodConfigurationRevolutPayDisplayPreference : StripeEntity<PaymentMethodConfigurationRevolutPayDisplayPreference>
+    {
+        /// <summary>
+        /// For child configs, whether or not the account's preference will be observed. If
+        /// <c>false</c>, the parent configuration's default is used.
+        /// </summary>
+        [JsonProperty("overridable")]
+        public bool? Overridable { get; set; }
+
+        /// <summary>
+        /// The account's display preference.
+        /// One of: <c>none</c>, <c>off</c>, or <c>on</c>.
+        /// </summary>
+        [JsonProperty("preference")]
+        public string Preference { get; set; }
+
+        /// <summary>
+        /// The effective display preference value.
+        /// One of: <c>off</c>, or <c>on</c>.
+        /// </summary>
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+}
