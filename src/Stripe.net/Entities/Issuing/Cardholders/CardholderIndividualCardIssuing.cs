20b692d246873965148a2bc83586614b583d5656
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-08 18:23:21 +0000

    Update generated code for v748

--- src/Stripe.net/Entities/Issuing/Cardholders/CardholderIndividualCardIssuing.cs
+++ src/Stripe.net/Entities/Issuing/Cardholders/CardholderIndividualCardIssuing.cs
@@ -6,8 +6,9 @@ namespace Stripe.Issuing
     public class CardholderIndividualCardIssuing : StripeEntity<CardholderIndividualCardIssuing>
     {
         /// <summary>
-        /// Information about cardholder acceptance of <a
-        /// href="https://stripe.com/docs/issuing/cards">Authorized User Terms</a>.
+        /// Information about cardholder acceptance of Celtic <a
+        /// href="https://stripe.com/docs/issuing/cards#accept-authorized-user-terms">Authorized
+        /// User Terms</a>. Required for cards backed by a Celtic program.
         /// </summary>
         [JsonProperty("user_terms_acceptance")]
         public CardholderIndividualCardIssuingUserTermsAcceptance UserTermsAcceptance { get; set; }
