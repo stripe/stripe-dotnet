d867fcacebaa294c255725ac0483c8ed4388080c
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-23 10:32:37 +0000

    Update generated code for v780

--- src/Stripe.net/Entities/Checkout/Sessions/SessionAutomaticTax.cs
+++ src/Stripe.net/Entities/Checkout/Sessions/SessionAutomaticTax.cs
@@ -12,6 +12,14 @@ namespace Stripe.Checkout
         public bool Enabled { get; set; }
 
         /// <summary>
+        /// The account that's liable for tax. If set, the business address and tax registrations
+        /// required to perform the tax calculation are loaded from this account. The tax
+        /// transaction is returned in the report of the connected account.
+        /// </summary>
+        [JsonProperty("liability")]
+        public SessionAutomaticTaxLiability Liability { get; set; }
+
+        /// <summary>
         /// The status of the most recent automated tax calculation for this session.
         /// One of: <c>complete</c>, <c>failed</c>, or <c>requires_location_inputs</c>.
         /// </summary>
