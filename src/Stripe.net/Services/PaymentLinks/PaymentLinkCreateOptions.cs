2ba523f7a54f5302a2aabe73ccb62b0cf1850ffc
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-23 16:08:45 +0000

    Update generated code for v782

--- src/Stripe.net/Services/PaymentLinks/PaymentLinkCreateOptions.cs
+++ src/Stripe.net/Services/PaymentLinks/PaymentLinkCreateOptions.cs
@@ -63,7 +63,7 @@ namespace Stripe
         public string Currency { get; set; }
 
         /// <summary>
-        /// Collect additional information from your customer using custom fields. Up to 2 fields
+        /// Collect additional information from your customer using custom fields. Up to 3 fields
         /// are supported.
         /// </summary>
         [JsonProperty("custom_fields")]
