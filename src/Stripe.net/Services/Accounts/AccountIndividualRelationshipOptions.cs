e30097a4297214f9408e5858ec6526e379c60b2e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-02-01 00:11:16 +0000

    Update generated code for v806

---
+++ src/Stripe.net/Services/Accounts/AccountIndividualRelationshipOptions.cs
@@ -0,0 +1,41 @@
+// File generated from our OpenAPI spec
+namespace Stripe
+{
+    using Newtonsoft.Json;
+
+    public class AccountIndividualRelationshipOptions : INestedOptions
+    {
+        /// <summary>
+        /// Whether the person is a director of the account's legal entity. Directors are typically
+        /// members of the governing board of the company, or responsible for ensuring the company
+        /// meets its regulatory obligations.
+        /// </summary>
+        [JsonProperty("director")]
+        public bool? Director { get; set; }
+
+        /// <summary>
+        /// Whether the person has significant responsibility to control, manage, or direct the
+        /// organization.
+        /// </summary>
+        [JsonProperty("executive")]
+        public bool? Executive { get; set; }
+
+        /// <summary>
+        /// Whether the person is an owner of the account’s legal entity.
+        /// </summary>
+        [JsonProperty("owner")]
+        public bool? Owner { get; set; }
+
+        /// <summary>
+        /// The percent owned by the person of the account's legal entity.
+        /// </summary>
+        [JsonProperty("percent_ownership")]
+        public decimal? PercentOwnership { get; set; }
+
+        /// <summary>
+        /// The person's title (e.g., CEO, Support Engineer).
+        /// </summary>
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+}
