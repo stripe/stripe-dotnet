99490946bfe4c2c531507007944ee16abb066fd0
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-02-01 22:30:42 +0000

    Update generated code for v808

--- src/Stripe.net/Services/Subscriptions/SubscriptionInvoiceSettingsOptions.cs
+++ src/Stripe.net/Services/Subscriptions/SubscriptionInvoiceSettingsOptions.cs
@@ -1,10 +1,18 @@
 // File generated from our OpenAPI spec
 namespace Stripe
 {
+    using System.Collections.Generic;
     using Newtonsoft.Json;
 
     public class SubscriptionInvoiceSettingsOptions : INestedOptions
     {
+        /// <summary>
+        /// The account tax IDs associated with the subscription. Will be set on invoices generated
+        /// by the subscription.
+        /// </summary>
+        [JsonProperty("account_tax_ids")]
+        public List<string> AccountTaxIds { get; set; }
+
         /// <summary>
         /// The connected account that issues the invoice. The invoice is presented with the
         /// branding and support information of the specified account.
