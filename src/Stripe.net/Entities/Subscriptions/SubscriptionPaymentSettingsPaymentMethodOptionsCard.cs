625112cee1c7d62f914a87628a22f523fbd28f9c
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-02-15 10:15:26 +0000

    Update generated code for v829

--- src/Stripe.net/Entities/Subscriptions/SubscriptionPaymentSettingsPaymentMethodOptionsCard.cs
+++ src/Stripe.net/Entities/Subscriptions/SubscriptionPaymentSettingsPaymentMethodOptionsCard.cs
@@ -24,9 +24,9 @@ namespace Stripe
         /// href="https://stripe.com/docs/strong-customer-authentication">other requirements</a>.
         /// However, if you wish to request 3D Secure based on logic from your own fraud engine,
         /// provide this option. Read our guide on <a
-        /// href="https://stripe.com/docs/payments/3d-secure#manual-three-ds">manually requesting 3D
-        /// Secure</a> for more information on how this configuration interacts with Radar and our
-        /// SCA Engine.
+        /// href="https://stripe.com/docs/payments/3d-secure/authentication-flow#manual-three-ds">manually
+        /// requesting 3D Secure</a> for more information on how this configuration interacts with
+        /// Radar and our SCA Engine.
         /// One of: <c>any</c>, <c>automatic</c>, or <c>challenge</c>.
         /// </summary>
         [JsonProperty("request_three_d_secure")]
