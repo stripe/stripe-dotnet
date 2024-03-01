0426635aff0088edcb4c514c148ccfe81da71077
Richard Marmorstein <52928443+richardm-stripe@users.noreply.github.com>
2024-02-29 17:09:51 -0800

    Explicitly define paths in manually maintained services (#2862)
    * Explicitly define paths in manually maintained services
    
    * Remove unused pragmas

--- src/Stripe.net/Services/Cards/CardService.cs
+++ src/Stripe.net/Services/Cards/CardService.cs
@@ -2,6 +2,7 @@ namespace Stripe
 {
     using System;
     using System.Collections.Generic;
+    using System.Net.Http;
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -12,7 +13,6 @@ namespace Stripe
         INestedRetrievable<Card, CardGetOptions>,
         INestedUpdatable<Card, CardUpdateOptions>
     {
-#pragma warning disable CS0618 // Type or member is obsolete
         public CardService()
             : base(null)
         {
@@ -28,63 +28,62 @@ namespace Stripe
 
         public virtual Card Create(string parentId, CardCreateOptions options, RequestOptions requestOptions = null)
         {
-            return this.CreateNestedEntity(parentId, options, requestOptions);
+            return this.Request<Card>(HttpMethod.Post, $"/v1/customers/{parentId}/sources", options, requestOptions);
         }
 
         public virtual Task<Card> CreateAsync(string parentId, CardCreateOptions options, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
         {
-            return this.CreateNestedEntityAsync(parentId, options, requestOptions, cancellationToken);
+            return this.RequestAsync<Card>(HttpMethod.Post, $"/v1/customers/{parentId}/sources", options, requestOptions, cancellationToken);
         }
 
         public virtual Card Delete(string parentId, string id, CardDeleteOptions options = null, RequestOptions requestOptions = null)
         {
-            return this.DeleteNestedEntity(parentId, id, options, requestOptions);
+            return this.Request<Card>(HttpMethod.Delete, $"/v1/customers/{parentId}/sources/{id}", options, requestOptions);
         }
 
         public virtual Task<Card> DeleteAsync(string parentId, string id, CardDeleteOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
         {
-            return this.DeleteNestedEntityAsync(parentId, id, options, requestOptions, cancellationToken);
+            return this.RequestAsync<Card>(HttpMethod.Delete, $"/v1/customers/{parentId}/sources/{id}", options, requestOptions, cancellationToken);
         }
 
         public virtual Card Get(string parentId, string id, CardGetOptions options = null, RequestOptions requestOptions = null)
         {
-            return this.GetNestedEntity(parentId, id, options, requestOptions);
+            return this.Request<Card>(HttpMethod.Get, $"/v1/customers/{parentId}/sources/{id}", options, requestOptions);
         }
 
         public virtual Task<Card> GetAsync(string parentId, string id, CardGetOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
         {
-            return this.GetNestedEntityAsync(parentId, id, options, requestOptions, cancellationToken);
+            return this.RequestAsync<Card>(HttpMethod.Get, $"/v1/customers/{parentId}/sources/{id}", options, requestOptions, cancellationToken);
         }
 
         public virtual StripeList<Card> List(string parentId, CardListOptions options = null, RequestOptions requestOptions = null)
         {
-            return this.ListNestedEntities(parentId, options ?? new CardListOptions(), requestOptions);
+            return this.Request<StripeList<Card>>(HttpMethod.Get, $"/v1/customers/{parentId}/sources", options ?? new CardListOptions(), requestOptions);
         }
 
         public virtual Task<StripeList<Card>> ListAsync(string parentId, CardListOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
         {
-            return this.ListNestedEntitiesAsync(parentId, options ?? new CardListOptions(), requestOptions, cancellationToken);
+            return this.RequestAsync<StripeList<Card>>(HttpMethod.Get, $"/v1/customers/{parentId}/sources", options ?? new CardListOptions(), requestOptions, cancellationToken);
         }
 
         public virtual IEnumerable<Card> ListAutoPaging(string parentId, CardListOptions options = null, RequestOptions requestOptions = null)
         {
-            return this.ListNestedEntitiesAutoPaging(parentId, options ?? new CardListOptions(), requestOptions);
+            return this.ListRequestAutoPaging<Card>($"/v1/customers/{parentId}/sources", options ?? new CardListOptions(), requestOptions);
         }
 
         public virtual IAsyncEnumerable<Card> ListAutoPagingAsync(string parentId, CardListOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
         {
-            return this.ListNestedEntitiesAutoPagingAsync(parentId, options ?? new CardListOptions(), requestOptions, cancellationToken);
+            return this.ListRequestAutoPagingAsync<Card>($"/v1/customers/{parentId}/sources", options ?? new CardListOptions(), requestOptions, cancellationToken);
         }
 
         public virtual Card Update(string parentId, string id, CardUpdateOptions options, RequestOptions requestOptions = null)
         {
-            return this.UpdateNestedEntity(parentId, id, options, requestOptions);
+            return this.Request<Card>(HttpMethod.Post, $"/v1/customers/{parentId}/sources/{id}", options, requestOptions);
         }
 
         public virtual Task<Card> UpdateAsync(string parentId, string id, CardUpdateOptions options, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
         {
-            return this.UpdateNestedEntityAsync(parentId, id, options, requestOptions, cancellationToken);
+            return this.RequestAsync<Card>(HttpMethod.Post, $"/v1/customers/{parentId}/sources/{id}", options, requestOptions, cancellationToken);
         }
-#pragma warning restore CS0618 // Type or member is obsolete
     }
 }
