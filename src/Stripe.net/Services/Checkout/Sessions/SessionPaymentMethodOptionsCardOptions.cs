ff4939278734cc6a3d6c8a8e81947ee919d959c2
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-04 10:13:45 +0000

    Update generated code for v860

--- src/Stripe.net/Services/Checkout/Sessions/SessionPaymentMethodOptionsCardOptions.cs
+++ src/Stripe.net/Services/Checkout/Sessions/SessionPaymentMethodOptionsCardOptions.cs
@@ -12,6 +12,21 @@ namespace Stripe.Checkout
         public SessionPaymentMethodOptionsCardInstallmentsOptions Installments { get; set; }
 
         /// <summary>
+        /// We strongly recommend that you rely on our SCA Engine to automatically prompt your
+        /// customers for authentication based on risk level and <a
+        /// href="https://stripe.com/docs/strong-customer-authentication">other requirements</a>.
+        /// However, if you wish to request 3D Secure based on logic from your own fraud engine,
+        /// provide this option. If not provided, this value defaults to <c>automatic</c>. Read our
+        /// guide on <a
+        /// href="https://stripe.com/docs/payments/3d-secure/authentication-flow#manual-three-ds">manually
+        /// requesting 3D Secure</a> for more information on how this configuration interacts with
+        /// Radar and our SCA Engine.
+        /// One of: <c>any</c>, <c>automatic</c>, or <c>challenge</c>.
+        /// </summary>
+        [JsonProperty("request_three_d_secure")]
+        public string RequestThreeDSecure { get; set; }
+
+        /// <summary>
         /// Indicates that you intend to make future payments with this PaymentIntent's payment
         /// method.
         ///
