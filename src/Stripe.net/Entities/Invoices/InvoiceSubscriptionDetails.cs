07f6bc25903f4964e3081e2e891c1d37cc40159e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-06 22:57:20 +0000

    Update generated code for v866

--- src/Stripe.net/Entities/Invoices/InvoiceSubscriptionDetails.cs
+++ src/Stripe.net/Entities/Invoices/InvoiceSubscriptionDetails.cs
@@ -7,9 +7,10 @@ namespace Stripe
     public class InvoiceSubscriptionDetails : StripeEntity<InvoiceSubscriptionDetails>, IHasMetadata
     {
         /// <summary>
-        /// Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that will
-        /// reflect the metadata of the subscription at the time of invoice creation. <em>Note: This
-        /// attribute is populated only for invoices created on or after June 29, 2023.</em>.
+        /// Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> defined as
+        /// subscription metadata when an invoice is created. Becomes an immutable snapshot of the
+        /// subscription metadata at the time of invoice finalization. <em>Note: This attribute is
+        /// populated only for invoices created on or after June 29, 2023.</em>.
         /// </summary>
         [JsonProperty("metadata")]
         public Dictionary<string, string> Metadata { get; set; }
