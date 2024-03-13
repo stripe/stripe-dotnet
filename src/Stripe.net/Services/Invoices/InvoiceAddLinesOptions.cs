ca432ef23f45771e846ffa5445239c8a81d5bebe
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-13 14:49:36 +0000

    Update generated code for v875

---
+++ src/Stripe.net/Services/Invoices/InvoiceAddLinesOptions.cs
@@ -0,0 +1,24 @@
+// File generated from our OpenAPI spec
+namespace Stripe
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    public class InvoiceAddLinesOptions : BaseOptions
+    {
+        /// <summary>
+        /// Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can
+        /// attach to an object. This can be useful for storing additional information about the
+        /// object in a structured format. Individual keys can be unset by posting an empty value to
+        /// them. All keys can be unset by posting an empty value to <c>metadata</c>.
+        /// </summary>
+        [JsonProperty("invoice_metadata")]
+        public Dictionary<string, string> InvoiceMetadata { get; set; }
+
+        /// <summary>
+        /// The line items to add.
+        /// </summary>
+        [JsonProperty("lines")]
+        public List<InvoiceLineOptions> Lines { get; set; }
+    }
+}
