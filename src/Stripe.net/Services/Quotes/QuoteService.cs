0cc116cb65a0ff080b4becf00383cb5183b6a444
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-15 21:38:57 +0000

    Update generated code for v879

--- src/Stripe.net/Services/Quotes/QuoteService.cs
+++ src/Stripe.net/Services/Quotes/QuoteService.cs
@@ -235,7 +235,12 @@ namespace Stripe
         public virtual Stream Pdf(string id, QuotePdfOptions options = null, RequestOptions requestOptions = null)
         {
             requestOptions ??= new RequestOptions();
-            requestOptions.BaseUrl ??= this.Client.FilesBase;
+            if (requestOptions.BaseUrl == null)
+            {
+                requestOptions = requestOptions.Clone();
+                requestOptions.BaseUrl = this.Client.FilesBase;
+            }
+
             return this.RequestStreaming(HttpMethod.Get, $"/v1/quotes/{id}/pdf", options, requestOptions);
         }
 
@@ -245,7 +250,12 @@ namespace Stripe
         public virtual Task<Stream> PdfAsync(string id, QuotePdfOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
         {
             requestOptions ??= new RequestOptions();
-            requestOptions.BaseUrl ??= this.Client.FilesBase;
+            if (requestOptions.BaseUrl == null)
+            {
+                requestOptions = requestOptions.Clone();
+                requestOptions.BaseUrl = this.Client.FilesBase;
+            }
+
             return this.RequestStreamingAsync(HttpMethod.Get, $"/v1/quotes/{id}/pdf", options, requestOptions, cancellationToken);
         }
 
