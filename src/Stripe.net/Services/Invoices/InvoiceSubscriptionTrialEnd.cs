85a016f2d4794515ca3fd287fc3580d12cdf4762
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-19 16:57:28 +0000

    Update generated code for v882

---
+++ src/Stripe.net/Services/Invoices/InvoiceSubscriptionTrialEnd.cs
@@ -0,0 +1,13 @@
+// File generated from our OpenAPI spec
+namespace Stripe
+{
+    public class InvoiceSubscriptionTrialEnd : StringEnum
+    {
+        public static readonly InvoiceSubscriptionTrialEnd Now = new InvoiceSubscriptionTrialEnd("now");
+
+        private InvoiceSubscriptionTrialEnd(string value)
+            : base(value)
+        {
+        }
+    }
+}
