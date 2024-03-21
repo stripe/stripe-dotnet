504fa9e77fa1b0c6e2a32912f6fcad53745e98c5
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-21 05:56:17 +0000

    Update generated code for v891

--- src/Stripe.net/Services/Subscriptions/SubscriptionUpdateOptions.cs
+++ src/Stripe.net/Services/Subscriptions/SubscriptionUpdateOptions.cs
@@ -172,7 +172,10 @@ namespace Stripe
         public string OnBehalfOf { get; set; }
 
         /// <summary>
-        /// If specified, payment collection for this subscription will be paused.
+        /// If specified, payment collection for this subscription will be paused. Note that the
+        /// subscription status will be unchanged and will not be updated to <c>paused</c>. Learn
+        /// more about <a href="https://stripe.com/billing/subscriptions/pause-payment">pausing
+        /// collection</a>.
         /// </summary>
         [JsonProperty("pause_collection")]
         public SubscriptionPauseCollectionOptions PauseCollection { get; set; }
