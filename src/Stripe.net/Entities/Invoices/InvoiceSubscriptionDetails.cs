6ba6bc5fcbfd3a50e5fc07f2304f69d83cb041bc
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-21 05:57:21 +0000

    Update generated code for v891

--- src/Stripe.net/Entities/Invoices/InvoiceSubscriptionDetails.cs
+++ src/Stripe.net/Entities/Invoices/InvoiceSubscriptionDetails.cs
@@ -16,7 +16,10 @@ namespace Stripe
         public Dictionary<string, string> Metadata { get; set; }
 
         /// <summary>
-        /// If specified, payment collection for this subscription will be paused.
+        /// If specified, payment collection for this subscription will be paused. Note that the
+        /// subscription status will be unchanged and will not be updated to <c>paused</c>. Learn
+        /// more about <a href="https://stripe.com/billing/subscriptions/pause-payment">pausing
+        /// collection</a>.
         /// </summary>
         [JsonProperty("pause_collection")]
         public InvoiceSubscriptionDetailsPauseCollection PauseCollection { get; set; }
