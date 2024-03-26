f012da458b3f27541e9f415334b4abd23fdb0e9e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-26 23:02:14 +0000

    Update generated code for v908

--- src/StripeTests/Services/GeneratedExamplesTest.cs
+++ src/StripeTests/Services/GeneratedExamplesTest.cs
@@ -4,6 +4,7 @@ namespace StripeTests
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Net;
     using System.Net.Http;
     using System.Threading.Tasks;
     using Stripe;
