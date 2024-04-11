6aba421ec09c0f86dfd6cb3cacee68505224ec90
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-11 23:09:33 +0000

    Update generated code for v951

--- src/Stripe.net/Entities/PaymentIntents/PaymentIntentPaymentMethodOptionsRevolutPay.cs
+++ src/Stripe.net/Entities/PaymentIntents/PaymentIntentPaymentMethodOptionsRevolutPay.cs
@@ -1,7 +1,14 @@
 // File generated from our OpenAPI spec
 namespace Stripe
 {
+    using Newtonsoft.Json;
+
     public class PaymentIntentPaymentMethodOptionsRevolutPay : StripeEntity<PaymentIntentPaymentMethodOptionsRevolutPay>
     {
+        /// <summary>
+        /// Controls when the funds will be captured from the customer's account.
+        /// </summary>
+        [JsonProperty("capture_method")]
+        public string CaptureMethod { get; set; }
     }
 }
