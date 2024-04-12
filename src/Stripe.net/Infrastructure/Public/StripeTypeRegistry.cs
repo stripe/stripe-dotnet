f8e3c52dbb039c29ecfd958e8fb9a40642c86cf2
Richard Marmorstein <52928443+richardm-stripe@users.noreply.github.com>
2024-04-12 08:40:31 -0700

    wip (#2892)

--- src/Stripe.net/Infrastructure/Public/StripeTypeRegistry.cs
+++ src/Stripe.net/Infrastructure/Public/StripeTypeRegistry.cs
@@ -1,4 +1,3 @@
-// File generated from our OpenAPI spec
 namespace Stripe
 {
     using System;
@@ -15,6 +14,7 @@ namespace Stripe
         public static readonly IReadOnlyDictionary<string, Type> ObjectsToTypes = new ReadOnlyDictionary<string, Type>(
             new Dictionary<string, Type>
             {
+                // ObjectsToTypes: The beginning of the section generated from our OpenAPI spec
                 { "account", typeof(Account) },
                 { "account_link", typeof(AccountLink) },
                 { "account_session", typeof(AccountSession) },
@@ -27,18 +27,9 @@ namespace Stripe
                 { "bank_account", typeof(BankAccount) },
                 { "billing.meter", typeof(Billing.Meter) },
                 { "billing.meter_event", typeof(Billing.MeterEvent) },
-                {
-                    "billing.meter_event_adjustment", typeof(
-                        Billing.MeterEventAdjustment)
-                },
-                {
-                    "billing.meter_event_summary", typeof(
-                        Billing.MeterEventSummary)
-                },
-                {
-                    "billing_portal.configuration", typeof(
-                        BillingPortal.Configuration)
-                },
+                { "billing.meter_event_adjustment", typeof(Billing.MeterEventAdjustment) },
+                { "billing.meter_event_summary", typeof(Billing.MeterEventSummary) },
+                { "billing_portal.configuration", typeof(BillingPortal.Configuration) },
                 { "billing_portal.session", typeof(BillingPortal.Session) },
                 { "capability", typeof(Capability) },
                 { "card", typeof(Card) },
@@ -49,76 +40,49 @@ namespace Stripe
                 { "climate.product", typeof(Climate.Product) },
                 { "climate.supplier", typeof(Climate.Supplier) },
                 { "confirmation_token", typeof(ConfirmationToken) },
-                {
-                    "connect_collection_transfer", typeof(
-                        ConnectCollectionTransfer)
-                },
+                { "connect_collection_transfer", typeof(ConnectCollectionTransfer) },
                 { "country_spec", typeof(CountrySpec) },
                 { "coupon", typeof(Coupon) },
                 { "credit_note", typeof(CreditNote) },
                 { "credit_note_line_item", typeof(CreditNoteLineItem) },
                 { "customer", typeof(Customer) },
-                {
-                    "customer_balance_transaction", typeof(
-                        CustomerBalanceTransaction)
-                },
-                {
-                    "customer_cash_balance_transaction", typeof(
-                        CustomerCashBalanceTransaction)
-                },
+                { "customer_balance_transaction", typeof(CustomerBalanceTransaction) },
+                { "customer_cash_balance_transaction", typeof(CustomerCashBalanceTransaction) },
                 { "customer_session", typeof(CustomerSession) },
                 { "discount", typeof(Discount) },
                 { "dispute", typeof(Dispute) },
-                {
-                    "entitlements.active_entitlement", typeof(
-                        Entitlements.ActiveEntitlement)
-                },
+                { "entitlements.active_entitlement", typeof(Entitlements.ActiveEntitlement) },
                 { "entitlements.feature", typeof(Entitlements.Feature) },
                 { "ephemeral_key", typeof(EphemeralKey) },
                 { "event", typeof(Event) },
                 { "fee_refund", typeof(ApplicationFeeRefund) },
                 { "file", typeof(File) },
                 { "file_link", typeof(FileLink) },
-                {
-                    "financial_connections.account", typeof(
-                        FinancialConnections.Account)
-                },
+                { "financial_connections.account", typeof(FinancialConnections.Account) },
                 {
                     "financial_connections.account_owner", typeof(
-                        FinancialConnections.AccountOwner)
+                    FinancialConnections.AccountOwner)
                 },
                 {
                     "financial_connections.account_ownership", typeof(
-                        FinancialConnections.AccountOwnership)
+                    FinancialConnections.AccountOwnership)
                 },
-                {
-                    "financial_connections.session", typeof(
-                        FinancialConnections.Session)
-                },
+                { "financial_connections.session", typeof(FinancialConnections.Session) },
                 {
                     "financial_connections.transaction", typeof(
-                        FinancialConnections.Transaction)
+                    FinancialConnections.Transaction)
                 },
                 { "forwarding.request", typeof(Forwarding.Request) },
                 { "funding_instructions", typeof(FundingInstructions) },
-                {
-                    "identity.verification_report", typeof(
-                        Identity.VerificationReport)
-                },
-                {
-                    "identity.verification_session", typeof(
-                        Identity.VerificationSession)
-                },
+                { "identity.verification_report", typeof(Identity.VerificationReport) },
+                { "identity.verification_session", typeof(Identity.VerificationSession) },
                 { "invoice", typeof(Invoice) },
                 { "invoiceitem", typeof(InvoiceItem) },
                 { "issuing.authorization", typeof(Issuing.Authorization) },
                 { "issuing.card", typeof(Issuing.Card) },
                 { "issuing.cardholder", typeof(Issuing.Cardholder) },
                 { "issuing.dispute", typeof(Issuing.Dispute) },
-                {
-                    "issuing.personalization_design", typeof(
-                        Issuing.PersonalizationDesign)
-                },
+                { "issuing.personalization_design", typeof(Issuing.PersonalizationDesign) },
                 { "issuing.physical_bundle", typeof(Issuing.PhysicalBundle) },
                 { "issuing.token", typeof(Issuing.Token) },
                 { "issuing.transaction", typeof(Issuing.Transaction) },
@@ -129,10 +93,7 @@ namespace Stripe
                 { "payment_intent", typeof(PaymentIntent) },
                 { "payment_link", typeof(PaymentLink) },
                 { "payment_method", typeof(PaymentMethod) },
-                {
-                    "payment_method_configuration", typeof(
-                        PaymentMethodConfiguration)
-                },
+                { "payment_method_configuration", typeof(PaymentMethodConfiguration) },
                 { "payment_method_domain", typeof(PaymentMethodDomain) },
                 { "payout", typeof(Payout) },
                 { "person", typeof(Person) },
@@ -143,9 +104,7 @@ namespace Stripe
                 { "product_feature", typeof(ProductFeature) },
                 { "promotion_code", typeof(PromotionCode) },
                 { "quote", typeof(Quote) },
-                {
-                    "radar.early_fraud_warning", typeof(Radar.EarlyFraudWarning)
-                },
+                { "radar.early_fraud_warning", typeof(Radar.EarlyFraudWarning) },
                 { "radar.value_list", typeof(Radar.ValueList) },
                 { "radar.value_list_item", typeof(Radar.ValueListItem) },
                 { "refund", typeof(Refund) },
@@ -158,33 +117,23 @@ namespace Stripe
                 { "setup_intent", typeof(SetupIntent) },
                 { "shipping_rate", typeof(ShippingRate) },
                 { "source", typeof(Source) },
-                {
-                    "source_mandate_notification", typeof(
-                        SourceMandateNotification)
-                },
+                { "source_mandate_notification", typeof(SourceMandateNotification) },
                 { "source_transaction", typeof(SourceTransaction) },
                 { "subscription", typeof(Subscription) },
                 { "subscription_item", typeof(SubscriptionItem) },
                 { "subscription_schedule", typeof(SubscriptionSchedule) },
                 { "tax.calculation", typeof(Tax.Calculation) },
-                {
-                    "tax.calculation_line_item", typeof(Tax.CalculationLineItem)
-                },
+                { "tax.calculation_line_item", typeof(Tax.CalculationLineItem) },
                 { "tax.registration", typeof(Tax.Registration) },
                 { "tax.settings", typeof(Tax.Settings) },
                 { "tax.transaction", typeof(Tax.Transaction) },
-                {
-                    "tax.transaction_line_item", typeof(Tax.TransactionLineItem)
-                },
+                { "tax.transaction_line_item", typeof(Tax.TransactionLineItem) },
                 { "tax_code", typeof(TaxCode) },
                 { "tax_deducted_at_source", typeof(TaxDeductedAtSource) },
                 { "tax_id", typeof(TaxId) },
                 { "tax_rate", typeof(TaxRate) },
                 { "terminal.configuration", typeof(Terminal.Configuration) },
-                {
-                    "terminal.connection_token", typeof(
-                        Terminal.ConnectionToken)
-                },
+                { "terminal.connection_token", typeof(Terminal.ConnectionToken) },
                 { "terminal.location", typeof(Terminal.Location) },
                 { "terminal.reader", typeof(Terminal.Reader) },
                 { "test_helpers.test_clock", typeof(TestHelpers.TestClock) },
@@ -194,36 +143,23 @@ namespace Stripe
                 { "transfer_reversal", typeof(TransferReversal) },
                 { "treasury.credit_reversal", typeof(Treasury.CreditReversal) },
                 { "treasury.debit_reversal", typeof(Treasury.DebitReversal) },
-                {
-                    "treasury.financial_account", typeof(
-                        Treasury.FinancialAccount)
-                },
+                { "treasury.financial_account", typeof(Treasury.FinancialAccount) },
                 {
                     "treasury.financial_account_features", typeof(
-                        Treasury.FinancialAccountFeatures)
+                    Treasury.FinancialAccountFeatures)
                 },
-                {
-                    "treasury.inbound_transfer", typeof(
-                        Treasury.InboundTransfer)
-                },
-                {
-                    "treasury.outbound_payment", typeof(
-                        Treasury.OutboundPayment)
-                },
-                {
-                    "treasury.outbound_transfer", typeof(
-                        Treasury.OutboundTransfer)
-                },
+                { "treasury.inbound_transfer", typeof(Treasury.InboundTransfer) },
+                { "treasury.outbound_payment", typeof(Treasury.OutboundPayment) },
+                { "treasury.outbound_transfer", typeof(Treasury.OutboundTransfer) },
                 { "treasury.received_credit", typeof(Treasury.ReceivedCredit) },
                 { "treasury.received_debit", typeof(Treasury.ReceivedDebit) },
                 { "treasury.transaction", typeof(Treasury.Transaction) },
-                {
-                    "treasury.transaction_entry", typeof(
-                        Treasury.TransactionEntry)
-                },
+                { "treasury.transaction_entry", typeof(Treasury.TransactionEntry) },
                 { "usage_record", typeof(UsageRecord) },
                 { "usage_record_summary", typeof(UsageRecordSummary) },
                 { "webhook_endpoint", typeof(WebhookEndpoint) },
+
+                // ObjectsToTypes: The end of the section generated from our OpenAPI spec
             });
 
         /// <summary>
