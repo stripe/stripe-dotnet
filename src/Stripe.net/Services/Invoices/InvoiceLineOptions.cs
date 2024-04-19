e025795ce70a24fd8aa6ef567150e867c2a961ea
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-19 23:20:32 +0000

    Update generated code for v974

--- src/Stripe.net/Services/Invoices/InvoiceLineOptions.cs
+++ src/Stripe.net/Services/Invoices/InvoiceLineOptions.cs
@@ -69,7 +69,10 @@ namespace Stripe
         /// Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can
         /// attach to an object. This can be useful for storing additional information about the
         /// object in a structured format. Individual keys can be unset by posting an empty value to
-        /// them. All keys can be unset by posting an empty value to <c>metadata</c>.
+        /// them. All keys can be unset by posting an empty value to <c>metadata</c>. For
+        /// <c>type=recurring</c> line items, the incoming metadata specified on the request is
+        /// directly used to set this value, in contrast to <c>type=invoiceitem</c> line items,
+        /// where any existing metadata on the invoice line is merged with the incoming data.
         /// </summary>
         [JsonProperty("metadata")]
         public Dictionary<string, string> Metadata { get; set; }
