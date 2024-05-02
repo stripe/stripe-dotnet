3fc044da334078b9854a4e64a3b0a0c75b96727e
helenye-stripe <111009531+helenye-stripe@users.noreply.github.com>
2024-05-02 08:51:31 -0700

    Fix clientId getter (#2903)
    * Fix clientId getter and add a 8.0 appsettings

--- src/Stripe.net/Infrastructure/Public/StripeConfiguration.cs
+++ src/Stripe.net/Infrastructure/Public/StripeConfiguration.cs
@@ -70,7 +70,7 @@ namespace Stripe
         {
             get
             {
-                if (string.IsNullOrEmpty(apiKey) &&
+                if (string.IsNullOrEmpty(clientId) &&
                     !string.IsNullOrEmpty(ConfigurationManager.AppSettings["StripeClientId"]))
                 {
                     clientId = ConfigurationManager.AppSettings["StripeClientId"];
