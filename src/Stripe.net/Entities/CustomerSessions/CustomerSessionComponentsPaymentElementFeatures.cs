e83c735e4864a7dc45fc6d8501e9ded939a4d4e2
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-10 01:08:28 +0000

    Update generated code for v1024

--- src/Stripe.net/Entities/CustomerSessions/CustomerSessionComponentsPaymentElementFeatures.cs
+++ src/Stripe.net/Entities/CustomerSessions/CustomerSessionComponentsPaymentElementFeatures.cs
@@ -7,7 +7,13 @@ namespace Stripe
     {
         /// <summary>
         /// Controls whether the Payment Element displays the option to remove a saved payment
-        /// method.
+        /// method.".
+        ///
+        /// Allowing buyers to remove their saved payment methods impacts subscriptions that depend
+        /// on that payment method. Removing the payment method detaches the <a
+        /// href="https://docs.stripe.com/api/payment_methods/object#payment_method_object-customer"><c>customer</c>
+        /// object</a> from that <a
+        /// href="https://docs.stripe.com/api/payment_methods">PaymentMethod</a>.
         /// One of: <c>disabled</c>, or <c>enabled</c>.
         /// </summary>
         [JsonProperty("payment_method_remove")]
@@ -16,6 +22,13 @@ namespace Stripe
         /// <summary>
         /// Controls whether the Payment Element displays a checkbox offering to save a new payment
         /// method.
+        ///
+        /// If a customer checks the box, the <a
+        /// href="https://docs.stripe.com/api/payment_methods/object#payment_method_object-allow_redisplay"><c>allow_redisplay</c></a>
+        /// value on the PaymentMethod is set to <c>'always'</c> at confirmation time. For
+        /// PaymentIntents, the <a
+        /// href="https://docs.stripe.com/api/payment_intents/object#payment_intent_object-setup_future_usage"><c>setup_future_usage</c></a>
+        /// value is also set to the value defined in <c>payment_method_save_usage</c>.
         /// One of: <c>disabled</c>, or <c>enabled</c>.
         /// </summary>
         [JsonProperty("payment_method_save")]
