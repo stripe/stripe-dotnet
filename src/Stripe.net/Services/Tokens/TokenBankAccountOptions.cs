e9a249f14669f6c8dd8403de07246240ffc68864
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-15 01:38:17 +0000

    Update generated code for v1037

--- src/Stripe.net/Services/Tokens/TokenBankAccountOptions.cs
+++ src/Stripe.net/Services/Tokens/TokenBankAccountOptions.cs
@@ -62,10 +62,10 @@ namespace Stripe
         public string PaymentMethod { get; set; }
 
         /// <summary>
-        /// The routing number, sort code, or other country-appropriateinstitution number for the
-        /// bank account. For US bank accounts, this is required and should bethe ACH routing
-        /// number, not the wire routing number. If you are providing an IBAN
-        /// for<c>account_number</c>, this field is not required.
+        /// The routing number, sort code, or other country-appropriate institution number for the
+        /// bank account. For US bank accounts, this is required and should be the ACH routing
+        /// number, not the wire routing number. If you are providing an IBAN for
+        /// <c>account_number</c>, this field is not required.
         /// </summary>
         [JsonProperty("routing_number")]
         public string RoutingNumber { get; set; }
