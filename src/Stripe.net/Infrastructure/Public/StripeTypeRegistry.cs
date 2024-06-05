d04a398491e93285c7fe88a3c07e4e298057876c
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-06-05 14:36:05 +0000

    Update generated code for v1062

--- src/Stripe.net/Infrastructure/Public/StripeTypeRegistry.cs
+++ src/Stripe.net/Infrastructure/Public/StripeTypeRegistry.cs
@@ -102,6 +102,7 @@ namespace Stripe
                 { "payout", typeof(Payout) },
                 { "person", typeof(Person) },
                 { "plan", typeof(Plan) },
+                { "platform_tax_fee", typeof(PlatformTaxFee) },
                 { "price", typeof(Price) },
                 { "product", typeof(Product) },
                 { "product_feature", typeof(ProductFeature) },
