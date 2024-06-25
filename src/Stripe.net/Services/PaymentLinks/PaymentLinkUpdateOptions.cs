cd49027ae2f96e106b6e3feab6c24c1ea1f96395
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-06-25 08:17:07 +0000

    Update generated code for v1096

--- src/Stripe.net/Services/PaymentLinks/PaymentLinkUpdateOptions.cs
+++ src/Stripe.net/Services/PaymentLinks/PaymentLinkUpdateOptions.cs
@@ -125,7 +125,7 @@ namespace Stripe
         /// <c>cashapp</c>, <c>eps</c>, <c>fpx</c>, <c>giropay</c>, <c>grabpay</c>, <c>ideal</c>,
         /// <c>klarna</c>, <c>konbini</c>, <c>link</c>, <c>mobilepay</c>, <c>oxxo</c>, <c>p24</c>,
         /// <c>paynow</c>, <c>paypal</c>, <c>pix</c>, <c>promptpay</c>, <c>sepa_debit</c>,
-        /// <c>sofort</c>, <c>swish</c>, <c>us_bank_account</c>, or <c>wechat_pay</c>.
+        /// <c>sofort</c>, <c>swish</c>, <c>us_bank_account</c>, <c>wechat_pay</c>, or <c>zip</c>.
         /// </summary>
         [JsonProperty("payment_method_types")]
         public List<string> PaymentMethodTypes { get; set; }
