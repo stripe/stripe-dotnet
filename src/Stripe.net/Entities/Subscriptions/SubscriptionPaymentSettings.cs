b14981c58735767e50f44e29a4187cd0ceb8c427
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-07-17 12:23:00 +0000

    Update generated code for v1132

--- src/Stripe.net/Entities/Subscriptions/SubscriptionPaymentSettings.cs
+++ src/Stripe.net/Entities/Subscriptions/SubscriptionPaymentSettings.cs
@@ -24,9 +24,9 @@ namespace Stripe
         /// <c>amazon_pay</c>, <c>au_becs_debit</c>, <c>bacs_debit</c>, <c>bancontact</c>,
         /// <c>boleto</c>, <c>card</c>, <c>cashapp</c>, <c>customer_balance</c>, <c>eps</c>,
         /// <c>fpx</c>, <c>giropay</c>, <c>grabpay</c>, <c>ideal</c>, <c>konbini</c>, <c>link</c>,
-        /// <c>p24</c>, <c>paynow</c>, <c>paypal</c>, <c>promptpay</c>, <c>revolut_pay</c>,
-        /// <c>sepa_credit_transfer</c>, <c>sepa_debit</c>, <c>sofort</c>, <c>swish</c>,
-        /// <c>us_bank_account</c>, or <c>wechat_pay</c>.
+        /// <c>multibanco</c>, <c>p24</c>, <c>paynow</c>, <c>paypal</c>, <c>promptpay</c>,
+        /// <c>revolut_pay</c>, <c>sepa_credit_transfer</c>, <c>sepa_debit</c>, <c>sofort</c>,
+        /// <c>swish</c>, <c>us_bank_account</c>, or <c>wechat_pay</c>.
         /// </summary>
         [JsonProperty("payment_method_types")]
         public List<string> PaymentMethodTypes { get; set; }
