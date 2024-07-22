fab78311f77b08e78ecade7cf23861fba4cb7bfd
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-07-22 18:53:36 +0000

    Update generated code for v1140

--- src/Stripe.net/Entities/Charges/ChargePaymentMethodDetailsBlik.cs
+++ src/Stripe.net/Entities/Charges/ChargePaymentMethodDetailsBlik.cs
@@ -1,7 +1,14 @@
 // File generated from our OpenAPI spec
 namespace Stripe
 {
+    using Newtonsoft.Json;
+
     public class ChargePaymentMethodDetailsBlik : StripeEntity<ChargePaymentMethodDetailsBlik>
     {
+        /// <summary>
+        /// A unique and immutable identifier assigned by BLIK to every buyer.
+        /// </summary>
+        [JsonProperty("buyer_id")]
+        public string BuyerId { get; set; }
     }
 }
