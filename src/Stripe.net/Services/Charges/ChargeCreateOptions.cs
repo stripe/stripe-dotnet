cf75a80e02f4fd15ca737081df223b25e78ef18e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-08-14 16:58:54 +0000

    Update generated code for v1199

--- src/Stripe.net/Services/Charges/ChargeCreateOptions.cs
+++ src/Stripe.net/Services/Charges/ChargeCreateOptions.cs
@@ -146,7 +146,7 @@ namespace Stripe
 
         /// <summary>
         /// Provides information about a card charge. Concatenated to the account's <a
-        /// href="https://docs.corp.stripe.com/get-started/account/statement-descriptors#static">statement
+        /// href="https://docs.stripe.com/get-started/account/statement-descriptors#static">statement
         /// descriptor prefix</a> to form the complete statement descriptor that appears on the
         /// customer's statement. If the account has no prefix value, the suffix is concatenated to
         /// the account's statement descriptor.
