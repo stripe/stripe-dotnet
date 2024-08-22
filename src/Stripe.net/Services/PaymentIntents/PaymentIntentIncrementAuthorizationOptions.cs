b0753480bd5a3c1dbf42d8e54a7e56064ff1323a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-08-22 00:44:56 +0000

    Update generated code for v1212

--- src/Stripe.net/Services/PaymentIntents/PaymentIntentIncrementAuthorizationOptions.cs
+++ src/Stripe.net/Services/PaymentIntents/PaymentIntentIncrementAuthorizationOptions.cs
@@ -47,14 +47,10 @@ namespace Stripe
 
         /// <summary>
         /// Text that appears on the customer's statement as the statement descriptor for a non-card
-        /// charge. This value overrides the account's default statement descriptor. For information
-        /// about requirements, including the 22-character limit, see <a
+        /// or card charge. This value overrides the account's default statement descriptor. For
+        /// information about requirements, including the 22-character limit, see <a
         /// href="https://docs.stripe.com/get-started/account/statement-descriptors">the Statement
         /// Descriptor docs</a>.
-        ///
-        /// Setting this value for a card charge returns an error. For card charges, set the <a
-        /// href="https://docs.stripe.com/get-started/account/statement-descriptors#dynamic">statement_descriptor_suffix</a>
-        /// instead.
         /// </summary>
         [JsonProperty("statement_descriptor")]
         public string StatementDescriptor { get; set; }
