97cffb546f7547846f92e760588a2fe37c1590fe
Jesse Rosalia <jar@stripe.com>
2024-09-25 10:48:13 -0700

    added example project

---
+++ src/Examples/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Examples
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public Program()
+        {
+        }
+
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            //await MeterEventExample.Run();
+        }
+    }
+}
