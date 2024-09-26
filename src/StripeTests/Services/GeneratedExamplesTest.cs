cac350f5c95c0de84bca6832c805c4fbce16fcb5
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-09-26 16:25:07 +0000

    Update generated code for v1268

--- src/StripeTests/Services/GeneratedExamplesTest.cs
+++ src/StripeTests/Services/GeneratedExamplesTest.cs
@@ -4485,21 +4485,6 @@ namespace StripeTests
         }
 
         [Fact]
-        public void TestTerminalReadersProcessSetupIntentPost()
-        {
-            var options = new Stripe.Terminal.ReaderProcessSetupIntentOptions
-            {
-                SetupIntent = "seti_xxxxxxxxxxxxx",
-                AllowRedisplay = "always",
-            };
-            var service = new Stripe.Terminal.ReaderService(this.StripeClient);
-            service.ProcessSetupIntent("tmr_xxxxxxxxxxxxx", options);
-            this.AssertRequest(
-                HttpMethod.Post,
-                "/v1/terminal/readers/tmr_xxxxxxxxxxxxx/process_setup_intent");
-        }
-
-        [Fact]
         public void TestTestHelpersCustomersFundCashBalancePost()
         {
             var options = new Stripe.TestHelpers.CustomerFundCashBalanceOptions
