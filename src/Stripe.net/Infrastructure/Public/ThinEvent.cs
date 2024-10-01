c7eded49b734e8e3f151638be3a02caff93c6448
prathmesh-stripe <165320323+prathmesh-stripe@users.noreply.github.com>
2024-10-01 13:35:37 -0400

    Added better description for ThinEvent class (#2986)

--- src/Stripe.net/Infrastructure/Public/ThinEvent.cs
+++ src/Stripe.net/Infrastructure/Public/ThinEvent.cs
@@ -9,19 +9,34 @@ namespace Stripe
     /// </summary>
     public class ThinEvent
     {
+        /// <summary>
+        /// Unique identifier for the event.
+        /// </summary>
         [JsonProperty("id")]
         public string Id { get; internal set; }
 
+        /// <summary>
+        /// The type of the event.
+        /// </summary>
         [JsonProperty("type")]
         public string Type { get; internal set; }
 
+        /// <summary>
+        /// Time at which the object was created.
+        /// </summary>
         [JsonProperty("created")]
         public DateTime Created { get; internal set; }
 
 #nullable enable
+        /// <summary>
+        /// [Optional] Authentication context needed to fetch the event or related object.
+        /// </summary>
         [JsonProperty("context")]
         public string? Context { get; internal set; }
 
+        /// <summary>
+        /// [Optional] Object containing the reference to API resource relevant to the event.
+        /// </summary>
         [JsonProperty("related_object")]
         public ThinEventRelatedObject? RelatedObject { get; internal set; }
     }
