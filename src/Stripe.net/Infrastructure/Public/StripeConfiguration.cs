f8b339e9bbe7cc578ad734318d2c35197e617c49
jar-stripe <jar@stripe.com>
2024-10-03 13:50:51 -0700

    Remove special case Newtonsoft version, and update TargetFrameworks (#2992)
    * removed Newtonsoft reference from net8.0 targeted item group
    
    * added net7.0 and net8.0 into target frameworks to ensure dotnet build builds for those targets
    suppressed an obsolete warning in net8.0 (safe for now, will take care of later)
    
    * updated StripeNetTargetFramework string building in SystemNetHttpClient
    
    * added empty line to appease the style cop
    
    ---------
    
    Co-authored-by: Ramya Rao <100975018+ramya-stripe@users.noreply.github.com>

--- src/Stripe.net/Infrastructure/Public/StripeConfiguration.cs
+++ src/Stripe.net/Infrastructure/Public/StripeConfiguration.cs
@@ -214,7 +214,12 @@ namespace Stripe
         {
             return new JsonSerializerSettings
             {
+                // Disable the warning (applies to net8.0).
+#pragma warning disable SYSLIB0050
                 Context = new StreamingContext(StreamingContextStates.All, new DeserializationContext { Requestor = requestor }),
+
+                // Re-enable the warning.
+#pragma warning restore SYSLIB0050
                 Converters = new List<JsonConverter>
                 {
                     new StripeObjectConverter(),
