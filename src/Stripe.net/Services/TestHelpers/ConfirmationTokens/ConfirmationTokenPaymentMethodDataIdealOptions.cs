9df884ebb1333cfc8a7aadff52395d5365ef835b
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-10-15 16:31:58 +0000

    Update generated code for v1297

--- src/Stripe.net/Services/TestHelpers/ConfirmationTokens/ConfirmationTokenPaymentMethodDataIdealOptions.cs
+++ src/Stripe.net/Services/TestHelpers/ConfirmationTokens/ConfirmationTokenPaymentMethodDataIdealOptions.cs
@@ -6,7 +6,8 @@ namespace Stripe.TestHelpers
     public class ConfirmationTokenPaymentMethodDataIdealOptions : INestedOptions
     {
         /// <summary>
-        /// The customer's bank.
+        /// The customer's bank. Only use this parameter for existing customers. Don't use it for
+        /// new customers.
         /// One of: <c>abn_amro</c>, <c>asn_bank</c>, <c>bunq</c>, <c>handelsbanken</c>, <c>ing</c>,
         /// <c>knab</c>, <c>moneyou</c>, <c>n26</c>, <c>nn</c>, <c>rabobank</c>, <c>regiobank</c>,
         /// <c>revolut</c>, <c>sns_bank</c>, <c>triodos_bank</c>, <c>van_lanschot</c>, or
