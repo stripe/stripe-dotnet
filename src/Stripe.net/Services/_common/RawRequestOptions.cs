4999809cc536fad6aee2f7b79e178410403c746c
Jesse Rosalia <jar@stripe.com>
2025-02-05 15:53:40 -0800

    removed ApiMode from RawRequestOptions

--- src/Stripe.net/Services/_common/RawRequestOptions.cs
+++ src/Stripe.net/Services/_common/RawRequestOptions.cs
@@ -5,10 +5,6 @@ namespace Stripe
 
     public class RawRequestOptions : RequestOptions
     {
-        /// <summary>Gets or sets the API mode to use for the request.</summary>
-        [Obsolete("This property is no longer used and will be removed in a future release")]
-        public ApiMode ApiMode { get; set; }
-
         /// <summary>Gets or sets additional headers for the request.</summary>
         public Dictionary<string, string> AdditionalHeaders { get; set; } = new Dictionary<string, string>();
 
