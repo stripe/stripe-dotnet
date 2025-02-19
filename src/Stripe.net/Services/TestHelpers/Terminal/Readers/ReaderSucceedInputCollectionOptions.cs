5aa6add4489884194d3a9fa2442e9485f97814f9
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-02-19 17:05:46 +0000

    Update generated code for v1496

---
+++ src/Stripe.net/Services/TestHelpers/Terminal/Readers/ReaderSucceedInputCollectionOptions.cs
@@ -0,0 +1,21 @@
+// File generated from our OpenAPI spec
+namespace Stripe.TestHelpers.Terminal
+{
+    using Newtonsoft.Json;
+#if NET6_0_OR_GREATER
+    using STJS = System.Text.Json.Serialization;
+#endif
+
+    public class ReaderSucceedInputCollectionOptions : BaseOptions
+    {
+        /// <summary>
+        /// Skip behavior for input collection.
+        /// One of: <c>all</c>, or <c>none</c>.
+        /// </summary>
+        [JsonProperty("skip_non_required_inputs")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("skip_non_required_inputs")]
+#endif
+        public string SkipNonRequiredInputs { get; set; }
+    }
+}
