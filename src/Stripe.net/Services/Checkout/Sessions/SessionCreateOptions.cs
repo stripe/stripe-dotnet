a955989fb0c01d6e03ca3316bb0f311cf8efd72c
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-03-21 03:02:33 +0000

    Update generated code for v1605

--- src/Stripe.net/Services/Checkout/Sessions/SessionCreateOptions.cs
+++ src/Stripe.net/Services/Checkout/Sessions/SessionCreateOptions.cs
@@ -63,7 +63,7 @@ namespace Stripe.Checkout
         /// <summary>
         /// If set, Checkout displays a back button and customers will be directed to this URL if
         /// they decide to cancel payment and return to your website. This parameter is not allowed
-        /// if <c>ui_mode</c> is <c>embedded</c> or <c>custom</c>.
+        /// if ui_mode is <c>embedded</c> or <c>custom</c>.
         /// </summary>
         [JsonProperty("cancel_url")]
 #if NET6_0_OR_GREATER
@@ -441,7 +441,7 @@ namespace Stripe.Checkout
         /// The URL to redirect your customer back to after they authenticate or cancel their
         /// payment on the payment method's app or site. This parameter is required if
         /// <c>ui_mode</c> is <c>embedded</c> or <c>custom</c> and redirect-based payment methods
-        /// are enabled on the Checkout Session.
+        /// are enabled on the session.
         /// </summary>
         [JsonProperty("return_url")]
 #if NET6_0_OR_GREATER
@@ -513,10 +513,9 @@ namespace Stripe.Checkout
 
         /// <summary>
         /// The URL to which Stripe should send customers when payment or setup is complete. This
-        /// parameter is not allowed if <c>ui_mode</c> is <c>embedded</c> or <c>custom</c>. If you'd
-        /// like to use information from the successful Checkout Session on your page, read the
-        /// guide on <a
-        /// href="https://stripe.com/docs/payments/checkout/custom-success-page">customizing your
+        /// parameter is not allowed if ui_mode is <c>embedded</c> or <c>custom</c>. If you'd like
+        /// to use information from the successful Checkout Session on your page, read the guide on
+        /// <a href="https://stripe.com/docs/payments/checkout/custom-success-page">customizing your
         /// success page</a>.
         /// </summary>
         [JsonProperty("success_url")]
