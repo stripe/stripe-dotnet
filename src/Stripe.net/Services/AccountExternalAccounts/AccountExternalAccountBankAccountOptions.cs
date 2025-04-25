d2b31efbd14f2468d9c303eb6036dc545029969e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-04-25 22:39:28 +0000

    Update generated code for v1719

--- src/Stripe.net/Services/AccountExternalAccounts/AccountExternalAccountBankAccountOptions.cs
+++ src/Stripe.net/Services/AccountExternalAccounts/AccountExternalAccountBankAccountOptions.cs
@@ -65,10 +65,10 @@ namespace Stripe
         public string Currency { get; set; }
 
         /// <summary>
-        /// The routing number, sort code, or other country-appropriateinstitution number for the
-        /// bank account. For US bank accounts, this is required and should bethe ACH routing
-        /// number, not the wire routing number. If you are providing an IBAN
-        /// for<c>account_number</c>, this field is not required.
+        /// The routing number, sort code, or other country-appropriate institution number for the
+        /// bank account. For US bank accounts, this is required and should be the ACH routing
+        /// number, not the wire routing number. If you are providing an IBAN for
+        /// <c>account_number</c>, this field is not required.
         /// </summary>
         [JsonProperty("routing_number")]
 #if NET6_0_OR_GREATER
