7dda2c666099e70d2813d521c38a9c1ca9859453
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-05-15 00:03:06 +0000

    Update generated code for v1756

--- src/Stripe.net/Entities/PaymentIntents/PaymentIntentPaymentDetailsCarRentalDriver.cs
+++ src/Stripe.net/Entities/PaymentIntents/PaymentIntentPaymentDetailsCarRentalDriver.cs
@@ -9,6 +9,24 @@ namespace Stripe
     public class PaymentIntentPaymentDetailsCarRentalDriver : StripeEntity<PaymentIntentPaymentDetailsCarRentalDriver>
     {
         /// <summary>
+        /// Driver's identification number.
+        /// </summary>
+        [JsonProperty("driver_identification_number")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("driver_identification_number")]
+#endif
+        public string DriverIdentificationNumber { get; set; }
+
+        /// <summary>
+        /// Driver's tax number.
+        /// </summary>
+        [JsonProperty("driver_tax_number")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("driver_tax_number")]
+#endif
+        public string DriverTaxNumber { get; set; }
+
+        /// <summary>
         /// Full name of the driver on the reservation.
         /// </summary>
         [JsonProperty("name")]
