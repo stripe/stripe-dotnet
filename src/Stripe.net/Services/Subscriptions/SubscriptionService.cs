205a5b0f2132f7bd6b82ac2958192d641a3c4f79
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-05-16 02:03:59 +0000

    Update generated code for v1763

--- src/Stripe.net/Services/Subscriptions/SubscriptionService.cs
+++ src/Stripe.net/Services/Subscriptions/SubscriptionService.cs
@@ -186,6 +186,24 @@ namespace Stripe
         }
 
         /// <summary>
+        /// <p>This endpoint allows merchants to upgrade the billing_mode on their existing
+        /// subscriptions.</p>.
+        /// </summary>
+        public virtual Subscription Migrate(string id, SubscriptionMigrateOptions options = null, RequestOptions requestOptions = null)
+        {
+            return this.Request<Subscription>(BaseAddress.Api, HttpMethod.Post, $"/v1/subscriptions/{WebUtility.UrlEncode(id)}/migrate", options, requestOptions);
+        }
+
+        /// <summary>
+        /// <p>This endpoint allows merchants to upgrade the billing_mode on their existing
+        /// subscriptions.</p>.
+        /// </summary>
+        public virtual Task<Subscription> MigrateAsync(string id, SubscriptionMigrateOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
+        {
+            return this.RequestAsync<Subscription>(BaseAddress.Api, HttpMethod.Post, $"/v1/subscriptions/{WebUtility.UrlEncode(id)}/migrate", options, requestOptions, cancellationToken);
+        }
+
+        /// <summary>
         /// <p>Initiates resumption of a paused subscription, optionally resetting the billing cycle
         /// anchor and creating prorations. If a resumption invoice is generated, it must be paid or
         /// marked uncollectible before the subscription will be unpaused. If payment succeeds the
