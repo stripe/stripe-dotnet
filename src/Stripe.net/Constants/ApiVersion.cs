d19e28122bcff71d385a99320b5f3f0da9d78d1d
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-06-12 15:09:36 +0000

    Update generated code for v1796 and 2025-05-28.preview

--- src/Stripe.net/Constants/ApiVersion.cs
+++ src/Stripe.net/Constants/ApiVersion.cs
@@ -4,5 +4,7 @@ namespace Stripe
     internal class ApiVersion
     {
         public const string Current = "2025-05-28.preview";
+        public const string CurrentMajor = "preview";
+        public const string CurrentMonthly = "2025-05-28";
     }
 }
