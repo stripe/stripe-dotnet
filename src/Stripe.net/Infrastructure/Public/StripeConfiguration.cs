bac1fa7e3c9f9201d70936fd127de811d4596496
jar-stripe <jar@stripe.com>
2025-07-22 19:34:59 -0700

    Separates default StripeConfiguration.StripeClient from feature-full StripeClient class and add telemetry string to StripeClient (#3150)

--- src/Stripe.net/Infrastructure/Public/StripeConfiguration.cs
+++ src/Stripe.net/Infrastructure/Public/StripeConfiguration.cs
@@ -3,7 +3,6 @@ namespace Stripe
     using System;
     using System.Collections.Generic;
     using System.Configuration;
-    using System.Reflection;
     using System.Runtime.Serialization;
     using Newtonsoft.Json;
     using Stripe.Infrastructure;
@@ -243,7 +242,7 @@ namespace Stripe
             ApiKey = newApiKey;
         }
 
-        private static StripeClient BuildDefaultStripeClient()
+        private static IStripeClient BuildDefaultStripeClient()
         {
             if (ApiKey != null && ApiKey.Length == 0)
             {
@@ -268,7 +267,7 @@ namespace Stripe
                 maxNetworkRetries: MaxNetworkRetries,
                 appInfo: AppInfo,
                 enableTelemetry: EnableTelemetry);
-            return new StripeClient(ApiKey, ClientId, httpClient: httpClient);
+            return new DefaultStripeClient(ApiKey, ClientId, httpClient);
         }
     }
 }
