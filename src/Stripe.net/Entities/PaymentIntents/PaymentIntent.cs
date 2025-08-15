4f092fb0548a90bf8261c98e171d98676063e1e9
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-08-15 22:38:59 +0000

    Update generated code for v1920 and 2025-08-27.preview

--- src/Stripe.net/Entities/PaymentIntents/PaymentIntent.cs
+++ src/Stripe.net/Entities/PaymentIntents/PaymentIntent.cs
@@ -332,6 +332,27 @@ namespace Stripe
         public string Description { get; set; }
 
         /// <summary>
+        /// The list of payment method types to exclude from use with this payment.
+        /// One of: <c>acss_debit</c>, <c>affirm</c>, <c>afterpay_clearpay</c>, <c>alipay</c>,
+        /// <c>alma</c>, <c>amazon_pay</c>, <c>au_becs_debit</c>, <c>bacs_debit</c>,
+        /// <c>bancontact</c>, <c>billie</c>, <c>blik</c>, <c>boleto</c>, <c>card</c>,
+        /// <c>cashapp</c>, <c>crypto</c>, <c>customer_balance</c>, <c>eps</c>, <c>fpx</c>,
+        /// <c>giropay</c>, <c>gopay</c>, <c>grabpay</c>, <c>id_bank_transfer</c>, <c>ideal</c>,
+        /// <c>kakao_pay</c>, <c>klarna</c>, <c>konbini</c>, <c>kr_card</c>, <c>mb_way</c>,
+        /// <c>mobilepay</c>, <c>multibanco</c>, <c>naver_pay</c>, <c>nz_bank_account</c>,
+        /// <c>oxxo</c>, <c>p24</c>, <c>pay_by_bank</c>, <c>payco</c>, <c>paynow</c>, <c>paypal</c>,
+        /// <c>payto</c>, <c>pix</c>, <c>promptpay</c>, <c>qris</c>, <c>rechnung</c>,
+        /// <c>revolut_pay</c>, <c>samsung_pay</c>, <c>satispay</c>, <c>sepa_debit</c>,
+        /// <c>shopeepay</c>, <c>sofort</c>, <c>stripe_balance</c>, <c>swish</c>, <c>twint</c>,
+        /// <c>us_bank_account</c>, <c>wechat_pay</c>, or <c>zip</c>.
+        /// </summary>
+        [JsonProperty("excluded_payment_method_types")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("excluded_payment_method_types")]
+#endif
+        public List<string> ExcludedPaymentMethodTypes { get; set; }
+
+        /// <summary>
         /// The FX Quote used for the PaymentIntent.
         /// </summary>
         [JsonProperty("fx_quote")]
