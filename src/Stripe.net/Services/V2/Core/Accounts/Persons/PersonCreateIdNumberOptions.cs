c3964f09671da23b7d8e2d9b068f3c24d0694436
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-08-19 18:06:50 +0000

    Update generated code for v1930 and 2025-08-27.preview

--- src/Stripe.net/Services/V2/Core/Accounts/Persons/PersonCreateIdNumberOptions.cs
+++ src/Stripe.net/Services/V2/Core/Accounts/Persons/PersonCreateIdNumberOptions.cs
@@ -10,8 +10,11 @@ namespace Stripe.V2.Core.Accounts
     {
         /// <summary>
         /// The ID number type of an individual.
-        /// One of: <c>ae_eid</c>, <c>br_cpf</c>, <c>de_stn</c>, <c>hk_id</c>, <c>mx_rfc</c>,
-        /// <c>my_nric</c>, <c>nl_bsn</c>, <c>sg_fin</c>, <c>sg_nric</c>, <c>th_lc</c>,
+        /// One of: <c>ae_eid</c>, <c>ao_nif</c>, <c>az_tin</c>, <c>bd_brc</c>, <c>bd_etin</c>,
+        /// <c>bd_nid</c>, <c>br_cpf</c>, <c>cr_cpf</c>, <c>cr_dimex</c>, <c>cr_nite</c>,
+        /// <c>de_stn</c>, <c>do_rcn</c>, <c>gt_nit</c>, <c>hk_id</c>, <c>kz_iin</c>, <c>mx_rfc</c>,
+        /// <c>my_nric</c>, <c>mz_nuit</c>, <c>nl_bsn</c>, <c>pe_dni</c>, <c>pk_cnic</c>,
+        /// <c>pk_snic</c>, <c>sa_tin</c>, <c>sg_fin</c>, <c>sg_nric</c>, <c>th_lc</c>,
         /// <c>th_pin</c>, <c>us_itin</c>, <c>us_itin_last_4</c>, <c>us_ssn</c>, or
         /// <c>us_ssn_last_4</c>.
         /// </summary>
