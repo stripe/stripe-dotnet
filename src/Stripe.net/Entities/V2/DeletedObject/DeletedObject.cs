8ae00439e65a847a5ab48297e887f708ac562d4d
David Brownman <109395161+xavdid-stripe@users.noreply.github.com>
2025-09-08 18:04:21 -0700

    ⚠️ Build SDK w/ V2 OpenAPI spec (#3167)
    * generate w/ v2 openapi spec
    
    * move deleted object path

---
+++ src/Stripe.net/Entities/V2/DeletedObject/DeletedObject.cs
@@ -0,0 +1,30 @@
+// File generated from our OpenAPI spec
+namespace Stripe.V2
+{
+    using Newtonsoft.Json;
+#if NET6_0_OR_GREATER
+    using STJS = System.Text.Json.Serialization;
+#endif
+
+    public class DeletedObject : StripeEntity<DeletedObject>, IHasId, IHasObject
+    {
+        /// <summary>
+        /// The ID of the object that's being deleted.
+        /// </summary>
+        [JsonProperty("id")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("id")]
+#endif
+        public string Id { get; set; }
+
+        /// <summary>
+        /// String representing the type of the object that has been deleted. Objects of the same
+        /// type share the same value of the object field.
+        /// </summary>
+        [JsonProperty("object")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("object")]
+#endif
+        public string Object { get; set; }
+    }
+}
