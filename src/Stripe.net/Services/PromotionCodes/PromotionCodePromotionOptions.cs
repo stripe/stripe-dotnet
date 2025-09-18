2a163b875f00f932e7e846a873ec49bcb98ca3f9
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-09-18 02:16:45 +0000

    Update generated code for v1981 and 2025-09-30.preview

---
+++ src/Stripe.net/Services/PromotionCodes/PromotionCodePromotionOptions.cs
@@ -0,0 +1,29 @@
+// File generated from our OpenAPI spec
+namespace Stripe
+{
+    using Newtonsoft.Json;
+#if NET6_0_OR_GREATER
+    using STJS = System.Text.Json.Serialization;
+#endif
+
+    public class PromotionCodePromotionOptions : INestedOptions
+    {
+        /// <summary>
+        /// The coupon for this promotion code.
+        /// </summary>
+        [JsonProperty("coupon")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("coupon")]
+#endif
+        public string Coupon { get; set; }
+
+        /// <summary>
+        /// Specifies the type of promotion code. Currently, only coupon is supported.
+        /// </summary>
+        [JsonProperty("type")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("type")]
+#endif
+        public string Type { get; set; }
+    }
+}
