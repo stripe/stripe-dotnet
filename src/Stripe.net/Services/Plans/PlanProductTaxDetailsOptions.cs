39fe6150034c5d70f597e907c163a1bee51a4328
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-12-17 08:41:47 +0000

    Update generated code for v2144 and

---
+++ src/Stripe.net/Services/Plans/PlanProductTaxDetailsOptions.cs
@@ -0,0 +1,20 @@
+// File generated from our OpenAPI spec
+namespace Stripe
+{
+    using Newtonsoft.Json;
+#if NET6_0_OR_GREATER
+    using STJS = System.Text.Json.Serialization;
+#endif
+
+    public class PlanProductTaxDetailsOptions : INestedOptions
+    {
+        /// <summary>
+        /// A <a href="https://docs.stripe.com/tax/tax-categories">tax code</a> ID.
+        /// </summary>
+        [JsonProperty("tax_code")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("tax_code")]
+#endif
+        public string TaxCode { get; set; }
+    }
+}
