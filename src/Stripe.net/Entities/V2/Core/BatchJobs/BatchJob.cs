4c117da2d7f7350ab86a6f724dd95b301ed3a9c9
Michael Broshi <mbroshi@stripe.com>
2026-01-29 17:23:51 -0500

    Inital PoC

---
+++ src/Stripe.net/Entities/V2/Core/BatchJobs/BatchJob.cs
@@ -0,0 +1,38 @@
+// File generated from our OpenAPI spec
+namespace Stripe.V2.Core
+{
+    using Newtonsoft.Json;
+#if NET6_0_OR_GREATER
+    using STJS = System.Text.Json.Serialization;
+#endif
+
+    public class BatchJob : StripeEntity<BatchJob>, IHasId, IHasObject
+    {
+        /// <summary>
+        /// Unique identifier for the batch job.
+        /// </summary>
+        [JsonProperty("id")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("id")]
+#endif
+        public string Id { get; set; }
+
+        /// <summary>
+        /// String representing the object's type. Objects of the same type share the same value.
+        /// </summary>
+        [JsonProperty("object")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("object")]
+#endif
+        public string Object { get; set; }
+
+        /// <summary>
+        /// The URL to upload the JSONL file to.
+        /// </summary>
+        [JsonProperty("url")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("url")]
+#endif
+        public string Url { get; set; }
+    }
+}
