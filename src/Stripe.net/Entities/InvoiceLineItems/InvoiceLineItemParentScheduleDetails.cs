345cc23bbe3d62d6df9e9e44886ceb89f7004203
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-02-17 20:14:22 +0000

    Update generated code for v2175 and

--- src/Stripe.net/Entities/InvoiceLineItems/InvoiceLineItemParentScheduleDetails.cs
+++ src/Stripe.net/Entities/InvoiceLineItems/InvoiceLineItemParentScheduleDetails.cs
@@ -16,5 +16,41 @@ namespace Stripe
         [STJS.JsonPropertyName("schedule")]
 #endif
         public string Schedule { get; set; }
+
+        /// <summary>
+        /// The invoice item that generated this line item.
+        /// </summary>
+        [JsonProperty("invoice_item")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("invoice_item")]
+#endif
+        public string InvoiceItem { get; set; }
+
+        /// <summary>
+        /// Whether this is a proration.
+        /// </summary>
+        [JsonProperty("proration")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("proration")]
+#endif
+        public bool Proration { get; set; }
+
+        /// <summary>
+        /// Additional details for proration line items.
+        /// </summary>
+        [JsonProperty("proration_details")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("proration_details")]
+#endif
+        public InvoiceLineItemParentScheduleDetailsProrationDetails ProrationDetails { get; set; }
+
+        /// <summary>
+        /// The subscription that the schedule belongs to.
+        /// </summary>
+        [JsonProperty("subscription")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("subscription")]
+#endif
+        public string Subscription { get; set; }
     }
 }
---
+++ src/Stripe.net/Entities/InvoiceLineItems/InvoiceLineItemParentScheduleDetailsProrationDetailsCreditedItems.cs
@@ -0,0 +1,30 @@
+// File generated from our OpenAPI spec
+namespace Stripe
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+#if NET6_0_OR_GREATER
+    using STJS = System.Text.Json.Serialization;
+#endif
+
+    public class InvoiceLineItemParentScheduleDetailsProrationDetailsCreditedItems : StripeEntity<InvoiceLineItemParentScheduleDetailsProrationDetailsCreditedItems>
+    {
+        /// <summary>
+        /// Invoice containing the credited invoice line items.
+        /// </summary>
+        [JsonProperty("invoice")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("invoice")]
+#endif
+        public string Invoice { get; set; }
+
+        /// <summary>
+        /// Credited invoice line items.
+        /// </summary>
+        [JsonProperty("invoice_line_items")]
+#if NET6_0_OR_GREATER
+        [STJS.JsonPropertyName("invoice_line_items")]
+#endif
+        public List<string> InvoiceLineItems { get; set; }
+    }
+}
