07e117d1e386c0fc892d63c8da0caae5133b3602
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-04-08 20:16:55 +0000

    Update generated code for v2229 and

--- src/Stripe.net/Constants/ApiVersion.cs
+++ src/Stripe.net/Constants/ApiVersion.cs
@@ -3,6 +3,6 @@ namespace Stripe
 {
     internal class ApiVersion
     {
-        public const string Current = "2026-04-01.preview";
+        public const string Current = "2026-04-08.preview";
     }
 }
