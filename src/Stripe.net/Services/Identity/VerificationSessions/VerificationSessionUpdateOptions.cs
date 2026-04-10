9e32b6896f86fc030cb68b5e9fef3e3793afcb85
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-04-10 22:17:31 +0000

    Update generated code for v2237 and

--- src/Stripe.net/Services/Identity/VerificationSessions/VerificationSessionUpdateOptions.cs
+++ src/Stripe.net/Services/Identity/VerificationSessions/VerificationSessionUpdateOptions.cs
@@ -10,6 +10,13 @@ namespace Stripe.Identity
     public class VerificationSessionUpdateOptions : BaseOptions, IHasMetadata
     {
         /// <summary>
+        /// Confirm and submit the provided details for verification.
+        /// </summary>
+        [JsonProperty("confirm")]
+        [STJS.JsonPropertyName("confirm")]
+        public bool? Confirm { get; set; }
+
+        /// <summary>
         /// Set of <a href="https://docs.stripe.com/api/metadata">key-value pairs</a> that you can
         /// attach to an object. This can be useful for storing additional information about the
         /// object in a structured format. Individual keys can be unset by posting an empty value to
