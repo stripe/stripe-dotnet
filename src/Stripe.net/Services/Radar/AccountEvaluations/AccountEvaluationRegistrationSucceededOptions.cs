e77efe75a728d5681e7bbd7ac8de0b5f7c0cf0f1
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-04-20 14:55:38 +0000

    Update generated code for v2248 and

---
+++ src/Stripe.net/Services/Radar/AccountEvaluations/AccountEvaluationRegistrationSucceededOptions.cs
@@ -0,0 +1,18 @@
+// File generated from our OpenAPI spec
+namespace Stripe.Radar
+{
+    using Newtonsoft.Json;
+    using Stripe.Infrastructure;
+    using STJS = System.Text.Json.Serialization;
+
+    [STJS.JsonConverter(typeof(STJStripeOptionsConverter))]
+    public class AccountEvaluationRegistrationSucceededOptions : INestedOptions
+    {
+        /// <summary>
+        /// An optional qualification for a registration success.
+        /// </summary>
+        [JsonProperty("qualification")]
+        [STJS.JsonPropertyName("qualification")]
+        public string Qualification { get; set; }
+    }
+}
