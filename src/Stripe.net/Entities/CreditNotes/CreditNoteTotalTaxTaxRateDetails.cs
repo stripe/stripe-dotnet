92a60924abb56b7cc8886c0fc98aa466a25102c4
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-04-22 01:09:04 +0000

    Update generated code for v2252 and

--- src/Stripe.net/Entities/CreditNotes/CreditNoteTotalTaxTaxRateDetails.cs
+++ src/Stripe.net/Entities/CreditNotes/CreditNoteTotalTaxTaxRateDetails.cs
@@ -8,39 +8,11 @@ namespace Stripe
     [STJS.JsonConverter(typeof(STJStripeEntityConverter))]
     public class CreditNoteTotalTaxTaxRateDetails : StripeEntity<CreditNoteTotalTaxTaxRateDetails>
     {
-        #region Expandable TaxRate
-
         /// <summary>
-        /// (ID of the TaxRate)
         /// ID of the tax rate.
         /// </summary>
-        [JsonIgnore]
-        [STJS.JsonIgnore]
-        public string TaxRateId
-        {
-            get => this.InternalTaxRate?.Id;
-            set => this.InternalTaxRate = SetExpandableFieldId(value, this.InternalTaxRate);
-        }
-
-        /// <summary>
-        /// (Expanded)
-        /// ID of the tax rate.
-        ///
-        /// For more information, see the <a href="https://stripe.com/docs/expand">expand documentation</a>.
-        /// </summary>
-        [JsonIgnore]
-        [STJS.JsonIgnore]
-        public TaxRate TaxRate
-        {
-            get => this.InternalTaxRate?.ExpandedObject;
-            set => this.InternalTaxRate = SetExpandableFieldObject(value, this.InternalTaxRate);
-        }
-
         [JsonProperty("tax_rate")]
-        [JsonConverter(typeof(ExpandableFieldConverter<TaxRate>))]
         [STJS.JsonPropertyName("tax_rate")]
-        [STJS.JsonConverter(typeof(STJExpandableFieldConverter<TaxRate>))]
-        internal ExpandableField<TaxRate> InternalTaxRate { get; set; }
-        #endregion
+        public string TaxRate { get; set; }
     }
 }
