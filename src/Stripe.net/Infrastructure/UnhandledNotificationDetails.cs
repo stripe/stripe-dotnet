b4258296182e683677077b4b388a4baa0cd34596
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-05-06 00:38:39 +0000

    Update generated code for v2255 and

---
+++ src/Stripe.net/Infrastructure/UnhandledNotificationDetails.cs
@@ -0,0 +1,23 @@
+// File copied from our code generator; changes here will be overwritten.
+namespace Stripe
+{
+    /// <summary>
+    /// EventArgs for Stripe webhook event notifications.
+    /// Contains the strongly-typed EventNotification and the StripeClient instance.
+    /// </summary>
+    public class UnhandledNotificationDetails
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnhandledNotificationDetails"/> class.
+        /// </summary>
+        public UnhandledNotificationDetails(bool isKnownEventType)
+        {
+            this.IsKnownEventType = isKnownEventType;
+        }
+
+        /// <summary>
+        /// Gets the StripeClient instance that can be used to make API requests.
+        /// </summary>
+        public bool IsKnownEventType { get; }
+    }
+}
